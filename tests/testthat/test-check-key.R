ecc327deb027d22fce3292964575cd0b389bb3bf
Tobias <tobias@cynkra.com>
2019-02-27 12:48:40 +0100

    tests

--- tests/testthat/test-check-key.R
+++ tests/testthat/test-check-key.R
@@ -3,9 +3,11 @@ context("test-check-key")
 data <-
   tribble(~c1, ~c2, ~c3,
           1, 2, 3,
-          4, 5, 6,
+          4, 5, 3,
           1, 2, 4)
 
 test_that("check_key() checks primary key properly?", {
   expect_error(check_key(data, c1, c2))
+  expect_silent(check_key(data, c1, c3))
+  expect_silent(check_key(data, c2, c3))
 })
