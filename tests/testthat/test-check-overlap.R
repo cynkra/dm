34ba1a730c509f8f7bdf9dd6d9e3489f528976c6
Kirill Müller <krlmlr+r@mailbox.org>
2019-02-28 14:17:59 +0100

    Rename

--- tests/testthat/test-check-overlap.R
+++ tests/testthat/test-check-overlap.R
@@ -1,6 +1,6 @@
 context("test-check-overlap")
 
-test_that("check_overlap() checks overlap properly?", {
+test_that("check_set_equality() checks overlap properly?", {
   expect_error(check_set_equality(data_1, a, data_2, a))
   expect_silent(check_set_equality(data_1, a, data_3, a))
 })
