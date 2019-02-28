6fefef3c2448601a9c1dbf525821d6fb7563b4fe
Kirill Müller <krlmlr+r@mailbox.org>
2019-02-28 14:13:48 +0100

    Rename in tests

--- tests/testthat/test-check-overlap.R
+++ tests/testthat/test-check-overlap.R
@@ -1,6 +1,6 @@
 context("test-check-overlap")
 
 test_that("check_overlap() checks overlap properly?", {
-  expect_error(check_overlap(data_1, a, data_2, a))
-  expect_silent(check_overlap(data_1, a, data_3, a))
+  expect_error(check_set_equality(data_1, a, data_2, a))
+  expect_silent(check_set_equality(data_1, a, data_3, a))
 })
