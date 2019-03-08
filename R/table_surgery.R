d05885fa0d0b5fbeeaefa2388834dda73c92c21d
Tobias <tobias@cynkra.com>
2019-03-08 08:41:11 +0100

    major_tweaks

--- R/table_surgery.R
+++ R/table_surgery.R
@@ -29,9 +29,7 @@
 #'   purrr::flatten_dfr()
 #' }
 #' @export
-decompose_table <- function(.data, new_id_column,...) {
-
-
+decompose_table <- function(.data, new_id_column, ...) {
   .data_q <- enquo(.data)
   cols_q <- enexprs(...)
   id_col_q <- enexpr(new_id_column)
@@ -40,17 +38,25 @@ decompose_table <- function(.data, new_id_column,...) {
     cols_q %>%
     map_chr(~ as_name(.))
 
-  if (as_label(id_col_q) %in% names(eval_tidy(.data_q))) stop(
-    paste0("`new_id_column` can not have an identical name as one of the columns of ", as_label(.data_q))
+  if (as_label(id_col_q) %in% colnames(eval_tidy(.data_q))) {
+    stop(
+      paste0("`new_id_column` can not have an identical name as one of the columns of ", as_label(.data_q))
     )
+  }
   if (!(length(cols_q))) stop(paste0("Columns of ", as_label(.data_q), " need to be specified in ellipsis"))
-  if (!all(cols_q %in% names(eval_tidy(.data_q)))) stop(
-    paste0("Not all specified variables `", paste(cols_chr, collapse = ", "), "` are columns of ", as_label(.data_q),
-    ". These columns are: `", paste(names(eval_tidy(.data_q)), collapse = ", "), "`.")
-  )
-  if (length(cols_q) == length(eval_tidy(.data_q))) stop(
-    paste0("Number of columns to be extracted has to be less than total number of columns of ", as_label(.data_q))
+  if (!all(cols_q %in% colnames(eval_tidy(.data_q)))) {
+    stop(
+      paste0(
+        "Not all specified variables `", paste(cols_chr, collapse = ", "), "` are columns of ", as_label(.data_q),
+        ". These columns are: `", paste(colnames(eval_tidy(.data_q)), collapse = ", "), "`."
+      )
     )
+  }
+  if (length(cols_q) == length(colnames(eval_tidy(.data_q)))) {
+    stop(
+      paste0("Number of columns to be extracted has to be less than total number of columns of ", as_label(.data_q))
+    )
+  }
 
   parent_table <-
     select(eval_tidy(.data_q), !!!cols_q) %>%
@@ -63,7 +69,8 @@ decompose_table <- function(.data, new_id_column,...) {
     map_chr(~ paste(.))
 
   names_data <-
-    eval_tidy(.data_q) %>% names()
+    eval_tidy(.data_q) %>%
+    colnames()
 
   non_key_names <-
     setdiff(names_data, cols_chr)
@@ -71,13 +78,14 @@ decompose_table <- function(.data, new_id_column,...) {
   child_table <-
     eval_tidy(.data_q) %>%
     left_join(
-      parent_table, by = cols_chr
+      parent_table,
+      by = cols_chr
     ) %>%
     select(non_key_names, !!id_col_q) %>%
     select(1, !!id_col_q, everything())
   # 1 was originally first column in ".data" after extracting child table. It is therefore assumed to be a key to table ".data"
 
-  return(list("child_table" = child_table, "parent_table" = parent_table))
+  list("child_table" = child_table, "parent_table" = parent_table)
 }
 
 #' Perform table fusion by combining two tables by a common (key) column and then removing this column.
@@ -88,7 +96,6 @@ decompose_table <- function(.data, new_id_column,...) {
 #' @name reunite_parent_child
 #' @export
 reunite_parent_child <- function(child_table, parent_table, id_column) {
-
   id_col_q <- enexpr(id_column)
 
   id_col_chr <-
@@ -97,32 +104,29 @@ reunite_parent_child <- function(child_table, parent_table, id_column) {
   child_table %>%
     left_join(parent_table, by = id_col_chr) %>%
     select(-!!id_col_q)
-
 }
 
 #' Perform table fusion by combining two tables by a common (key) column and then removing this column.
 #'
 #' @description `reunite_parent_child_from_list`: After joining the two tables by the column `id_column`, this column is removed.
-#' The function is exactly the inverse of `decompose_table()`.
+#' The function is almost exactly the inverse of `decompose_table()`.
 #'
 #' @rdname reunite_parent_child
 #' @export
-reunite_parent_child_from_list <- function(list_of_child_tables, id_column) {
-
+reunite_parent_child_from_list <- function(list_of_parent_child_tables, id_column) {
   id_col_q <- enexpr(id_column)
 
   id_col_chr <-
     as_name(id_col_q)
 
-  child_table <- list_of_child_tables %>%
-      extract("child_table") %>%
-      flatten_dfr()
-    parent_table <- list_of_child_tables %>%
-      magrittr::extract("parent_table") %>%
-      flatten_dfr()
+  child_table <- list_of_parent_child_tables %>%
+    extract("child_table") %>%
+    flatten_dfr()
+  parent_table <- list_of_parent_child_tables %>%
+    magrittr::extract("parent_table") %>%
+    flatten_dfr()
 
   child_table %>%
     left_join(parent_table, by = id_col_chr) %>%
     select(-!!id_col_q)
-
 }
