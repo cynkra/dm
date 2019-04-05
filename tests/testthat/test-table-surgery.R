4eeaf3615bb306a822feda98b43e2f0a564a6f10
Tobias <tobias@cynkra.com>
2019-04-05 12:13:55 +0200

    expect_equal() does already what is expected

--- tests/testthat/test-table-surgery.R
+++ tests/testthat/test-table-surgery.R
@@ -6,10 +6,10 @@ test_that("decompose_table() decomposes tables nicely?", {
 })
 
 test_that("reunite_parent_child() reunites parent and child nicely?", {
-  expect_true(all_equal(data_4, reunite_parent_child(data_4_child, data_4_parent, aef_id))) # not identical, since column order is normally changed
+  expect_equal(data_4, reunite_parent_child(data_4_child, data_4_parent, aef_id)) # not identical, since column order is normally changed
 })
 
 test_that("reunite_parent_child_from_list() reunites parent and child nicely?", {
-  expect_true(all_equal(data_4, reunite_parent_child_from_list(list_of_data_4_parent_and_child, aef_id))) # not identical, since column order is normally changed
+  expect_equal(data_4, reunite_parent_child_from_list(list_of_data_4_parent_and_child, aef_id)) # not identical, since column order is normally changed
 })
 
