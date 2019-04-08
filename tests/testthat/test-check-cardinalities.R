2e9ea9abb0c26c8aad10f4a18e6af880943a613f
Tobias <tobias@cynkra.com>
2019-04-08 12:08:22 +0200

    expect another known output

--- tests/testthat/test-check-cardinalities.R
+++ tests/testthat/test-check-cardinalities.R
@@ -13,15 +13,21 @@ test_that("check_cardinality_...() functions are checking the cardinality correc
   expect_known_output(
     expect_error(
       check_cardinality_0_n(parent_table = d1, primary_key_column = a, child_table = d2, foreign_key_column = a)
-    ),
+      ),
     "out/card-0-n-d1-d2.txt"
   )
 
-  expect_error(check_cardinality_1_1(d1, a, d4, c))
-  expect_error(check_cardinality_1_1(d4, c, d1, a))
-  expect_error(check_cardinality_0_1(d1, a, d2, a))
-  expect_error(check_cardinality_0_1(d1, a, d4, c))
-  expect_error(check_cardinality_0_1(d4, c, d1, a))
-  expect_error(check_cardinality_0_n(d4, c, d5, a))
+  expect_known_output(
+    expect_error(
+      check_cardinality_0_1(d1, a, d2, a)
+    ),
+    "out/card-0-1-d1-d2.txt"
+  )
+
   expect_error(check_cardinality_1_1(d5, a, d4, c))
+  expect_error(check_cardinality_0_n(d4, c, d5, a))
+  expect_error(check_cardinality_0_1(d4, c, d1, a))
+  expect_error(check_cardinality_0_1(d1, a, d4, c))
+  expect_error(check_cardinality_1_1(d4, c, d1, a))
+  expect_error(check_cardinality_1_1(d1, a, d4, c))
 })
