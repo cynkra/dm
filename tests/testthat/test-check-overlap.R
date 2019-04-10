0546e07ee0b097431efcdd9dfff49960bf40a804
Tobias <tobias@cynkra.com>
2019-04-10 16:51:31 +0200

    test for `check_set_equality()` on all sources

--- tests/testthat/test-check-overlap.R
+++ tests/testthat/test-check-overlap.R
@@ -1,6 +1,21 @@
-context("test-check-overlap")
+context("test-check-set-equality")
 
-test_that("check_set_equality() checks overlap properly?", {
-  expect_error(check_set_equality(data_1, a, data_2, a))
-  expect_silent(check_set_equality(data_1, a, data_3, a))
+test_that("check_set_equality() checks properly if 2 sets of values are equal?", {
+
+  map2(
+    .x = data_1_src,
+    .y = data_3_src,
+    ~ expect_silent(
+      check_set_equality(.x, a, .y, a)
+      )
+    )
+
+  map2(
+    .x = data_1_src,
+    .y = data_2_src,
+    ~ expect_error(
+      check_set_equality(.x, a, .y, a),
+      "Column `a` in table `.y` contains values \\(see above\\) that are not present in column `a` in table `.x`"
+    )
+  )
 })
