21e23425e43040f089c6442cc36e35fe9f54499e
Tobias <tobias@cynkra.com>
2019-04-10 17:07:41 +0200

    suppress message of where {here} thinks it is

--- tests/testthat/helper-src.R
+++ tests/testthat/helper-src.R
@@ -5,7 +5,7 @@ search_lib_and_load <- function(pkg) {
 
 search_lib_and_load("testthat")
 search_lib_and_load("dbplyr")
-search_lib_and_load("here")
+suppressMessages(search_lib_and_load("here"))
 
 test_register_src("df", src_df(env = new.env()))
 test_register_src("sqlite", src_sqlite(":memory:", create = TRUE))
