5af98beae2a1a711d236dcea4734acd196c12848
Tobias <tobias@cynkra.com>
2019-04-11 17:45:18 +0200

    import {datamodelr}

--- R/import.R
+++ R/import.R
@@ -4,4 +4,5 @@
 #' @import tibble
 #' @rawNamespace import(purrr, except = c(list_along, rep_along, invoke, modify, as_function, flatten_dbl, flatten_lgl, flatten_int, flatten_raw, flatten_chr, splice, flatten, prepend, `%@%`, `%||%`))
 #' @rawNamespace import(magrittr, except = c(set_names))
+#' @import datamodelr
 NULL
