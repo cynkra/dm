f19e05dfadcb808ac5dd94e36217db42513ecc1c
Tobias <tobias@cynkra.com>
2019-04-12 11:31:42 +0200

    functions  and

---
+++ R/test_dm.R
@@ -0,0 +1,37 @@
+#' Loads `dm`-objects into one or more registered sources
+#'
+#' @description Works like `dbplyr::test_load()`, just for `dm`_objects.
+#'
+#' @examples
+#' dbplyr::test_register_src("df", dplyr::src_df(env = new.env()))
+#' dbplyr::test_register_src("sqlite", dplyr::src_sqlite(":memory:", create = TRUE))
+#'
+#' dm_test_obj <- dm(dplyr::src_df(pkg = "nycflights13"))
+#' dm_test_obj_srcs <- dm_test_load(dm_test_obj)
+#'
+#' @export
+dm_test_load <-
+  function(
+    x,
+    name = NULL, # NULL results in the same name on the src for each table as the current table name in the `dm`-object
+    srcs = dbplyr:::test_srcs$get(), # FIXME: nto exported from {dplyr}... could also "borrow" source code as new function here!?
+    ignore = character()) {
+
+    stopifnot(is.character(ignore))
+    srcs <- srcs[setdiff(names(srcs), ignore)]
+    dm_table_names <- src_tbls(x)
+    if (is_null(name)) name <- dm_table_names
+    current_data_model <- dm_get_data_model(x)
+    tables <- map(dm_table_names, ~ tbl(dm_get_src(x), .x)) %>% set_names(dm_table_names) # FIXME: should be replaced by `dm_select_tables()` once it exists
+
+    map(srcs, ~ copy_list_of_tables_to(src = .x, list_of_tables = tables, overwrite = TRUE))
+    map(srcs, ~ dm(.x, current_data_model))
+  }
+
+# FIXME: when passing on the ... to `copy_to()` an error is thrown during copying tables to DBs: "temporary is not a flag (a length one logical vector)."
+# when checking `temporary`, it shows seems to be the name of one of the tables that are being copied. If the function would be exported, we might
+# want to have a look at it.
+copy_list_of_tables_to <-
+  function(src, list_of_tables, name_vector = names(list_of_tables), overwrite = FALSE, ...) {
+    map2(list_of_tables, name_vector, ~ copy_to(dest = src, df = .x, name = .y, overwrite = overwrite))
+  }
