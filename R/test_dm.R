47f8aeea48c04100adf2c4ab9a82f5eff61684b0
Tobias <tobias@cynkra.com>
2019-04-17 10:21:10 +0200

    unnecessary with new `dm`-class definition

--- R/test_dm.R
+++ R/test_dm.R
@@ -53,15 +53,3 @@ check_correct_input <- function(dm, table) {
     )
   )
 }
-
-# internal helper function for finding a new suitable table name based on an old, existing table name
-find_new_name <- function(dm, table) {
-  tables <- src_tbls(dm_get_src(dm))
-  i <-  1L
-  new_name <-  paste0(table, "_", toString(i))
-  while(new_name %in% tables) {
-    i  <- i + 1L
-    new_name <- paste0(table, "_", toString(i))
-  }
-  new_name
-}
