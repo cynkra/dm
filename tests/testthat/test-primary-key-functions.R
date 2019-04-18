15904c1295451b0da07330f6c8bd96ac4b9dcff9
Tobias <tobias@cynkra.com>
2019-04-18 09:41:42 +0200

    merge master

--- tests/testthat/test-primary-key-functions.R
+++ tests/testthat/test-primary-key-functions.R
@@ -1,5 +1,8 @@
 context("test-primary-key-functions")
 
+candidates_table_1 <- tibble(column = c("a", "b"), candidate = c(TRUE, TRUE))
+candidates_table_2 <- tibble(column = c("c"), candidate = c(FALSE))
+
 test_that("cdm_add_pk() works as intentended?", {
   map(
     .x = cdm_test_obj_src,
@@ -42,7 +45,6 @@ test_that("cdm_add_pk() works as intentended?", {
 })
 
 
-
 test_that("cdm_remove_pk() works as intentended?", {
   map(
     .x = cdm_test_obj_src,
@@ -54,38 +56,10 @@ test_that("cdm_remove_pk() works as intentended?", {
 
   map(
     .x = cdm_test_obj_src,
-    ~ expect_silent(
+    ~ expect_true(
       cdm_add_pk(.x, "cdm_table_1", a) %>%
-        cdm_remove_pk("cdm_table_2") # still does its job, even if there was no key in the first place :)
-    )
-  )
-
-  map(
-    .x = cdm_test_obj_src,
-    ~ expect_error(
-      cdm_add_pk(.x, "cdm_table_1", a) %>%
-        cdm_remove_pk("cdm_table_4"),
-      "cdm_table_4 not in `dm`-object. Available table names are: cdm_table_1, cdm_table_2, cdm_table_3"
-    )
-  )
-})
-
-
-
-test_that("cdm_remove_pk() works as intentended?", {
-  map(
-    .x = cdm_test_obj_src,
-    ~ expect_silent(
-      cdm_add_pk(.x, "cdm_table_1", a) %>%
-        cdm_remove_pk("cdm_table_1")
-    )
-  )
-
-  map(
-    .x = cdm_test_obj_src,
-    ~ expect_silent(
-      cdm_add_pk(.x, "cdm_table_1", a) %>%
-        cdm_remove_pk("cdm_table_2") # still does its job, even if there was no key in the first place :)
+        cdm_remove_pk("cdm_table_2") %>% # still does its job, even if there was no key in the first place :)
+        cdm_has_pk("cdm_table_1")
     )
   )
 
@@ -145,3 +119,22 @@ test_that("cdm_get_pk() works as intentended?", {
     )
   )
 })
+
+test_that("cdm_check_for_pk_candidates() works properly?", {
+  map(
+    cdm_test_obj_src,
+    ~ expect_identical(
+      cdm_check_for_pk_candidates(.x, "cdm_table_1"),
+      candidates_table_1
+    )
+  )
+
+  map(
+    cdm_test_obj_src,
+    ~ expect_identical(
+      cdm_check_for_pk_candidates(.x, "cdm_table_2"),
+      candidates_table_2
+    )
+  )
+})
+
