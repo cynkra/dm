f97abb04ab31a387c9eea25f3b84b7bc4ff7d518
Tobias <tobias@cynkra.com>
2019-04-18 12:06:16 +0200

    test if col is in table

--- R/test_dm.R
+++ R/test_dm.R
@@ -5,7 +5,7 @@
 #' @examples
 #' dbplyr::test_register_src("df", dplyr::src_df(env = new.env()))
 #' dbplyr::test_register_src("sqlite", dplyr::src_sqlite(":memory:", create = TRUE))
-#' 
+#'
 #' cdm_test_obj <- dm(dplyr::src_df(pkg = "nycflights13"))
 #' cdm_test_obj_srcs <- cdm_test_load(cdm_test_obj)
 #' @export
@@ -31,7 +31,7 @@ copy_list_of_tables_to <- function(src, list_of_tables,
   map2(list_of_tables, name_vector, copy_to, dest = src, overwrite = overwrite, ...)
 }
 
-# internal helper function:
+# internal helper functions:
 # validates, that object `dm` is of class `dm` and that `table` is character and is part of the `dm`-object
 check_correct_input <- function(dm, table) {
   if (!is_dm(dm)) abort("'dm' has to be of class 'dm'")
@@ -49,4 +49,10 @@ check_correct_input <- function(dm, table) {
       )
     )
   }
+}
+
+# validates, that the given column is indeed part of the table of the `dm` object.
+check_col_input <- function(dm, table, column) {
+  tbl_colnames <- cdm_get_tables(dm) %>% extract2(table) %>% names()
+  if (!column %in% tbl_colnames) abort(paste0(column, " is not a column of ", table, ". Its columns are: \n", paste0(tbl_colnames, collapse = ", ")))
 }
