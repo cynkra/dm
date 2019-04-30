dc3e4ee3c89502079000bc9f68f71ade2b590d5f
Tobias <tobias@cynkra.com>
2019-04-30 09:16:47 +0200

    function `cdm_remove_fk(dm, table, ref_table, column)`

--- R/foreign-keys.R
+++ R/foreign-keys.R
@@ -72,3 +72,53 @@ cdm_get_fk <- function(dm, table, ref_table) {
 
   as.character(dm_data_model$references$column[fk_ind]) # FIXME: maybe something nicer?
 }
+
+#' Remove reference(s) from one table of a `dm` to another
+#'
+#' @details Can either remove one reference between the two tables or all at once if parameter `column = NULL`.
+#' All parameters can be provided unquoted or quoted.'
+#'
+#' @export
+cdm_remove_fk <- function(dm, table, column, ref_table) {
+
+  table_name <- as_name(enquo(table))
+  ref_table_name <- as_name(enquo(ref_table))
+
+  check_correct_input(dm, eval_tidy(table_name))
+  check_correct_input(dm, eval_tidy(ref_table_name))
+
+  if (!cdm_has_fk(dm, !! table_name, !! ref_table_name)) {
+    return(dm)
+  }
+
+  if (is_null(eval_tidy(enexprs(column)) %>% extract2(1))) { # FIXME: there must be a nicer way of checking if NULL, without running into problems, when column is a symbol (I tried is_symbol() without success)
+      col_names <- cdm_get_fk(dm, !! table_name, !! ref_table_name)
+  } else {
+    col_names <- as_name(enexpr(column))
+    if (col_names == "") {
+      abort("Parameter 'column' has to be set. 'NULL' for removing all references.")
+    }
+  }
+
+  if (!(all(col_names %in% cdm_get_fk(dm, !! table_name, !! ref_table_name)))) {
+    abort(paste0("The given column '",
+                 paste0(col_names, collapse = ", "),
+                 "' is not a foreign key column of table '",
+                 table_name,
+                 "' with regards to ref_table '",
+                 ref_table_name,
+                 "'. Foreign key columns are: '",
+                 paste0(cdm_get_fk(dm, !! table_name, !! ref_table_name), collapse = ", "), "'.")
+          )
+  }
+
+  dm$data_model <-
+    rm_data_model_reference(
+      cdm_get_data_model(dm),
+      table_name,
+      col_names,
+      ref_table_name
+      )
+
+  dm
+}
