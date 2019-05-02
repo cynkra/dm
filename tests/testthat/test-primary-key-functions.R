9d98035bac27f1331b39dd7e26e457dbd16dc31a
Tobias <tobias@cynkra.com>
2019-05-02 18:10:14 +0200

    unquoted tables

--- tests/testthat/test-primary-key-functions.R
+++ tests/testthat/test-primary-key-functions.R
@@ -11,16 +11,16 @@ test_that("cdm_add_pk() works as intended?", {
   map(
     .x = cdm_test_obj_src,
     ~ expect_silent(
-      cdm_add_pk(.x, "cdm_table_1", a) %>%
-        cdm_add_pk("cdm_table_1", b, force = TRUE)
+      cdm_add_pk(.x, cdm_table_1, a) %>%
+        cdm_add_pk(cdm_table_1, b, force = TRUE)
     )
   )
 
   map(
     .x = cdm_test_obj_src,
     ~ expect_error(
-      cdm_add_pk(.x, "cdm_table_1", a) %>%
-        cdm_add_pk("cdm_table_1", b),
+      cdm_add_pk(.x, cdm_table_1, a) %>%
+        cdm_add_pk(cdm_table_1, b),
       "If you want to change the existing primary key for a table, set `force` == TRUE."
     )
   )
@@ -28,7 +28,7 @@ test_that("cdm_add_pk() works as intended?", {
   map(
     .x = cdm_test_obj_src,
     ~ expect_error(
-      cdm_add_pk(.x, "cdm_table_2", c),
+      cdm_add_pk(.x, cdm_table_2, c),
       "`c` is not a unique key of `table_from_dm`"
     )
   )
@@ -36,7 +36,7 @@ test_that("cdm_add_pk() works as intended?", {
   map(
     .x = cdm_test_obj_src,
     ~ expect_silent(
-      cdm_add_pk(.x, "cdm_table_2", c, check = FALSE)
+      cdm_add_pk(.x, cdm_table_2, c, check = FALSE)
     )
   )
 })
@@ -46,25 +46,25 @@ test_that("cdm_rm_pk() works as intended?", {
   map(
     .x = cdm_test_obj_src,
     ~ expect_silent(
-      cdm_add_pk(.x, "cdm_table_1", a) %>%
-        cdm_rm_pk("cdm_table_1")
+      cdm_add_pk(.x, cdm_table_1, a) %>%
+        cdm_rm_pk(cdm_table_1)
     )
   )
 
   map(
     .x = cdm_test_obj_src,
     ~ expect_true(
-      cdm_add_pk(.x, "cdm_table_1", a) %>%
-        cdm_rm_pk("cdm_table_2") %>% # still does its job, even if there was no key in the first place :)
-        cdm_has_pk("cdm_table_1")
+      cdm_add_pk(.x, cdm_table_1, a) %>%
+        cdm_rm_pk(cdm_table_2) %>% # still does its job, even if there was no key in the first place :)
+        cdm_has_pk(cdm_table_1)
     )
   )
 
   map(
     .x = cdm_test_obj_src,
     ~ expect_error(
-      cdm_add_pk(.x, "cdm_table_1", a) %>%
-        cdm_rm_pk("cdm_table_5"),
+      cdm_add_pk(.x, cdm_table_1, a) %>%
+        cdm_rm_pk(cdm_table_5),
       "cdm_table_5 not in `dm`-object. Available table names are: cdm_table_1, cdm_table_2, cdm_table_3, cdm_table_4"
     )
   )
@@ -74,15 +74,15 @@ test_that("cdm_has_pk() works as intended?", {
   map(
     .x = cdm_test_obj_src,
     ~ expect_false(
-      cdm_has_pk(.x, "cdm_table_2")
+      cdm_has_pk(.x, cdm_table_2)
     )
   )
 
   map(
     .x = cdm_test_obj_src,
     ~ expect_true(
-      cdm_add_pk(.x, "cdm_table_1", a) %>%
-        cdm_has_pk("cdm_table_1")
+      cdm_add_pk(.x, cdm_table_1, a) %>%
+        cdm_has_pk(cdm_table_1)
     )
   )
 })
@@ -91,7 +91,7 @@ test_that("cdm_get_pk() works as intended?", {
   map(
     .x = cdm_test_obj_src,
     ~ expect_identical(
-      cdm_get_pk(.x, "cdm_table_1"),
+      cdm_get_pk(.x, cdm_table_1),
       character(0)
     )
   )
@@ -99,8 +99,8 @@ test_that("cdm_get_pk() works as intended?", {
   map(
     .x = cdm_test_obj_src,
     ~ expect_identical(
-      cdm_add_pk(.x, "cdm_table_1", a) %>%
-        cdm_get_pk("cdm_table_1"),
+      cdm_add_pk(.x, cdm_table_1, a) %>%
+        cdm_get_pk(cdm_table_1),
       "a"
     )
   )
@@ -108,9 +108,9 @@ test_that("cdm_get_pk() works as intended?", {
   map(
     .x = cdm_test_obj_src,
     ~ expect_error(
-      cdm_add_pk(.x, "cdm_table_1", a) %>%
+      cdm_add_pk(.x, cdm_table_1, a) %>%
         cdm_add_pk_impl("cdm_table_1", "b") %>%
-        cdm_get_pk("cdm_table_1"),
+        cdm_get_pk(cdm_table_1),
       "Please use cdm_rm_pk() on cdm_table_1, more than 1 primary key is currently set for it.",
       fixed = TRUE
     )
@@ -125,7 +125,7 @@ test_that("cdm_check_for_pk_candidates() works properly?", {
   map(
     cdm_test_obj_src,
     ~ expect_identical(
-      cdm_check_for_pk_candidates(.x, "cdm_table_1"),
+      cdm_check_for_pk_candidates(.x, cdm_table_1),
       candidates_table_1
     )
   )
@@ -133,7 +133,7 @@ test_that("cdm_check_for_pk_candidates() works properly?", {
   map(
     cdm_test_obj_src,
     ~ expect_identical(
-      cdm_check_for_pk_candidates(.x, "cdm_table_2"),
+      cdm_check_for_pk_candidates(.x, cdm_table_2),
       candidates_table_2
     )
   )
