044021993fe615596eda52c89b48cc35ed9a589b
Tobias <tobias@cynkra.com>
2019-05-03 12:15:42 +0200

    nicer structure, documentation

--- R/draw-dm.R
+++ R/draw-dm.R
@@ -1,22 +1,32 @@
-# in datamodelr the following colors defined:
-#
-# default: greyish yellow
-# accent1nb: blue, no border
-# accent2nb: orange, no border
-# accent3nb: yellow, no border
-# accent4nb: green, no border
-# accent5nb: dark blue, no border
-# accent6nb: light grey, no border
-# accent7nb: grey, no border
-# accent1: blue
-# accent2: orange
-# accent3: yellow
-# accent4: green
-# accent5: dark blue
-# accent6: light grey
-# accent7: grey
+#' Draw schema of a 'dm'-object's data model
+#'
+#' @description `cdm_draw_data_model()` draws a schema of the data model using `datamodelr` (which in turn uses `DiagrammeR`)
+#' @name cdm_draw_data_model
+#' @export
+cdm_draw_data_model <- function(dm, rankdir = "LR", col_attr = "column", view_type = "keys_only", columnArrows = TRUE, ...) {
+  data_model <- cdm_get_data_model(dm)
+  graph <- dm_create_graph(data_model,
+                           rankdir = rankdir,
+                           col_attr = col_attr,
+                           view_type = view_type,
+                           columnArrows = columnArrows,
+                           ...)
 
+  dm_render_graph(graph)
+}
+
+
+#' @description `cdm_draw_data_model_set_colors()` allows to define the colors in which to display the tables of the data model
+#' @rdname cdm_draw_data_model
+#' @export
 cdm_draw_data_model_set_colors <- function(dm, list_of_table_colors) {
+
+  names <- names(list_of_table_colors)
+  if (!all(names %in% available)) {
+    abort(paste0("Available color names are only: ",
+                 paste0(available, " (= ", color, ")", collapse = ",\n")))
+  }
+
   data_model <- cdm_get_data_model(dm)
   data_model <- dm_set_display(data_model, list_of_table_colors)
 
@@ -24,14 +34,42 @@ cdm_draw_data_model_set_colors <- function(dm, list_of_table_colors) {
   dm
 }
 
-cdm_draw_data_model <- function(dm, rankdir = "LR", col_attr = "column", view_type = "keys_only", columnArrows = TRUE, ...) {
-  data_model <- cdm_get_data_model(dm)
-  graph <- dm_create_graph(data_model,
-                  rankdir = rankdir,
-                  col_attr = col_attr,
-                  view_type = view_type,
-                  columnArrows = columnArrows,
-                  ...)
+#' @description `cdm_draw_data_model_print_colors()` prints an overview of the available colors and their names
+#' @rdname cdm_draw_data_model
+#' @export
+cdm_draw_data_model_print_colors <- function() {
+  cat(paste0("'", available, "'", " (= ", color, ")"), sep = ",\n")
+}
 
-  dm_render_graph(graph)
-  }
+available <- c("default",
+               "accent1nb",
+               "accent2nb",
+               "accent3nb",
+               "accent4nb",
+               "accent5nb",
+               "accent6nb",
+               "accent7nb",
+               "accent1",
+               "accent2",
+               "accent3",
+               "accent4",
+               "accent5",
+               "accent6",
+               "accent7")
+
+color <- c("greyish yellow",
+           "blue, no border",
+           "orange, no border",
+           "yellow, no border",
+           "green, no border",
+           "dark blue, no border",
+           "light grey, no border",
+           "grey, no border",
+           "blue",
+           "orange",
+           "yellow",
+           "green",
+           "dark blue",
+           "light grey",
+           "grey")
+
