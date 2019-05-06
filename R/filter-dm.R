d8468c2bdf6761bfa3c7cee9a8dd4f2f88f4ddf0
Tobias <tobias@cynkra.com>
2019-05-06 15:40:23 +0200

    should be in other file

--- R/filter-dm.R
+++ R/filter-dm.R
@@ -32,30 +32,3 @@ cdm_filter <- function(dm, table, ...) {
     data_model = cdm_get_data_model(dm)
   )
 }
-
-compute_join <- function(dm, filtered_table, table_to_filter) {
-  filtered_table_name <- as_name(enexpr(filtered_table))
-  table_to_filter_name <- as_name(enexpr(table_to_filter))
-
-  if (!(cdm_has_fk(dm, !!filtered_table_name, !!table_to_filter_name) |
-        cdm_has_fk(dm, !!table_to_filter_name, !!filtered_table_name))) {
-    abort(paste0("No foreign key relation exists between table '", filtered_table_name,
-                 "' and table ", table_to_filter_name, ", joining not possible."))
-  }
-
-  if (cdm_has_fk(dm, !!filtered_table_name, !!table_to_filter_name)) {
-    filtered_table_col <- cdm_get_fk(dm, !!filtered_table_name, !!table_to_filter_name)
-    table_to_filter_col <- cdm_get_pk(dm, !!table_to_filter_name)
-  } else {
-    filtered_table_col <- cdm_get_pk(dm, !!filtered_table_name)
-    table_to_filter_col <- cdm_get_fk(dm, !!table_to_filter_name, !!filtered_table_name)
-  }
-  by <- filtered_table_col
-  names(by) <- table_to_filter_col
-
-  tables_obj <- cdm_get_tables(dm)
-  filtered_table_obj <- tables_obj[[filtered_table_name]]
-  table_to_filter_obj <- tables_obj[[table_to_filter_name]]
-
-  semi_join(table_to_filter_obj, filtered_table_obj, by = by)
-}
