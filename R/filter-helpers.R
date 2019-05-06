34eb9f5e549a4063c4e3d5870a59292dbbdd244b
Tobias <tobias@cynkra.com>
2019-05-06 15:42:46 +0200

    function cdm_join_tbl()

---
+++ R/filter-helpers.R
@@ -0,0 +1,26 @@
+#' @export
+cdm_join_tbl <- function(dm, lhs, rhs, join = semi_join) {
+  lhs_name <- as_name(enexpr(lhs))
+  rhs_name <- as_name(enexpr(rhs))
+
+  if (!(cdm_has_fk(dm, !!lhs_name, !!rhs_name) |
+        cdm_has_fk(dm, !!rhs_name, !!lhs_name))) {
+    abort(paste0("No foreign key relation exists between table '", lhs_name,
+                 "' and table ", rhs_name, ", joining not possible."))
+  }
+
+  if (cdm_has_fk(dm, !!lhs_name, !!rhs_name)) {
+    lhs_col <- cdm_get_fk(dm, !!lhs_name, !!rhs_name)
+    rhs_col <- cdm_get_pk(dm, !!rhs_name)
+  } else {
+    lhs_col <- cdm_get_pk(dm, !!lhs_name)
+    rhs_col <- cdm_get_fk(dm, !!rhs_name, !!lhs_name)
+  }
+  by <- rhs_col
+  names(by) <- lhs_col
+
+  lhs_obj <- tbl(dm, lhs_name)
+  rhs_obj <- tbl(dm, rhs_name)
+
+  join(lhs_obj, rhs_obj, by = by)
+}
