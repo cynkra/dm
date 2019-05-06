a6c64347a5373b36b347b73a30db1f21b03c2d96
Tobias <tobias@cynkra.com>
2019-05-06 14:08:00 +0200

    superficial implementation of 'cdm_filter()', filtering only 1 table

---
+++ R/filter-dm.R
@@ -0,0 +1,34 @@
+#' @export
+cdm_filter <- function(dm, table, ...) {
+  table_name <- as_name(enexpr(table))
+  check_correct_input(dm, table_name)
+
+  tables_obj <- cdm_get_tables(dm)
+  orig_tbl <- tables_obj[[table_name]]
+
+  if (!cdm_has_pk(dm, !!table_name)) {
+    abort(paste0(
+      "Table '", table_name, "' needs primary key for the filtering to work. ",
+      "Please set one using cdm_add_pk()."))
+  }
+
+  pk_name_orig <- cdm_get_pk(dm, !!table_name)
+  filtered_tbl_pk <- filter(orig_tbl, ...) %>%
+    select(!!pk_name_orig) %>%
+    compute()
+
+  by = pk_name_orig
+
+  filtered_tbl <- left_join(
+    filtered_tbl_pk,
+    orig_tbl,
+    by = by
+    )
+
+  tables_obj[[table_name]] <- filtered_tbl
+  new_dm(
+    src = cdm_get_src(dm),
+    tables = tables_obj,
+    data_model = cdm_get_data_model(dm)
+  )
+}
