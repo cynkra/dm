f0100f503c1506c25236c4c4bd8363d788094c45
Tobias <tobias@cynkra.com>
2019-05-08 13:38:21 +0200

    tweak

--- R/filter-dm.R
+++ R/filter-dm.R
@@ -30,7 +30,7 @@ cdm_filter <- function(dm, table, ...) {
 
   join_list <- calculate_join_list(cdm_get_data_model(dm), table_name)
 
-  # perform joins of `join_list` and update $tables part of `dm`-object
+  # perform joins of `join_list`
   tables_list <- cdm_get_tables(dm)
   tables_list[[table_name]] <- filtered_tbl
   tables_list <- perform_joins_of_join_list(tables_list, join_list)
