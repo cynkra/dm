c2cfddf0f56910611cdbcab8de81f71b74b59913
Tobias <tobias@cynkra.com>
2019-05-08 08:22:26 +0200

    comment for quicker reading the code

--- R/filter-dm.R
+++ R/filter-dm.R
@@ -11,21 +11,29 @@ cdm_filter <- function(dm, table, ...) {
       "Please set one using cdm_add_pk()."))
   }
 
+  # get local tibble of pk-values after filtering
   pk_name_orig <- cdm_get_pk(dm, !!table_name)
   filtered_tbl_pk_obj <- filter(orig_tbl, ...) %>%
     select(!!pk_name_orig) %>%
     compute()
 
-  if (nrow(filtered_tbl_pk_obj) == nrow(orig_tbl)) return(dm)
+  if (nrow(filtered_tbl_pk_obj) == nrow(orig_tbl)) return(dm) # early return if no filtering was done
 
   by = pk_name_orig
 
+  # filter original table by performing join with own pk-values
   filtered_tbl <- left_join(
     filtered_tbl_pk_obj,
     orig_tbl,
     by = by
     )
 
+  # FIXME: missing:
+  #   1. produce ordered list of filtering joins
+  #   2. perform joins
+  #   (3. adapt the following part of updating the $tables part)
+
+  # update $tables part of `dm`-object
   tables_obj <- cdm_get_tables(dm)
   tables_obj[[table_name]] <- filtered_tbl
   new_dm(
