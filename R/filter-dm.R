765a4dd1f90adb1771604f92aaa7e376f639bc21
Kirill Müller <kirill@cynkra.com>
2019-05-15 08:26:36 +0200

    - New `cdm_semi_join()`.

--- R/filter-dm.R
+++ R/filter-dm.R
@@ -19,12 +19,13 @@ cdm_filter <- function(dm, table, ...) {
 
   if (pull(count(filtered_tbl_pk_obj)) == pull(count(orig_tbl))) return(dm) # early return if no filtering was done
 
+  cdm_semi_join(dm, !!table_name, filtered_tbl_pk_obj)
+}
+
+#' @export
+cdm_semi_join <- function(dm, table, filter) {
   # filter original table by performing join with own pk-values
-  filtered_tbl <- semi_join(
-    orig_tbl,
-    filtered_tbl_pk_obj,
-    by = pk_name_orig
-  )
+  filtered_tbl <- semi_join(orig_tbl, filter, by = pk_name_orig)
 
   filtered_dm <- cdm_update_table(dm, table_name, filtered_tbl)
 
