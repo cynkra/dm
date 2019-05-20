dd2e77d6af69a03b78ab2d5cc2ea00e1f5f27780
Kirill Müller <kirill@cynkra.com>
2019-05-20 18:52:05 +0200

    Much simpler

--- tests/testthat/helper-print.R
+++ tests/testthat/helper-print.R
@@ -1,13 +1,5 @@
-# Only run if the top level call is devtools::test() or testthat::test_check()
-
-calls <-
-  sys.calls() %>%
-  as.list() %>%
-  map(as.list) %>%
-  map(1) %>%
-  map_chr(as_label)
-
-if (any(calls %in% c("devtools::test", "testthat::test_check"))) {
+# Only run if testing:
+if (testthat::is_testing()) {
   tbl_sum.tbl_sql <- function(x, ...) c()
   vctrs::s3_register("tibble::tbl_sum", "tbl_sql")
 }
