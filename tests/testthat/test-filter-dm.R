364143526d0604edd11ea9615f3cdee625d50201
Tobias <tobias@cynkra.com>
2019-05-22 14:14:58 +0200

    move object creation for tests to helper-src

--- tests/testthat/test-filter-dm.R
+++ tests/testthat/test-filter-dm.R
@@ -1,53 +1,5 @@
 context("test-filter-dm")
 
-output_1 <- list(
-  t1 = tibble(a = c(4:7), b = LETTERS[4:7]),
-  t2 = tibble(c = c("seal", "worm", "dog", "cat"), d = 4:7, e = c("F", "G", "E", "F")),
-  t3 = tibble(f = LETTERS[5:7], g = c("four", "five", "six")),
-  t4 = tibble(h = letters[3:5], i = c("five", "six", "seven"), j = c("E", "F", "F")),
-  t5 = tibble(k = 2:4,
-              l = letters[3:5],
-              m = c("tree", "streetlamp", "streetlamp")),
-  t6 = tibble(n = c("tree", "streetlamp"),
-              o = c("f", "h"))
-)
-
-output_3 <- list(
-  t1 = tibble::tribble(
-    ~a,  ~b,
-    4L, "D",
-    7L, "G"
-  )
-  ,
-  t2 = tibble::tribble(
-        ~c, ~d,  ~e,
-    "seal", 4L, "F",
-    "cat",  7L, "F"
-  )
-  ,
-  t3 = tibble::tribble(
-    ~f,     ~g,
-    "F", "five"
-  )
-  ,
-  t4 = tibble::tribble(
-    ~h,      ~i,  ~j,
-    "d",   "six", "F",
-    "e", "seven", "F"
-  )
-  ,
-  t5 = tibble::tribble(
-    ~k,  ~l,           ~m,
-    3L, "d", "streetlamp",
-    4L, "e", "streetlamp"
-  )
-  ,
-  t6 = tibble::tribble(
-              ~n,  ~o,
-    "streetlamp", "h"
-)
-)
-
 test_that("cdm_filter() works as intended for reversed dm", {
   map(.x = dm_for_filter_rev_src,
       ~ expect_identical(
