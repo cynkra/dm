02336524c58a1bc7b8f26b883f1e0ccdf6a1e2f1
Tobias Schieferdecker <tobias@cynkra.com>
2019-05-22 07:26:39 +0000

    Apply suggestion to R/table-surgery.R

--- R/table-surgery.R
+++ R/table-surgery.R
@@ -63,6 +63,7 @@ decompose_table <- function(.data, new_id_column, ...) {
   parent_table <-
     select(eval_tidy(.data_q), !!!cols_q) %>%
     distinct() %>%
+    # Without as.integer(), RPostgres creates integer64 column (#15)
     mutate(!!id_col_q := as.integer(row_number())) %>%
     select(!!id_col_q, everything())
 
