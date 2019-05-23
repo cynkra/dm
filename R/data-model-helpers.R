b60278386cb079909ea09fc2c5028bb03981b4e8
Tobias <tobias@cynkra.com>
2019-05-23 16:59:15 +0200

    nicer way of handling `data_model`

--- R/data-model-helpers.R
+++ R/data-model-helpers.R
@@ -1,7 +1,10 @@
 # helper function for updating the data model when adding a reference from one table to another
 upd_data_model_reference <- function(data_model, table, column, ref_table, ref_column) {
-  temp_data_model <- upd_references_reference(data_model, table, column, ref_table, ref_column)
-  upd_columns_reference(temp_data_model, table, column, ref_table, ref_column)
+  new_data_model(
+    tables = data_model$tables,
+    columns = upd_columns_reference(data_model, table, column, ref_table, ref_column),
+    references = upd_references_reference(data_model, table, column, ref_table, ref_column)
+  )
 }
 
 # updates only the $references part of the data model with a reference
@@ -28,25 +31,29 @@ upd_references_reference <- function(data_model, table, column, ref_table, ref_c
       ref_col_num = 1)
   }
 
-  data_model$references <- new_references
-  data_model
+    new_references
 }
 
 # updates only the $columns part of the data model with a reference
 upd_columns_reference <- function(data_model, table, column, ref_table, ref_column) {
-  if (is_null(data_model$columns$ref_col)) data_model$columns$ref_col <- NA
-  ind_columns_upd <- data_model$columns$column == column & data_model$columns$table == table
+  columns <- data_model$columns
+  new_columns <- columns
+  if (is_null(columns$ref_col)) new_columns$ref_col <- NA
+  ind_columns_upd <- columns$column == column & columns$table == table
 
-  data_model$columns$ref[ind_columns_upd] <- ref_table
-  data_model$columns$ref_col[ind_columns_upd] <- ref_column
-  data_model
+  new_columns$ref[ind_columns_upd] <- ref_table
+  new_columns$ref_col[ind_columns_upd] <- ref_column
+  new_columns
 }
 
 
 # helper function for removing one or more references from one table to another in the data model
 rm_data_model_reference <- function(data_model, table, cols, ref_table) {
-  temp_data_model <- rm_references_reference(data_model, table, cols, ref_table)
-  rm_columns_reference(temp_data_model, table, cols, ref_table)
+  new_data_model(
+    tables = data_model$tables,
+    columns = rm_columns_reference(data_model, table, cols, ref_table),
+    references = rm_references_reference(data_model, table, cols, ref_table)
+  )
 }
 
 
@@ -64,8 +71,7 @@ rm_references_reference <- function(data_model, table, cols, ref_table) {
     new_references <- references[lines_to_keep,]
   }
 
-  data_model$references <- new_references
-  data_model
+  new_references
 }
 
 # removes one or more references only from the $columns part of the data model
@@ -79,28 +85,46 @@ rm_columns_reference <- function(data_model, table, cols, ref_table) {
   cols_data_model[lines_to_modify,]$ref <- NA
   cols_data_model[lines_to_modify,]$ref_col <- NA
 
-  data_model$columns <- cols_data_model
-  data_model
+  cols_data_model
 }
 
 # removes one or more tables from a data model, taking key relations into consideration
 rm_table_from_data_model <- function(data_model, tables) {
   ind_keep_tables <- !(data_model$tables$table %in% tables)
-  data_model$tables <- data_model$tables[ind_keep_tables,]
+  new_tables <- data_model$tables[ind_keep_tables,]
 
   ind_keep_columns <- !(data_model$columns$table %in% tables)
-  data_model$columns <- data_model$columns[ind_keep_columns,]
+  new_columns <- data_model$columns[ind_keep_columns,]
 
-  ind_alter_columns <- !is.na(data_model$columns$ref) & data_model$columns$ref %in% tables
+  ind_alter_columns <- !is.na(new_columns$ref) & new_columns$ref %in% tables
   if (any(ind_alter_columns)) {
-    data_model$columns[ind_alter_columns,]$ref <- NA
-    data_model$columns[ind_alter_columns,]$ref_col <- NA
+    new_columns[ind_alter_columns,]$ref <- NA
+    new_columns[ind_alter_columns,]$ref_col <- NA
   }
 
   ind_keep_references <-
     !(data_model$references$table %in% tables) &
     !(data_model$references$ref %in% tables)
-  data_model$references <- data_model$references[ind_keep_references,]
-  data_model$references$ref_id <- seq_along(data_model$references$ref_id)
-  data_model
+  new_references <- data_model$references[ind_keep_references,]
+  new_references$ref_id <- seq_along(new_references$ref_id)
+
+  new_data_model(
+    tables = new_tables,
+    columns = new_columns,
+    references = new_references
+  )
+}
+
+new_data_model <- function(tables, columns, references) {
+  stopifnot(nrow(tables) > 0)
+  stopifnot(nrow(columns) > 0)
+
+  structure(
+    list(
+      tables = tables,
+      columns = columns,
+      references = references
+    ),
+    class = "data_model"
+  )
 }
