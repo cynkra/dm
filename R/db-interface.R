5227bc985a4e75317ac00e8a703b33bf244a431d
Tobias <tobias@cynkra.com>
2019-05-24 10:51:06 +0200

    add and pass the dots

--- R/db-interface.R
+++ R/db-interface.R
@@ -1,12 +1,12 @@
 #' @export
-cdm_copy_to <- function(dest, dm, overwrite = TRUE) {
+cdm_copy_to <- function(dest, dm, overwrite = TRUE, ...) {
 # for now focusing on MSSQL
 # we want to
 #   1. change `dm$src` to `dest`
 #   2. copy the tables to `dest`
 #   3. implement the key situation within our `dm` on the DB
 
-new_tables <- copy_list_of_tables_to(dest, list_of_tables = cdm_get_tables(dm), overwrite = overwrite)
+new_tables <- copy_list_of_tables_to(dest, list_of_tables = cdm_get_tables(dm), overwrite = overwrite, ...)
 
 invisible(
   new_dm(
