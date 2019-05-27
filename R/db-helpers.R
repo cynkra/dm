76ef8922311a50798b1f3910f617294081035c2a
Tobias <tobias@cynkra.com>
2019-05-27 17:46:38 +0200

    mv copy_to()

--- R/db-helpers.R
+++ R/db-helpers.R
@@ -12,3 +12,10 @@ systime_convenient <- function() {
     str_replace_all("-", "_") %>%
     str_replace(" ", "_")
 }
+
+# FIXME: should this be exported?
+copy_list_of_tables_to <- function(src, list_of_tables,
+                                   name_vector = names(list_of_tables),
+                                   overwrite = FALSE, ...) {
+  map2(list_of_tables, name_vector, copy_to, dest = src, overwrite = overwrite, ...)
+}
