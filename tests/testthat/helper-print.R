ff9561483166af4770bee0979f157876317b600a
Tobias <tobias@cynkra.com>
2019-06-03 12:56:00 +0200

    test if this is a test as a function

--- tests/testthat/helper-print.R
+++ tests/testthat/helper-print.R
@@ -1,17 +1,5 @@
 # Only run if the top level call is devtools::test() or testthat::test_check()
-
-calls <-
-  sys.calls() %>%
-  as.list() %>%
-  map(as.list) %>%
-  map(1) %>%
-  map_chr(as_label)
-
-is_test_call <- any(calls %in% c("devtools::test", "testthat::test_check"))
-
-is_testing <- rlang::is_installed("testthat") && testthat::is_testing()
-
-if (is_test_call || is_testing) {
+if (is_this_a_test()) {
   tbl_sum.tbl_sql <- function(x, ...) c()
   vctrs::s3_register("tibble::tbl_sum", "tbl_sql")
 }
