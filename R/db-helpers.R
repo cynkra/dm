2d7eb2538a257eb5eaf3dd3e5f4d193c9e5869bb
Tobias <tobias@cynkra.com>
2019-06-17 12:23:02 +0200

    new function 'cdm_set_key_constraints()' & make use of 'get_db_table_names()'

--- R/db-helpers.R
+++ R/db-helpers.R
@@ -23,20 +23,19 @@ copy_list_of_tables_to <- function(src, list_of_tables,
 create_queries <- function(
   dest,
   pk_information,
-  fk_information,
-  temporary
+  fk_information
   ) {
   if (!is_null(pk_information)) {
-    q_not_nullable <- queries_not_nullable(dest, pk_information, temporary)
-    q_set_pk_cols <- queries_set_pk_cols(dest, pk_information, temporary)
+    q_not_nullable <- queries_not_nullable(dest, pk_information)
+    q_set_pk_cols <- queries_set_pk_cols(dest, pk_information)
   } else {
     q_not_nullable <- ""
     q_set_pk_cols <- ""
   }
 
   if (!is_null(fk_information)) {
-    q_adapt_fk_col_classes <- queries_adapt_fk_col_classes(dest, fk_information, temporary)
-    q_set_fk_relations <- queries_set_fk_relations(dest, fk_information, temporary)
+    q_adapt_fk_col_classes <- queries_adapt_fk_col_classes(dest, fk_information)
+    q_set_fk_relations <- queries_set_fk_relations(dest, fk_information)
   } else {
     q_adapt_fk_col_classes <- ""
     q_set_fk_relations <- ""
@@ -46,14 +45,13 @@ create_queries <- function(
   queries[queries != ""]
 }
 
-queries_not_nullable <- function(dest, pk_information, temporary) {
-  db_tables <- pk_information$unique_names
+queries_not_nullable <- function(dest, pk_information) {
+  db_tables <- pk_information$remote_name
   cols_to_set_not_null <- pk_information$pk_col
   cols_classes <- pk_information$pk_class
   cols_db_classes <- class_to_db_class(dest, cols_classes)
 
   if (is_mssql(dest)) {
-    if (temporary) db_tables <- paste0("##", db_tables)
     pmap_chr(
       list(
         db_tables,
@@ -71,12 +69,10 @@ queries_not_nullable <- function(dest, pk_information, temporary) {
       } else return("")
 }
 
-queries_set_pk_cols <- function(dest, pk_information, temporary) {
-  db_tables <- pk_information$unique_names
+queries_set_pk_cols <- function(dest, pk_information) {
+  db_tables <- pk_information$remote_name
   cols_to_set_as_pk <- pk_information$pk_col
   if (is_mssql(dest) || is_postgres(dest)) {
-    if (temporary && is_mssql(dest))
-      db_tables <- paste0("##", db_tables)
     map2_chr(
       db_tables,
       cols_to_set_as_pk,
@@ -86,13 +82,12 @@ queries_set_pk_cols <- function(dest, pk_information, temporary) {
     return("")
 }
 
-queries_adapt_fk_col_classes <- function(dest, fk_information, temporary) {
+queries_adapt_fk_col_classes <- function(dest, fk_information) {
   db_child_tables <- fk_information$db_child_table
   cols_to_adapt <- fk_information$child_fk_col
   child_col_classes <- fk_information$col_class
   cols_db_classes <- class_to_db_class(dest, child_col_classes)
   if (is_mssql(dest)) {
-    if (temporary) db_child_tables <- paste0("##", db_child_tables)
     pmap_chr(
       list(db_child_tables,
            cols_to_adapt,
@@ -108,17 +103,13 @@ queries_adapt_fk_col_classes <- function(dest, fk_information, temporary) {
     } else return("")
 }
 
-queries_set_fk_relations <- function(dest, fk_information, temporary) {
+queries_set_fk_relations <- function(dest, fk_information) {
   db_child_tables <- fk_information$db_child_table
   child_fk_cols <- fk_information$child_fk_col
   db_parent_tables <- fk_information$db_parent_table
   parent_pk_col <- fk_information$pk_col
 
   if (is_mssql(dest) || is_postgres(dest)) {
-    if (temporary && is_mssql(dest)) {
-      db_child_tables <- paste0("##", db_child_tables)
-      db_parent_tables <- paste0("##", db_parent_tables)
-      }
     pmap_chr(
       list(
         db_child_tables,
--- R/db-interface.R
+++ R/db-interface.R
@@ -30,36 +30,42 @@ new_tables <- copy_list_of_tables_to(
   temporary = temporary,
   ...)
 
+new_src <- src_from_src_or_con(dest)
 
-if (set_key_constraints) {
-  # FIXME: all of the if() {...} content should probably be in a function like `cdm_set_key_constraints()`
+remote_dm <- new_dm(
+  src = new_src,
+  tables = new_tables,
+  data_model = cdm_get_data_model(dm)
+  )
+
+if (set_key_constraints) cdm_set_key_constraints(remote_dm)
+
+invisible(remote_dm)
+}
+
+#' @export
+cdm_set_key_constraints <- function(dm) {
+
+  if (!is_src_db(dm) && !is_this_a_test()) abort_src_not_db()
+  db_table_names <- get_db_table_names(dm)
+
   tables_w_pk <- cdm_get_all_pks(dm)
   if (nrow(tables_w_pk) > 0) {
     pk_info <- tables_w_pk %>%
-      left_join(list_of_unique_names, by = c("table" = "table_names"))
+      left_join(db_table_names, by = c("table" = "table_name"))
   } else pk_info <- NULL
 
   if (nrow(cdm_get_all_fks(dm)) > 0) {
-  fk_info <-
-    cdm_get_all_fks(dm) %>%
-    left_join(tables_w_pk, by = c("parent_table" = "table")) %>%
-    left_join(list_of_unique_names, by = c("child_table" = "table_names")) %>%
-    rename(db_child_table = unique_names) %>%
-    left_join(list_of_unique_names, by = c("parent_table" = "table_names")) %>%
-    rename(db_parent_table = unique_names)
+    fk_info <-
+      cdm_get_all_fks(dm) %>%
+      left_join(tables_w_pk, by = c("parent_table" = "table")) %>%
+      left_join(db_table_names, by = c("child_table" = "table_name")) %>%
+      rename(db_child_table = remote_name) %>%
+      left_join(db_table_names, by = c("parent_table" = "table_name")) %>%
+      rename(db_parent_table = remote_name)
   } else fk_info <- NULL
 
-  if (is.src(dest)) con <- dest$con else con <- dest
-  queries <- create_queries(dest, pk_info, fk_info, temporary)
+  con <- con_from_src_or_con(cdm_get_src(dm))
+  queries <- create_queries(con, pk_info, fk_info)
   if (!is_empty(queries)) walk(queries, ~dbExecute(con, .))
-  }
-
-new_src <- if (is.src(dest)) dest else src_dbi(dest) # FIXME: with MR !23 not necessary anymore
-
-invisible(
-  new_dm(
-    src = new_src,
-    tables = new_tables,
-    data_model = cdm_get_data_model(dm))
-  )
 }
