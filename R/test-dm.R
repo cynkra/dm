412f4c52f2a35186d1043e479e054bdb2faa077a
Tobias <tobias@cynkra.com>
2019-06-17 17:59:57 +0200

    R CMD check doesn't find it otherwise

--- R/test-dm.R
+++ R/test-dm.R
@@ -41,3 +41,22 @@ check_col_input <- function(dm, table, column) {
   tbl_colnames <- cdm_get_tables(dm) %>% extract2(table) %>% colnames()
   if (!column %in% tbl_colnames) abort_wrong_col_names(table, tbl_colnames, column)
 }
+
+
+is_this_a_test <- function() {
+  # Only run if the top level call is devtools::test() or testthat::test_check()
+
+  calls <-
+    sys.calls() %>%
+    as.list() %>%
+    map(as.list) %>%
+    map(1) %>%
+    map_chr(as_label)
+
+  is_test_call <- any(calls %in% c("devtools::test", "testthat::test_check"))
+
+  is_testing <- rlang::is_installed("testthat") && testthat::is_testing()
+
+  is_test_call || is_testing
+
+}
