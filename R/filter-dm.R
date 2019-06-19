c3b1feb130f4a1b4de969c63aaad60056f5c327d
Tobias <tobias@cynkra.com>
2019-06-19 08:50:25 +0200

    doc cdm_filter

--- R/filter-dm.R
+++ R/filter-dm.R
@@ -1,3 +1,21 @@
+#' Cascading filter of a `dm`-object
+#'
+#' @description 'cdm_filter()' allows you to set one or more filter conditions for one table
+#' of a `dm`-object. If the `dm`'s tables are connected via key constrains, the
+#' filtering will affect all other connected tables, leaving only the rows with
+#' the corresponding key values.
+#'
+#' @name cdm_filter
+#'
+#' @inheritParams cdm_add_pk
+#' @param ... Logical predicates defined in terms of the variables in .data.
+#' Multiple conditions are combined with &. Only rows where the condition evaluates
+#' to TRUE are kept.
+#'
+#' The arguments in ... are automatically quoted and evaluated in the context of
+#' the data frame. They support unquoting and splicing. See vignette("programming")
+#' for an introduction to these concepts.
+#'
 #' @export
 cdm_filter <- function(dm, table, ...) {
   table_name <- as_name(enexpr(table))
