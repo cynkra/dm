3f1f747daa78cc89b618c47acb302599049f3711
Tobias <tobias@cynkra.com>
2019-06-19 15:27:34 +0200

    one file per src for test-table-surgery

--- tests/testthat/test-table-surgery.R
+++ tests/testthat/test-table-surgery.R
@@ -1,38 +1,49 @@
+decompose_table_data_ts_names <-
+  find_testthat_root_file(paste0("out/decompose-table-data-ts-", src_names, ".txt"))
+reunite_parent_child_data_ts_names <-
+  find_testthat_root_file(paste0("out/reunite-parent-child-data-ts-", src_names, ".txt"))
+reunite_parent_child_from_list_data_ts_names <-
+  find_testthat_root_file(paste0("out/reunite-parent-child_from_list-data-ts-", src_names, ".txt"))
+
 context("test-table-surgery")
 
 test_that("decompose_table() decomposes tables nicely on all sources?", {
-  expect_known_output(
-    print(
-      map(
-        data_ts_src,
-        ~ decompose_table(.x, aef_id, a, e, f)
-      )
-    ),
-    file = find_testthat_root_file("out/decompose_table_data_ts_all_srcs.txt")
+
+  walk2(
+    data_ts_src,
+    decompose_table_data_ts_names,
+    ~expect_known_output(
+      print(decompose_table(.x, aef_id, a, e, f)),
+      .y
+    )
   )
 })
 
 test_that("reunite_parent_child() reunites parent and child nicely on all sources?", {
-  expect_known_output(
-    print(
-      map2(
-        .x = data_ts_child_src, data_ts_parent_src, ~ reunite_parent_child(.x, .y, aef_id)
-      )
+
+  pwalk(
+    list(
+      data_ts_child_src, data_ts_parent_src, reunite_parent_child_data_ts_names
     ),
-    file = find_testthat_root_file("out/reunite_parent_child_data_ts_all_srcs.txt")
+    ~expect_known_output(
+      print(reunite_parent_child(..1, ..2, aef_id)),
+      ..3
+    )
   )
+
 })
 
 test_that("reunite_parent_child_from_list() reunites parent and child nicely on all sources?", {
-  expect_known_output(
-    print(
-      map(
-        .x = list_of_data_ts_parent_and_child_src,
-        ~ reunite_parent_child_from_list(.x, aef_id)
-      )
-    ),
-    file = find_testthat_root_file("out/reunite_parent_child_from_list_data_ts_all_srcs.txt")
+
+  walk2(
+    list_of_data_ts_parent_and_child_src,
+    reunite_parent_child_from_list_data_ts_names,
+    ~ expect_known_output(
+      print(reunite_parent_child_from_list(.x, aef_id)),
+      .y
+    )
   )
+
 })
 
 
