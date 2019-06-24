c6312977995a588bdfdf3ec1406510c6685d4567
Kirill Müller <kirill@cynkra.com>
2019-06-24 15:20:13 +0200

    cycle = TRUE

--- R/nycflights13.R
+++ R/nycflights13.R
@@ -5,24 +5,28 @@
 #' since this should only be seen as a quick way of getting a `dm` with known tables to
 #' play around with).
 #'
-#' @param cycle_free Boolean, if `TRUE` (default), only one foreign key relation
-#' (from `flights$origin` to `airports$faa`) between `flights` and `airports` is
-#' established. If `FALSE`, a `dm` object with a double reference
-#' between those tables will be produced.
+#' @param cycle Boolean, if `FALSE`, only one foreign key relation
+#'   (from `flights$origin` to `airports$faa`) between `flights` and `airports` is
+#'   established. If `TRUE` (default), a `dm` object with a double reference
+#'   between those tables will be produced.
 #'
 #' @export
-cdm_nycflights13 <- function(cycle_free = TRUE) h(~ {
+cdm_nycflights13 <- function(cycle = TRUE) h(~ {
     dm <-
       dm(
         src_df("nycflights13")
-        ) %>%
+      ) %>%
       cdm_add_pk(planes, tailnum) %>%
       cdm_add_pk(airlines, carrier) %>%
       cdm_add_pk(airports, faa) %>%
       cdm_add_fk(flights, tailnum, planes, check = FALSE) %>%
       cdm_add_fk(flights, carrier, airlines) %>%
-      cdm_add_fk(flights, origin, airports) %>%
-      cdm_add_fk(flights, dest, airports, check = FALSE)
+      cdm_add_fk(flights, origin, airports)
 
-    if (cycle_free) dm %>% cdm_rm_fk(flights, dest, airports) else dm
+    if (cycle) {
+      dm %>%
+        cdm_add_fk(flights, dest, airports, check = FALSE)
+    } else {
+      dm
+    }
   })
