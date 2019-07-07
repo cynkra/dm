bd8dc31094638d577e6366270639ee43242065e3
Kirill Müller <kirill@cynkra.com>
2019-07-07 18:26:36 +0200

    New semantics of s3_register()

--- tests/testthat/helper-print.R
+++ tests/testthat/helper-print.R
@@ -2,5 +2,5 @@
 if (is_this_a_test()) {
   tbl_sql <- dbplyr::tbl_sql
   tbl_sum.tbl_sql <- function(x, ...) c()
-  vctrs::s3_register("tibble::tbl_sum", "tbl_sql")
+  vctrs::s3_register("tibble::tbl_sum", "tbl_sql", tbl_sum.tbl_sql)
 }
