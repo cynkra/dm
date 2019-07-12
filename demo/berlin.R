e2d4ffd2e295c63b09281857521c4c4feb7d7839
Kirill Müller <kirill@cynkra.com>
2019-07-12 14:58:19 +0200

    Update demo

--- demo/berlin.R
+++ demo/berlin.R
@@ -1,2 +1,86 @@
-## Demo for presentation at the 16th Berlin R meetup
+# Demo for presentation at the 16th Berlin R meetup
+
+# {dm} facilitates working with multiple tables
+
+## Why?
+## --------------------------------------------------------------------
+
+library(nycflights13)
+
+# Example dataset: tables linked with each other
+?flights
+?airports
+?airlines
+
+library(tidyverse)
+flights %>%
+  select(year, month, day, carrier, tailnum, origin, dest)
+
+# carrier column also present in `airlines`, this table contains
+# additional information
+airlines
+
+flights %>%
+  select(year, month, day, carrier, tailnum, origin, dest) %>%
+  left_join(airlines)
+
+# single source of truth: updating in one single location
+airlines[airlines$carrier == "UA", "name"] <- "United broke my guitar"
+
+# ...propagates to related records
+flights %>%
+  select(year, month, day, carrier, tailnum, origin, dest) %>%
+  left_join(airlines)
+
+# Same for airplanes
+planes
+
+flights %>%
+  select(year, month, day, carrier, tailnum, origin, dest) %>%
+  left_join(planes)
+
+# Same for airports?
+airports
+
+try(
+  flights %>%
+    select(year, month, day, carrier, tailnum, origin, dest) %>%
+    left_join(airports)
+)
+
+# Need to specify join variables
+flights %>%
+  select(year, month, day, carrier, tailnum, origin, dest) %>%
+  left_join(airports, by = c("origin" = "faa"))
+
+# Ensure uniqueness
+airlines %>%
+  count(carrier)
+
+airlines %>%
+  count(carrier) %>%
+  count(n)
+
+planes %>%
+  count(tailnum) %>%
+  count(n)
+
+# dm shortcut
+planes %>%
+  dm::check_key(tailnum)
+
+try(
+  planes %>%
+    dm::check_key(engines)
+)
+
+airports %>%
+  dm::check_key(faa)
+
+# Determine key candidates
+airports %>%
+  dm::pk_candidates(faa)
+
+# cleanup
+rm(airlines)
 
