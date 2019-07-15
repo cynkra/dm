5321a4188548889e799baf9bcdb7eb6f81dc8109
Kirill Müller <kirill@cynkra.com>
2019-07-15 11:39:28 +0200

    Joins

--- demo/berlin.R
+++ demo/berlin.R
@@ -198,7 +198,29 @@ nycflights13_sqlite %>%
   tbl("flights") %>%
   dbplyr::sql_render()
 
-# - cdm_join_tbl()
+## Joining tables
+## --------------------------------------------------------------------
+
+cdm_nycflights13() %>%
+  cdm_join_tbl(airlines, flights, join = left_join)
+
+cdm_nycflights13() %>%
+  cdm_join_tbl(flights, airlines, join = left_join)
+
+aa_non_jfk_january %>%
+  cdm_join_tbl(flights, airlines, join = left_join)
+
+# FIXME: Multi-joins
+
+try(
+  cdm_nycflights13() %>%
+    cdm_join_tbl(airports, airlines, join = left_join)
+)
+
+try(
+  cdm_nycflights13() %>%
+    cdm_join_tbl(flights, airports, airlines, join = left_join)
+)
 
 ## Build up data model from scratch
 ## --------------------------------------------------------------------
