e93dd37f3a14b7c1213d2cf5afd5aaf36670c193
Kirill Müller <kirill@cynkra.com>
2019-08-16 13:40:35 +0200

    Add test

--- tests/testthat/test-dm.R
+++ tests/testthat/test-dm.R
@@ -5,3 +5,11 @@ test_that("can access tables", {
     class = cdm_error("table_not_in_dm")
   )
 })
+
+test_that("can create dm with as_dm()", {
+  test_obj_df <- as_dm(cdm_get_tables(cdm_test_obj))
+
+  walk(
+    cdm_test_obj_src, ~ expect_equivalent_dm(as_dm(cdm_get_tables(.)), test_obj_df)
+  )
+})
