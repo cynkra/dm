cc12033485c5a517f8a19478d3d18602e5501841
Tobias <tobias@cynkra.com>
2019-08-21 16:14:45 +0200

    change 'browse_docs()'

--- R/pkgdown.R
+++ R/pkgdown.R
@@ -2,9 +2,8 @@
 #'
 #' Opens the documentation website in a browser.
 #'
-#' @importFrom pkgdown preview_site
 #' @keywords internal
 #' @export
 browse_docs <- function() {
-  pkgdown::preview_site()
+  browseURL("https://krlmlr.github.io/dm/")
 }
