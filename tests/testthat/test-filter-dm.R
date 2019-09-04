048946ec83a04deea41170c4eab684673b8dabfe
Tobias <tobias@cynkra.com>
2019-09-04 16:33:20 +0200

    upd tests for filtering

--- tests/testthat/test-filter-dm.R
+++ tests/testthat/test-filter-dm.R
@@ -60,55 +60,52 @@ test_that("get_all_filtered_connected() calculates the paths correctly", {
 
 
 test_that("cdm_filter() works as intended for reversed dm", {
-  skip("until further notice about filtering")
     map(.x = dm_for_filter_rev_src,
         ~ expect_identical(
-          cdm_filter(.x, t1, a < 8, a > 3) %>% cdm_get_tables() %>% map(collect),
+          cdm_filter(.x, t1, a < 8, a > 3) %>% collect() %>% cdm_get_tables(),
           rev(output_1)
           )
         )
 })
 
 test_that("cdm_filter() works as intended?", {
-skip("until further notice about filtering")
   map(
     .x = dm_for_filter_src,
     ~ expect_identical(
-      cdm_filter(.x, t1, a < 8, a > 3) %>% cdm_get_tables() %>% map(collect),
+      cdm_filter(.x, t1, a < 8, a > 3) %>% collect() %>% cdm_get_tables(),
       output_1
       )
     )
 })
 
 test_that("cdm_filter() works as intended for inbetween table", {
-skip("until further notice about filtering")
   map(
     .x = dm_for_filter_src,
     ~ expect_identical(
-      cdm_filter(.x, t3, g == "five") %>% cdm_get_tables() %>% map(collect),
+      cdm_filter(.x, t3, g == "five") %>% collect() %>% cdm_get_tables(),
       output_3
       )
     )
 })
 
 test_that("cdm_filter() works without primary keys", {
-skip("until further notice about filtering")
+
   map(
     .x = dm_for_filter_src,
     ~ expect_error(
       .x %>%
         cdm_rm_pk(t5) %>%
-        cdm_filter(t5, l == "c"),
+        cdm_filter(t5, l == "c") %>%
+        compute(),
       NA
     )
   )
 })
 
 test_that("cdm_filter() returns original `dm` object when ellipsis empty", {
-skip("until further notice about filtering")
   map(
     dm_for_filter_src,
-    ~ expect_equal(
+    ~ expect_equivalent_dm(
       cdm_filter(.x, t3),
       .x
     )
@@ -116,7 +113,6 @@ skip("until further notice about filtering")
 })
 
 test_that("cdm_filter() fails when no table name is provided", {
-skip("until further notice about filtering")
   map(
     dm_for_filter_src,
     ~ expect_error(
