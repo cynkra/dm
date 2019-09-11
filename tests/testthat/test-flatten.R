77c706034dab0156cf4a667d76d1c6cb413a1af5
Tobias <tobias@cynkra.com>
2019-09-11 13:15:32 +0200

    repair test

--- tests/testthat/test-flatten.R
+++ tests/testthat/test-flatten.R
@@ -2,7 +2,7 @@ test_that("`cdm_flatten_to_tbl()` does the right thing", {
   map(
     dm_for_flatten_src,
     ~expect_equivalent(
-      cdm_flatten_to_tbl(.) %>% collect(),
+      cdm_flatten_to_tbl(., fact) %>% collect(),
       result_from_flatten
     )
   )
