cd006e81aaa86d99426c6461f410181a786c5bfa
Kirill Müller <kirill@cynkra.com>
2019-09-13 02:26:07 +0200

    Remove unused

--- R/data-model-helpers.R
+++ R/data-model-helpers.R
@@ -171,13 +171,6 @@ add_column_row <- function(.data, col_name, col_type, table_name) {
   add_row(.data, column = col_name, type = col_type, table = table_name, key = FALSE, ref = NA)
 }
 
-cdm_colnames <- function(dm) { # maybe better as dm-method for function `colnames()`
-  data_model <- cdm_get_data_model(dm)
-  data_model %>%
-    extract2("columns") %>%
-    pull("column")
-}
-
 get_datamodel_from_overview <- function(overview) {
   new_data_model(
     tables = datamodel_tables_from_overview(overview),
