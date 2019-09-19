4508d67e8cdbb64c871f88835caca45d2174bd48
Tobias <tobias@cynkra.com>
2019-09-19 11:20:45 +0200

    tests

--- tests/testthat/test-flatten.R
+++ tests/testthat/test-flatten.R
@@ -41,20 +41,33 @@ test_that("`cdm_flatten_to_tbl()` does the right thing for 'right_join()'", {
     )
   )
 
-  # It might be expected, that if the user choses a `right_join()`, `full_join()`, `semi_join()` or
-  # `anti_join()`, he expects only the direct effect of filter conditions on dimension tables
-  # to take an effect (so `dim_1, dim_1_pk > 4` has only an effect on table `dim_1` and no cascading effect);
-  # that would mean, we'd need to sort the filter conditions by table and alter the `list_of_tables` accordingly
-  #
-  # this test depends on the answer to the above issue:
-  # map(
-  #   dm_for_flatten_src[setdiff(names(dm_for_flatten_src), "sqlite")],
-  #   ~ expect_equivalent(
-  #     cdm_flatten_to_tbl(cdm_filter(.x, fact, something > 4), fact, dim_1, join = right_join) %>% collect(),
-  #     right_join(filter(fact, something > 4), dim_1, by = c("dim_1_key" = "dim_1_pk")) %>%
-  #       rename(fact.something = something.x, dim_1.something = something.y)
-  #   )
-  # )
+  map(
+    # sqlite: RIGHT and FULL OUTER JOINs are not currently supported
+    # yet, here we eventually use `left_join()` anyway
+    dm_for_flatten_src,
+    ~ expect_equivalent(
+      cdm_flatten_to_tbl(cdm_filter(.x, fact, something > 4), fact, dim_1, join = right_join) %>% collect(),
+      left_join(filter(fact, something > 4), dim_1, by = c("dim_1_key" = "dim_1_pk")) %>%
+        rename(fact.something = something.x, dim_1.something = something.y)
+    )
+  )
+
+  map(
+    # sqlite: RIGHT and FULL OUTER JOINs are not currently supported
+    # yet, here we eventually use `left_join()` anyway
+    dm_for_flatten_src,
+    ~ expect_equivalent(
+      cdm_flatten_to_tbl(cdm_filter(.x, fact, something > 4), fact, join = right_join) %>% collect(),
+      left_join(filter(fact, something > 4), dim_1, by = c("dim_1_key" = "dim_1_pk")) %>%
+        rename(fact.something = something.x, dim_1.something = something.y) %>%
+        left_join(rename(dim_2, dim_2.something = something), by = c("dim_2_key" = "dim_2_pk")) %>%
+        left_join(rename(dim_3, dim_3.something = something), by = c("dim_3_key" = "dim_3_pk")) %>%
+        left_join(rename(dim_4, dim_4.something = something), by = c("dim_4_key" = "dim_4_pk"))
+    )
+  )
+
+
+})
 
   test_that("`cdm_flatten_to_tbl()` does the right thing for 'anti_join()' and `semi_join()`", {
 
@@ -71,9 +84,9 @@ test_that("`cdm_flatten_to_tbl()` does the right thing for 'right_join()'", {
         cdm_nycflights13(cycle = TRUE) %>%
           cdm_rm_fk(flights, origin, airports),
         flights, join = semi_join),
-      semi_join(rename(flights, flights.year = year), rename(airlines, airlines.name = name), by = "carrier") %>%
-        semi_join(rename(airports, airports.name = name), by = c("dest" = "faa")) %>%
-        semi_join(rename(planes, planes.year = year), by = "tailnum")
+      semi_join(flights, airlines, by = "carrier") %>%
+        semi_join(airports, by = c("dest" = "faa")) %>%
+        semi_join(planes, by = "tailnum")
     )
 
     expect_equivalent(
@@ -85,4 +98,4 @@ test_that("`cdm_flatten_to_tbl()` does the right thing for 'right_join()'", {
     )
 
   })
-})
+
