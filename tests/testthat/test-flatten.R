73972d3c76a6c5f99e19d4c6e14c637059404926
Kirill Müller <kirill@cynkra.com>
2019-09-29 10:02:04 +0200

    - Run some tests only for one source (#76).

--- tests/testthat/test-flatten.R
+++ tests/testthat/test-flatten.R
@@ -1,72 +1,54 @@
 test_that("`cdm_flatten_to_tbl()` does the right thing", {
-  map(
-    dm_for_flatten_src,
-    ~ expect_equivalent(
-      cdm_flatten_to_tbl(., fact) %>% collect(),
-      result_from_flatten
-    )
+  expect_equivalent(
+    cdm_flatten_to_tbl(dm_for_flatten, fact),
+    result_from_flatten
   )
 
-  map(
-    dm_for_flatten_src,
-    ~ expect_equivalent(
-      cdm_flatten_to_tbl(., fact, dim_1, dim_2) %>% collect(),
-      left_join(
-        rename(fact, fact.something = something), rename(dim_1, dim_1.something = something),
-        by = c("dim_1_key" = "dim_1_pk")) %>%
-        left_join(rename(dim_2, dim_2.something = something), by = c("dim_2_key" = "dim_2_pk"))
-    )
+  expect_equivalent(
+    cdm_flatten_to_tbl(dm_for_flatten, fact, dim_1, dim_2),
+    left_join(
+      rename(fact, fact.something = something), rename(dim_1, dim_1.something = something),
+      by = c("dim_1_key" = "dim_1_pk")) %>%
+      left_join(rename(dim_2, dim_2.something = something), by = c("dim_2_key" = "dim_2_pk"))
   )
 })
 
 test_that("`cdm_flatten_to_tbl()` does the right thing for a one-table-dm", {
-  map(
-    dm_for_flatten_src,
-    ~ expect_equivalent(
-      cdm_flatten_to_tbl(cdm_select_tbl(., fact), fact) %>% collect(),
-      fact
-    )
+  expect_equivalent(
+    dm_for_flatten %>%
+      cdm_select_tbl(fact) %>%
+      cdm_flatten_to_tbl(fact),
+    fact
   )
 })
 
 test_that("`cdm_flatten_to_tbl()` does the right thing for 'right_join()'", {
-  map(
-    # sqlite: RIGHT and FULL OUTER JOINs are not currently supported
-    # yet, here we eventually use `left_join()` anyway
-    dm_for_flatten_src,
-    ~ expect_equivalent(
-      cdm_flatten_to_tbl(cdm_filter(.x, dim_1, dim_1_pk > 4), fact, dim_1, join = right_join) %>% collect(),
-      left_join(fact, filter(dim_1, dim_1_pk > 4), by = c("dim_1_key" = "dim_1_pk")) %>%
-        rename(fact.something = something.x, dim_1.something = something.y)
-    )
+  expect_equivalent(
+    dm_for_flatten %>%
+      cdm_filter(dim_1, dim_1_pk > 4) %>%
+      cdm_flatten_to_tbl(fact, dim_1, join = right_join),
+    left_join(fact, filter(dim_1, dim_1_pk > 4), by = c("dim_1_key" = "dim_1_pk")) %>%
+      rename(fact.something = something.x, dim_1.something = something.y)
   )
 
-  map(
-    # sqlite: RIGHT and FULL OUTER JOINs are not currently supported
-    # yet, here we eventually use `left_join()` anyway
-    dm_for_flatten_src,
-    ~ expect_equivalent(
-      cdm_flatten_to_tbl(cdm_filter(.x, fact, something > 4), fact, dim_1, join = right_join) %>% collect(),
-      left_join(filter(fact, something > 4), dim_1, by = c("dim_1_key" = "dim_1_pk")) %>%
-        rename(fact.something = something.x, dim_1.something = something.y)
-    )
+  expect_equivalent(
+    dm_for_flatten %>%
+      cdm_filter(fact, something > 4) %>%
+      cdm_flatten_to_tbl(fact, dim_1, join = right_join),
+    left_join(filter(fact, something > 4), dim_1, by = c("dim_1_key" = "dim_1_pk")) %>%
+      rename(fact.something = something.x, dim_1.something = something.y)
   )
 
-  map(
-    # sqlite: RIGHT and FULL OUTER JOINs are not currently supported
-    # yet, here we eventually use `left_join()` anyway
-    dm_for_flatten_src,
-    ~ expect_equivalent(
-      cdm_flatten_to_tbl(cdm_filter(.x, fact, something > 4), fact, join = right_join) %>% collect(),
-      left_join(filter(fact, something > 4), dim_1, by = c("dim_1_key" = "dim_1_pk")) %>%
-        rename(fact.something = something.x, dim_1.something = something.y) %>%
-        left_join(rename(dim_2, dim_2.something = something), by = c("dim_2_key" = "dim_2_pk")) %>%
-        left_join(rename(dim_3, dim_3.something = something), by = c("dim_3_key" = "dim_3_pk")) %>%
-        left_join(rename(dim_4, dim_4.something = something), by = c("dim_4_key" = "dim_4_pk"))
-    )
+  expect_equivalent(
+    dm_for_flatten %>%
+      cdm_filter(fact, something > 4) %>%
+      cdm_flatten_to_tbl(fact, join = right_join),
+    left_join(filter(fact, something > 4), dim_1, by = c("dim_1_key" = "dim_1_pk")) %>%
+      rename(fact.something = something.x, dim_1.something = something.y) %>%
+      left_join(rename(dim_2, dim_2.something = something), by = c("dim_2_key" = "dim_2_pk")) %>%
+      left_join(rename(dim_3, dim_3.something = something), by = c("dim_3_key" = "dim_3_pk")) %>%
+      left_join(rename(dim_4, dim_4.something = something), by = c("dim_4_key" = "dim_4_pk"))
   )
-
-
 })
 
   test_that("`cdm_flatten_to_tbl()` does the right thing for 'anti_join()' and `semi_join()`", {
