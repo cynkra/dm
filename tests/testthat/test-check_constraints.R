1f9506a7934b2fb26db8771fba71e1ec3a770d64
Tobias <tobias@cynkra.com>
2019-10-04 14:31:17 +0200

    upd tests

--- tests/testthat/test-check_constraints.R
+++ tests/testthat/test-check_constraints.R
@@ -1,13 +1,26 @@
+nyc_check <- tibble::tribble(
+  ~table,     ~kind,   ~column,   ~is_key,  ~problem,
+  "flights",  "FK",    "tailnum", FALSE,    "<reason>",
+  "airlines", "PK",    "carrier", TRUE,     "",
+  "airports", "PK",    "faa",     TRUE,     "",
+  "planes",   "PK",    "tailnum", TRUE,     "",
+  "flights",  "FK",    "carrier", TRUE,     "",
+  "flights",  "FK",    "origin",  TRUE,     ""
+)
+
 test_that("`cdm_check_constraints()` works", {
   expect_equal(
-    suppressWarnings(cdm_check_constraints(cdm_nycflights13())),
-    list(pk = c(`airlines$carrier` = TRUE, `airports$faa` = TRUE, `planes$tailnum` = TRUE),
-         fk = c(`flights$tailnum` = FALSE, `flights$carrier` = TRUE, `flights$origin` = TRUE))
+    cdm_check_constraints(cdm_nycflights13()) %>%
+      mutate(problem = if_else(problem == "", "", "<reason>")),
+    nyc_check
   )
 
   expect_equal(
     cdm_check_constraints(dm_for_disambiguate),
-    list(pk = c(`iris_1$key` = TRUE),
-         fk = c(`iris_2$key` = TRUE))
+    tibble(table = c("iris_1", "iris_2"),
+           kind = c("PK", "FK"),
+           column = "key",
+           is_key = TRUE,
+           problem = "")
   )
 })
