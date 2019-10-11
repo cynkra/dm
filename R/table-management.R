2683a22faf555c07fe67825daea4885a66c0274a
Kirill Müller <kirill@cynkra.com>
2019-10-11 21:28:33 +0200

    Tweak

--- R/table-management.R
+++ R/table-management.R
@@ -11,8 +11,7 @@
 #'
 #' @export
 cdm_add_tbls <- function(dm, ...) {
-  # FIXME: following line needs to be replaced using check_dm() after PR 86 merged
-  if (!is_dm(dm)) abort("First parameter in `cdm_add_tbls()` needs to be of class `dm`")
+  check_dm(dm)
 
   orig_tbls <- src_tbls(dm)
 
@@ -43,14 +42,13 @@ cdm_add_tbls <- function(dm, ...) {
 #'
 #' @export
 cdm_add_tbl <- function(dm, table, table_name = NULL) {
-  # FIXME: following line needs to be replaced using check_dm() after PR 86 merged
-  if (!is_dm(dm)) abort("First parameter in `cdm_add_tbl()` needs to be of class `dm`")
+  check_dm(dm)
 
   if (!is_symbol(enexpr(table_name)) && !is_character(enexpr(table_name))) {
     table_name <- as_string(ensym(table))
   } else table_name <- as_string(ensym(table_name))
   # this function has a secondary effect and returns a value; generally not good style, but it is more convenient
-  table_name <- check_new_tbls(dm, table, table_name)
+  table_name <- check_new_tbls(dm, list(table), table_name)
   if (table_name %in% src_tbls(dm)) abort_table_already_exists(table_name)
 
   cdm_add_tbl_impl(cdm_get_def(dm), table, table_name)
@@ -84,20 +82,26 @@ cdm_add_tbl_impl <- function(def, tbl, table_name) {
 #'
 #' @export
 cdm_rm_tbls <- function(dm, ...) {
-  # FIXME: following line needs to be replaced using check_dm() after PR 86 merged
-  if (!is_dm(dm)) abort("First parameter in `cdm_add_tbl()` needs to be of class `dm`")
-  table_names <- names(exprs(..., .named = TRUE))
+  check_dm(dm)
+
+  table_names <-
+    ensyms(..., .named = FALSE) %>%
+    map_chr(~ as_name(.))
+
   check_correct_input(dm, table_names)
 
   cdm_select_tbl(dm, -one_of(!!!table_names))
-
 }
 
 
 check_new_tbls <- function(dm, tbls, name) {
   orig_tbls <- cdm_get_tables(dm)
+
   # are all new tables on the same source as the original ones?
-  if (has_length(cdm_get_tables(dm)) && !all_same_source(c(cdm_get_tables(dm)[1], tbls))) abort_not_same_src()
+  if (has_length(orig_tbls) && !all_same_source(c(orig_tbls[1], tbls))) {
+    abort_not_same_src()
+  }
+
   # test if a name "." is part of the new names, indicating a piped table that is not explicitly named
   # or if table names of the kind ".x" or "..1" are present, which would indicate a `map()`-type operation
   if ("." %in% name || any(str_detect(name, "^\\.[x-z]$")) || any(str_detect(name, "^\\.\\.[0-9][0-9]?$"))) {
