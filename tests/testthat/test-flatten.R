7a80124e4b7dc45174fb0c72c40f90f7711ba7d9
Kirill Müller <kirill@cynkra.com>
2019-10-12 00:31:42 +0200

    Get rid of new_dm2()

--- tests/testthat/test-flatten.R
+++ tests/testthat/test-flatten.R
@@ -326,7 +326,10 @@ test_that("prepare_dm_for_flatten() works", {
   red_dm <- cdm_select_tbl(dm_for_flatten, fact, dim_1, dim_3)
   tables <- cdm_get_tables(red_dm)
   tables[["fact"]] <- filter(tables[["fact"]], dim_1_key > 7)
-  prep_dm <- new_dm2(base_dm = red_dm, data = tables)
+
+  def <- cdm_get_def(red_dm)
+  def$data <- tables
+  prep_dm <- new_dm3(def)
   prep_dm_renamed <- cdm_disambiguate_cols(prep_dm, quiet = TRUE)
 
   expect_equivalent_dm(
--- tests/testthat/test-select-tbl.R
+++ tests/testthat/test-select-tbl.R
@@ -1,12 +1,11 @@
 test_that("cdm_select_tbl() selects a part of a larger `dm` as a reduced `dm`?", {
-  dm_for_filter_smaller <-
-    new_dm2(
-      data = cdm_get_tables(dm_for_filter)[c("t1", "t6")],
-      table = c("t1", "t6"),
-      segment = NA_character_,
-      display = NA_character_,
-      base_dm = dm_for_filter %>% cdm_rm_fk(t5, m, t6) %>% cdm_rm_fk(t2, d, t1)
-    )
+  def <-
+    dm_for_filter %>%
+    cdm_rm_fk(t5, m, t6) %>%
+    cdm_rm_fk(t2, d, t1) %>%
+    cdm_get_def()
+
+  dm_for_filter_smaller <- new_dm3(def[def$table %in% c("t1", "t6"), ])
 
   expect_equivalent_dm(
     cdm_select_tbl(dm_for_filter, -t2, -t3, -t4, -t5),
