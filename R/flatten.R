2ab32ae83649d3d08f3e5d58b10ab07167681982
Kirill Müller <kirill@cynkra.com>
2019-10-11 21:11:52 +0200

    Move

--- R/flatten.R
+++ R/flatten.R
@@ -242,3 +242,38 @@ check_flatten_to_tbl <- function(
     )
   }
 }
+
+prepare_dm_for_flatten <- function(dm, tables, gotta_rename) {
+  start <- tables[1]
+  # filters need to be empty, for the disambiguation to work
+  # the renaming will be minimized, if we reduce the `dm` to the necessary tables here
+  red_dm <-
+    cdm_reset_all_filters(dm) %>%
+    cdm_select_tbl(tables)
+  # Only need to compute `tbl(dm, start)`, `cdm_apply_filters()` not necessary
+  # Need to use `dm` and not `clean_dm` here, cause of possible filter conditions.
+  start_tbl <- tbl(dm, start)
+
+  if (gotta_rename) {
+    recipe <-
+      compute_disambiguate_cols_recipe(red_dm, tables, sep = ".")
+    explain_col_rename(recipe)
+    # prepare `dm` by disambiguating columns (on a reduced dm)
+    clean_dm <-
+      col_rename(red_dm, recipe)
+    # the column names of start_tbl need to be updated, since taken from `dm` and not `clean_dm`,
+    # therefore we need a named variable containing the new and old names
+    renames <-
+      pluck(recipe$renames[recipe$table == start], 1)
+    start_tbl <- start_tbl %>% rename(!!!renames)
+  } else {
+    # for `anti_join()` and `semi_join()` no renaming necessary
+    clean_dm <- red_dm
+    renames <- character(0)
+  }
+  tables <- cdm_get_tables(clean_dm)
+  tables[[start]] <- start_tbl
+  new_dm2(
+    data = tables,
+    base_dm = clean_dm)
+}
