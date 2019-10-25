2599b2134db5ba9c2c85f1cc6d64ecca96bc3c01
Tobias <tobias@cynkra.com>
2019-10-25 11:55:31 +0200

    methods 'ungroup.zoomed_dm()', 'ungroup.dm()'

--- R/dplyr.R
+++ R/dplyr.R
@@ -8,3 +8,14 @@ group_by.zoomed_dm <- function(zoomed_dm, ..., add = FALSE, .drop = group_by_dro
 
   replace_zoomed_tbl(zoomed_dm, grouped_tbl)
 }
+
+ungroup.dm <- function(dm, ...) {
+  abort_no_table_zoomed_dplyr("ungroup")
+}
+
+ungroup.zoomed_dm <- function(zoomed_dm, ...) {
+  tbl <- get_zoomed_tbl(zoomed_dm)
+  ungrouped_tbl <- ungroup(tbl, ...)
+
+  replace_zoomed_tbl(zoomed_dm, ungrouped_tbl)
+}
