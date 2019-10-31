067b394d550b95b0c708d1430d18176d4bb67a53
Tobias <tobias@cynkra.com>
2019-10-31 13:39:47 +0100

    reexport {dplyr}-functions

--- R/reexport.R
+++ R/reexport.R
@@ -1,2 +1,26 @@
 #' @export
 dplyr::`%>%`
+
+#' @export
+dplyr::filter
+
+#' @export
+dplyr::mutate
+
+#' @export
+dplyr::transmute
+
+#' @export
+dplyr::summarise_
+
+#' @export
+dplyr::select
+
+#' @export
+dplyr::rename
+
+#' @export
+dplyr::group_by
+
+#' @export
+dplyr::ungroup
