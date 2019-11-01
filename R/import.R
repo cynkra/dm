1179d0ba1f7b695a46f39276e60c42d71889dc1f
Tobias <tobias@cynkra.com>
2019-11-01 10:49:25 +0100

    import.R

--- R/import.R
+++ R/import.R
@@ -5,7 +5,6 @@
 #' @rawNamespace import(tidyr, except = c(extract))
 #' @rawNamespace import(purrr, except = c(list_along, rep_along, invoke, modify, as_function, flatten_dbl, flatten_lgl, flatten_int, flatten_raw, flatten_chr, splice, flatten, prepend, `%@%`, `%||%`))
 #' @rawNamespace import(magrittr, except = c(set_names))
-#' @import datamodelr
 #' @importFrom methods is
 #' @import stringr
 #' @rawNamespace import(glue, except = c(collapse))
