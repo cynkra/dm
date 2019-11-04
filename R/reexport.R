8f1e61cdc537795884f837edd59a022ecb4de4ae
Tobias <tobias@cynkra.com>
2019-11-04 10:59:43 +0100

    doc and reexport

--- R/reexport.R
+++ R/reexport.R
@@ -24,3 +24,21 @@ dplyr::group_by
 
 #' @export
 dplyr::ungroup
+
+#' @export
+dplyr::left_join
+
+#' @export
+dplyr::inner_join
+
+#' @export
+dplyr::full_join
+
+#' @export
+dplyr::semi_join
+
+#' @export
+dplyr::anti_join
+
+#' @export
+dplyr::right_join
