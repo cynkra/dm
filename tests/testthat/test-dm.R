9e190e0f9c4e0a77daf914b2f2d134a2eabb5552
Tobias <tobias@cynkra.com>
2019-11-06 17:29:05 +0100

    tests

--- tests/testthat/test-dm.R
+++ tests/testthat/test-dm.R
@@ -23,3 +23,36 @@ test_that("creation of empty `dm` works", {
     is_empty(new_dm())
   )
 })
+
+test_that("'copy_to.dm()' works", {
+  expect_cdm_error(
+    copy_to(dm_for_filter, list(mtcars, iris)),
+    "one_name_for_each_table"
+  )
+
+  expect_cdm_error(
+    copy_to(dm_for_filter, letters[1:5], name = "letters"),
+    "only_data_frames_supported"
+  )
+
+  expect_equivalent_dm(
+    copy_to(dm_for_filter, mtcars, "car_table"),
+    cdm_add_tbl(dm_for_filter, car_table = mtcars)
+  )
+
+  # copying local `tibble` to postgres `dm`
+  skip_if_error(
+    expect_equivalent_dm(
+      copy_to(dm_for_filter_src$postgres, d1_src$df, "test_table"),
+      cdm_add_tbl(dm_for_filter_src$postgres, test_table = d1_src$postgres)
+      )
+  )
+
+  # copying list of postgres `tibbles` to local `dm`
+  skip_if_error(
+    expect_equivalent_dm(
+      copy_to(dm_for_filter_src$df, list(d1_src$postgres, d2_src$postgres), c("test_table_1", "test_table_2")),
+      cdm_add_tbl(dm_for_filter_src$df, test_table_1 = d1_src$df, test_table_2 = d2_src$df)
+    )
+  )
+})
