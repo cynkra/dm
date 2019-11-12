13bba44b69fc09c74d65e479a9549c76c7a48a67
Tobias <tobias@cynkra.com>
2019-11-12 12:03:34 +0100

    tests

--- tests/testthat/test-dm.R
+++ tests/testthat/test-dm.R
@@ -23,3 +23,15 @@ test_that("creation of empty `dm` works", {
     is_empty(new_dm())
   )
 })
+
+test_that("'compute.dm()' computes tables on DB", {
+    db_src_names <- setdiff(src_names, c("df"))
+    skip_if(is_empty(db_src_names))
+    walk(
+      db_src_names,
+      ~expect_true({
+          def <- dm_for_filter_src[[.x]] %>% cdm_filter(t1, a > 3) %>% compute() %>% cdm_get_def()
+          test <- map_chr(map(def$data, sql_render), as.character)
+          all(map_lgl(test, ~ !str_detect(., "WHERE")))})
+      )
+})
