cd7ce8fe25787b252a74b9740e6b35a44eae5aed
Tobias <tobias@cynkra.com>
2019-11-13 17:22:48 +0100

    migrate tests for tidyr methods

--- tests/testthat/test-dplyr.R
+++ tests/testthat/test-dplyr.R
@@ -1,6 +1,3 @@
-zoomed_dm <- cdm_zoom_to_tbl(dm_for_filter, t2)
-zoomed_dm_2 <- cdm_zoom_to_tbl(dm_for_filter, t3)
-
 # basic tests -------------------------------------------------------------
 
 
@@ -154,34 +151,6 @@ test_that("basic test: 'slice()'-methods work", {
     slice(dm_for_filter, 2),
     "no_table_zoomed_dplyr"
   )
-})
-
-test_that("basic test: 'unite()'-methods work", {
-  expect_identical(
-    unite(zoomed_dm, "new_col", c, e) %>% get_zoomed_tbl(),
-    unite(t2, "new_col", c, e)
-  )
-
-  expect_cdm_error(
-    unite(dm_for_filter),
-    "no_table_zoomed_dplyr"
-  )
-})
-
-test_that("basic test: 'separate()'-methods work", {
-    expect_identical(
-      unite(zoomed_dm, "new_col", c, e) %>%
-        separate("new_col", c("c", "e")) %>%
-        select(c, d, e) %>%
-        get_zoomed_tbl(),
-      t2
-    )
-
-    expect_cdm_error(
-      separate(dm_for_filter),
-      "no_table_zoomed_dplyr"
-    )
-
 })
 
 test_that("basic test: 'join()'-methods for `zoomed.dm` work", {
@@ -450,40 +419,6 @@ test_that("key tracking works", {
   expect_identical(
     arrange(zoomed_dm, e) %>% cdm_update_zoomed_tbl() %>% cdm_get_all_fks(),
     cdm_get_all_fks(dm_for_filter)
-  )
-
-  expect_identical(
-    unite(zoomed_dm, "new_col", c, e) %>% cdm_update_zoomed_tbl() %>% get_all_keys("t2"),
-    set_names("d")
-  )
-
-  expect_identical(
-    unite(zoomed_dm, "new_col", c, e, remove = FALSE) %>%
-      cdm_update_zoomed_tbl() %>%
-      get_all_keys("t2"),
-    set_names(c("c", "d", "e"))
-  )
-
-  expect_identical(
-    unite(zoomed_dm, "new_col", c, e, remove = FALSE) %>%
-      cdm_update_zoomed_tbl() %>%
-      cdm_add_fk(t2, new_col, t6) %>%
-      cdm_zoom_to_tbl(t2) %>%
-      separate(new_col, c("c", "e"), remove = TRUE) %>%
-      cdm_update_zoomed_tbl() %>%
-      get_all_keys("t2"),
-    set_names(c("c", "d", "e"))
-  )
-
-  expect_identical(
-    unite(zoomed_dm, "new_col", c, e, remove = FALSE) %>%
-      cdm_update_zoomed_tbl() %>%
-      cdm_add_fk(t2, new_col, t6) %>%
-      cdm_zoom_to_tbl(t2) %>%
-      separate(new_col, c("c", "e"), remove = FALSE) %>%
-      cdm_update_zoomed_tbl() %>%
-      get_all_keys("t2"),
-    set_names(c("c", "d", "e", "new_col"))
   )
 
   # keys tracking when there are no keys to track
