9905c70f9a74a887e6cf92dfd52ec61475502e44
Tobias <tobias@cynkra.com>
2019-11-25 14:40:44 +0100

    remove dependency of {stringr}

--- R/import.R
+++ R/import.R
@@ -6,7 +6,6 @@
 #' @rawNamespace import(purrr, except = c(list_along, rep_along, invoke, modify, as_function, flatten_dbl, flatten_lgl, flatten_int, flatten_raw, flatten_chr, splice, flatten, prepend, `%@%`, `%||%`))
 #' @rawNamespace import(magrittr, except = c(set_names))
 #' @importFrom methods is
-#' @import stringr
 #' @rawNamespace import(glue, except = c(collapse))
 #' @importFrom igraph V E
 NULL
