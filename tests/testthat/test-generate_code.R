4e5b841699c740979794fc9046ef7d77fd5e3fd6
Tobias <tobias@cynkra.com>
2019-11-26 11:15:34 +0100

    update tests

--- tests/testthat/test-generate_code.R
+++ tests/testthat/test-generate_code.R
@@ -14,8 +14,8 @@ test_that("generating code for creation of existing 'dm' works", {
     fixed = TRUE
   )
 
-  # `t1_new` does not exist in environment, so a message is issued
-  expect_message(
+  # `t1_new` does not exist in environment, so a warning is issued
+  expect_warning(
     expect_output(
       cdm_paste(dm_for_filter %>% cdm_rename_tbl(t1_new = t1)),
       paste0("dm(t1_new, t2, t3, t4, t5, t6) %>%\n  cdm_add_pk(t1_new, a) %>%\n  cdm_add_pk(t2, c) %>%",
@@ -27,19 +27,15 @@ test_that("generating code for creation of existing 'dm' works", {
   )
 
   # if `select = TRUE` it wants to compare column names, but it can't for `t1_new`, so skipping comparisons and warning about it
-  expect_message(
-    expect_warning(
-      expect_output(
-        cdm_paste(dm_for_filter %>% cdm_rename_tbl(t1_new = t1), select = TRUE),
-        paste0(
-          "dm(t1_new, t2, t3, t4, t5, t6) %>%\n  cdm_add_pk(t1_new, a) %>%\n  cdm_add_pk(t2, c) %>%",
-          "\n  cdm_add_pk(t3, f) %>%\n  cdm_add_pk(t4, h) %>%\n  cdm_add_pk(t5, k) %>%\n  ",
-          "cdm_add_pk(t6, n) %>%\n  cdm_add_fk(t2, d, t1_new) %>%\n  cdm_add_fk(t2, e, t3) %>%\n  ",
-          "cdm_add_fk(t4, j, t3) %>%\n  cdm_add_fk(t5, l, t4) %>%\n  cdm_add_fk(t5, m, t6) "
-        ),
-        fixed = TRUE
-      )
-    )
+  # in addition it still warns about not finding table `t1_new` (warnings[1])
+  warnings <- capture_warnings(expect_output(cdm_paste(dm_for_filter %>% cdm_rename_tbl(t1_new = t1), select = TRUE)))
+  expect_match(
+    warnings[1],
+    "The following tables do not exist"
+  )
+  expect_match(
+    warnings[2],
+    "Ignoring `select = TRUE`"
   )
 
   # produce `cdm_select()` statements in addition to the rest
@@ -60,7 +56,26 @@ test_that("generating code for creation of existing 'dm' works", {
              "cdm_add_fk(t4, j, t3) %>%\n  cdm_add_fk(t5, l, t4) %>%\n  cdm_add_fk(t5, m, t6) "),
       fixed = TRUE
     )
+  )
 
+  expect_warning(
+    expect_output(cdm_paste(dm_for_filter, env = new_environment())),
+    "The following tables do not exist",
+    fixed = TRUE
+  )
+
+  skip_if_not("postgres" %in% src_names)
+  expect_warning(
+    expect_output(
+      cdm_paste(dm_for_filter_src$postgres),
+      paste0("dm(t1, t2, t3, t4, t5, t6) %>%\n  cdm_add_pk(t1, a) %>%\n  cdm_add_pk(t2, c) %>%",
+             "\n  cdm_add_pk(t3, f) %>%\n  cdm_add_pk(t4, h) %>%\n  cdm_add_pk(t5, k) %>%\n  ",
+             "cdm_add_pk(t6, n) %>%\n  cdm_add_fk(t2, d, t1) %>%\n  cdm_add_fk(t2, e, t3) %>%\n  ",
+             "cdm_add_fk(t4, j, t3) %>%\n  cdm_add_fk(t5, l, t4) %>%\n  cdm_add_fk(t5, m, t6) "),
+      fixed = TRUE
+    ),
+    "Tables with the same names as",
+    fixed = TRUE
   )
 
 })
