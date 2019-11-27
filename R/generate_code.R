c413739d9acd9bad46c449fb2c79132ea828b018
Tobias <tobias@cynkra.com>
2019-11-27 10:34:09 +0100

    update doc

--- R/generate_code.R
+++ R/generate_code.R
@@ -7,11 +7,8 @@
 #'
 #' @details At the very least (if no keys exist in the given [`dm`]) a `dm()` statement is produced that -- when executed --
 #' produces the same `dm`. In addition, the code for setting the existing primary keys as well as the relations between the
-#' tables is produced. Should the tables not be available in the given environment, a warning is issued. The code won't work
-#' as is in this case. If `select = TRUE`, the column names of the `dm` tables are compared with the column names
-#' of the tables of the same name in the given environment. For each table:
-#' 1. If the `dm` tables have less columns and all of them exist in the tables in the given environment, appropriate `cdm_select()` statements are created
-#' 1. If the `dm` tables have extra columns, the table is skipped and a warning is issued.
+#' tables is produced. If `select = TRUE`, statements are included to select the respective columns of each table of the `dm` (useful if
+#' only a subset of the columns of the original tables is used for the `dm`).
 #'
 #' @return Code for producing the given `dm`
 #'
@@ -27,31 +24,27 @@ cdm_paste <- function(dm, select = FALSE) {
 
   if (select) {
     # adding code for selection of columns
-    code_select <- tibble(tbl_name = src_tbls(dm), tbls = cdm_get_tables(dm)) %>%
+    tbl_select <- tibble(tbl_name = src_tbls(dm), tbls = cdm_get_tables(dm)) %>%
       mutate(cols = map(tbls, colnames)) %>%
       mutate(code = map2_chr(
         tbl_name,
         cols,
-        ~glue("{paste0(' %>% \n')}  cdm_select({..1}, {paste0(tibble:::tick_if_needed(..2), collapse = ', ')})"))
-        ) %>%
-      summarize(code = glue_collapse(code)) %>%
-      pull()
-    code <- paste0(code, code_select)
+        ~glue("  cdm_select({..1}, {paste0(tibble:::tick_if_needed(..2), collapse = ', ')})"))
+        )
+    code_select <- if (nrow(tbl_select)) summarize(tbl_select, code = glue_collapse(code, sep = " %>%\n")) %>% pull() else character()
+    code <- glue_collapse(c(code, code_select), sep = " %>%\n")
   }
-
   # adding code for establishing PKs
-  code_pks <- cdm_get_all_pks(dm) %>%
-    mutate(code = glue("{paste0(' %>% \n')}  cdm_add_pk({table}, {pk_col})")) %>%
-    summarize(code = glue_collapse(code)) %>%
-    pull()
+  tbl_pks <- cdm_get_all_pks(dm) %>%
+    mutate(code = glue("  cdm_add_pk({table}, {pk_col})"))
+  code_pks <- if (nrow(tbl_pks)) summarize(tbl_pks, code = glue_collapse(code, sep = " %>%\n")) %>% pull() else character()
 
   # adding code for establishing FKs
-  code_fks <- cdm_get_all_fks(dm) %>%
-    mutate(code = glue("{paste0(' %>% \n')}  cdm_add_fk({child_table}, {child_fk_col}, {parent_table})")) %>%
-    summarize(code = glue_collapse(code)) %>%
-    pull()
+  tbl_fks <- cdm_get_all_fks(dm) %>%
+    mutate(code = glue("  cdm_add_fk({child_table}, {child_fk_col}, {parent_table})"))
+  code_fks <- if (nrow(tbl_fks)) summarize(tbl_fks, code = glue_collapse(code, sep = " %>%\n")) %>% pull() else character()
 
   # without "\n" in the end it looks weird when a warning is issued
-  cat(code, code_pks, code_fks)
+  cat(glue_collapse(c(code, code_pks, code_fks), sep = " %>%\n"))
   invisible(dm)
 }
