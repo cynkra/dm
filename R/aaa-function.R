b9b5b92d1ad1c881b30fc266c3ff0463ee3b603c
Kirill Müller <kirill@cynkra.com>
2019-12-02 09:02:35 +0100

    new_cdm_forward()

--- R/aaa-function.R
+++ R/aaa-function.R
@@ -1,3 +1,19 @@
 nse <- function(fun, env = caller_env()) {
   fun
 }
+
+new_cdm_forward <- function(fwd, env = caller_env()) {
+  fwd_sym <- ensym(fwd)
+  fwd_name <- as_name(fwd_sym)
+  old_fwd_name <- gsub("^dm_", "cdm_", fwd_name)
+
+  args <- formals(fwd)
+  fwd_args <- set_names(syms(names(args)), names(args))
+
+  body <- expr({
+    deprecate_soft("0.1.0", !!paste0("dm::", old_fwd_name, "()"))
+    (!!fwd_sym)(!!!fwd_args)
+  })
+
+  new_function(args, body, env)
+}
