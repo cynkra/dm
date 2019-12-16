21efbf361bafc960867109b59d089c29859bb315
Tobias <tobias@cynkra.com>
2019-12-16 16:21:46 +0100

    delete 'decompose_table()' and 'reunite_parent_child()'

--- R/table-surgery.R
+++ R/table-surgery.R
@@ -110,148 +110,29 @@ dm_separate_tbl <- function(dm, table, new_key_column, ..., new_table_name = NUL
     )
 }
 
-
-#' Decompose a table into two linked tables
-#'
-#' @description Perform table surgery by extracting a 'parent table' from a table, linking the original table and the new table by a key, and returning both tables.
-#'
-#' `decompose_table()` accepts a data frame, a name for the 'ID column' that will be newly created, and the names
-#' of the columns that will be extracted into the new data frame.
-#'
-#' It creates a 'parent table', which consists of the columns specified in the ellipsis, and a new 'ID column'.
-#' Then it removes those
-#' columns from the original table, which is now called the 'child table, and adds the 'ID column'.
-#'
-#' @param .data Data frame from which columns `...` are to be extracted.
-#' @param new_id_column Name of the identifier column (primary key column) for the parent table.
-#'   A column of this name is also added in 'child table'.
-#' @param ... The columns to be extracted from the `.data`.
-#'
-#'   One or more unquoted expressions separated by commas.
-#'   You can treat variable names as if they were positions, so you
-#'   can use expressions like x:y to select ranges of variables.
-#'
-#'   The arguments in ... are automatically quoted and evaluated in a context where column names represent column positions.
-#'   They also support
-#'   unquoting and splicing.
-#'   See vignette("programming") for an introduction to those concepts.
-#'
-#'   See select helpers for more details, and the examples about tidyselect helpers, such as starts_with(), everything(), ...
-#'
-#' @family table surgery functions
-#'
-#' @return A named list of length two:
-#'   - entry "child_table": the child table with column `new_id_column` referring to the same column in `parent_table`,
-#'   - entry "parent_table": the "lookup table" for `child_table`.
-#'
-#' @examples
-#' decomposed_table <- decompose_table(mtcars, new_id, am, gear, carb)
-#' decomposed_table$child_table
-#' decomposed_table$parent_table
-#' @export
-decompose_table <- function(.data, new_id_column, ...) {
-  table_name <- deparse(substitute(.data))
-  avail_cols <- colnames(.data)
-  id_col_q <- ensym(new_id_column)
-
-  if (as_string(id_col_q) %in% avail_cols) {
-    abort_dupl_new_id_col_name(table_name)
-  }
-
-  sel_vars <- tidyselect::vars_select(avail_cols, ...)
-
-  parent_table <-
-    select(.data, !!!sel_vars) %>%
-    distinct() %>%
-    # Without as.integer(), RPostgres creates integer64 column (#15)
-    arrange(!!!syms(names(sel_vars))) %>%
-    mutate(!!id_col_q := as.integer(row_number())) %>%
-    select(!!id_col_q, everything())
-
-  non_key_names <-
-    setdiff(avail_cols, sel_vars)
-
-  child_table <-
-    .data %>%
-    left_join(
-      parent_table,
-      by = prep_recode(sel_vars)
-    ) %>%
-    select(non_key_names, !!id_col_q)
-  # FIXME: Think about a good place for the target column,
-  # perhaps if this operation is run in a data model?
-
-  list("child_table" = child_table, "parent_table" = parent_table)
-}
-
 #' Merge two tables that are linked by a foreign key relation
 #'
-#' @description Perform table fusion by combining two tables by a common (key) column, and then removing this column.
+#' @description Join two tables together by their foreign key and update the
+#' `dm` accordingly. This is similar to `dm_join_to_tbl()`, only that the resulting
+#' table will be part of the `dm`. The name of the child table (the table with the
+#' foreign key relation pointing away from it) will be used for the resulting table.
 #'
-#' `reunite_parent_child()`: After joining the two tables by the column `id_column`, this column will be removed.
-#' The transformation is roughly the
-#' inverse of what `decompose_table()` does.
+#' Foreign key relations are kept whenever possible.
 #'
-#' @param child_table Table (possibly created by `decompose_table()`) that references `parent_table`
-#' @param parent_table Table (possibly created by `decompose_table()`).
-#' @param id_column Identical name of referencing / referenced column in `child_table`/`parent_table`.
+#' @inheritParams dm_add_pk
+#' @param table_1 A table of the `dm` that is directly linked to `table_2`
+#' @param table_2 A table of the `dm` that is directly linked to `table_1`
+#' @param rm_key_col Boolean, if `TRUE` (default), the FK column is removed after
+#' joining the two tables. Otherwise the key column of the child table is kept.
 #'
-#' @family table surgery functions
+#' @rdname dm_separate_tbl
 #'
-#' @return A wide table produced by joining the two given tables.
+#' @return The original `dm` with two of its tables merged into one.
 #'
 #' @examples
-#' decomposed_table <- decompose_table(mtcars, new_id, am, gear, carb)
-#' ct <- decomposed_table$child_table
-#' pt <- decomposed_table$parent_table
-#'
-#' reunite_parent_child(ct, pt, new_id)
-#' reunite_parent_child_from_list(decomposed_table, new_id)
-#' @name reunite_parent_child
+#' dm_nycflights13() %>%
+#'   dm_unite_tbls(flights, planes)
 #' @export
-reunite_parent_child <- function(child_table, parent_table, id_column) {
-  id_col_q <- ensym(id_column)
-
-  id_col_chr <-
-    as_name(id_col_q)
-
-  child_table %>%
-    left_join(parent_table, by = id_col_chr) %>%
-    select(-!!id_col_q)
-}
-
-#' Merge two tables that are linked by a foreign key relation
-#'
-#' @description `reunite_parent_child_from_list()`: After joining the two tables
-#' by the column `id_column`, `id_column` is removed.
-#'
-#' This function is almost exactly the inverse of `decompose_table()` (the order
-#' of the columns is not retained, and the original row names are lost).
-#'
-#' @param list_of_parent_child_tables Cf arguments `child_table` and `parent_table` from
-#'   `reunite_parent_child()`, but both in a named list (as created by `decompose_table()`).
-#'
-#' @family table surgery functions
-#'
-#' @rdname reunite_parent_child
-#' @export
-reunite_parent_child_from_list <- function(list_of_parent_child_tables, id_column) {
-  id_col_q <- ensym(id_column)
-
-  id_col_chr <-
-    as_name(id_col_q)
-
-  child_table <- list_of_parent_child_tables %>%
-    extract2("child_table")
-
-  parent_table <- list_of_parent_child_tables %>%
-    extract2("parent_table")
-
-  child_table %>%
-    left_join(parent_table, by = id_col_chr) %>%
-    select(-!!id_col_q)
-}
-
 dm_unite_tbls <- function(dm, table_1, table_2) {
   table_1_name <- as_string(ensym(table_1))
   table_2_name <- as_string(ensym(table_2))
--- tests/testthat/test-dm-table-surgery.R
+++ tests/testthat/test-dm-table-surgery.R
@@ -1,110 +1,21 @@
-decompose_table_data_ts_names <-
-  find_testthat_root_file(paste0("out/decompose-table-data-ts-", src_names, ".txt"))
-reunite_parent_child_data_ts_names <-
-  find_testthat_root_file(paste0("out/reunite-parent-child-data-ts-", src_names, ".txt"))
-reunite_parent_child_from_list_data_ts_names <-
-  find_testthat_root_file(paste0("out/reunite-parent-child_from_list-data-ts-", src_names, ".txt"))
-
-context("test-table-surgery")
-
-test_that("decompose_table() decomposes tables nicely on all sources?", {
-  walk2(
-    data_ts_src,
-    decompose_table_data_ts_names,
-    ~ expect_known_output(
-      print(decompose_table(.x, aef_id, a, e, f) %>% map(arrange_all)),
-      .y
-    )
-  )
-})
-
-test_that("decomposition works with {tidyselect}", {
-  pt_iris <- select(iris, starts_with("Sepal")) %>%
-    distinct() %>%
-    arrange(Sepal.Length, Sepal.Width) %>%
-    mutate(Sepal_id = row_number()) %>%
-    select(Sepal_id, everything())
-
-  ct_iris <- left_join(iris, pt_iris, by = c("Sepal.Length", "Sepal.Width")) %>%
-    select(-Sepal.Length, -Sepal.Width)
-
-  reference_flower_object <- list(
-    child_table = ct_iris,
-    parent_table = pt_iris
-  ) %>%
-    map(arrange_all)
-
-  expect_identical(
-    decompose_table(iris, Sepal_id, starts_with("Sepal")) %>% map(arrange_all),
-    reference_flower_object
-  )
-})
-
-test_that("reunite_parent_child() reunites parent and child nicely on all sources?", {
-  pwalk(
-    list(
-      data_ts_child_src, data_ts_parent_src, reunite_parent_child_data_ts_names
-    ),
-    ~ expect_known_output(
-      print(reunite_parent_child(..1, ..2, aef_id) %>% arrange_all()),
-      ..3
-    )
-  )
-})
-
-test_that("reunite_parent_child_from_list() reunites parent and child nicely on all sources?", {
-  walk2(
-    list_of_data_ts_parent_and_child_src,
-    reunite_parent_child_from_list_data_ts_names,
-    ~ expect_known_output(
-      print(reunite_parent_child_from_list(.x, aef_id) %>% arrange_all()),
-      .y
-    )
-  )
-})
-
-
-test_that("table surgery functions fail in the expected ways?", {
-  walk(
-    data_ts_src,
-    ~ expect_error(
-      decompose_table(., aex_id, a, e, x),
-      "."
-    )
-  )
-
-  walk(
-    data_ts_src,
-    ~ expect_dm_error(
-      decompose_table(., a, a, e, x),
-      class = "dupl_new_id_col_name"
-    )
-  )
-
-  walk(
-    data_ts_src,
-    function(data_ts) {
-      expect_equal(
-        decompose_table(data_ts, abcdef_id, a, b, c, d, e, f)$parent_table %>%
-          select(-abcdef_id) %>%
-          arrange_all() %>%
-          collect(),
-        data_ts %>%
-          arrange_all() %>%
-          collect()
-      )
-    }
-  )
-})
+iris_1_pt <-
+  iris_1 %>%
+  select(starts_with("Sepal"), Species) %>%
+  distinct() %>%
+  arrange(Sepal.Length, Sepal.Width, Species) %>%
+  mutate(Species_Sepal = row_number()) %>%
+  select(Species_Sepal, everything())
+iris_1_ct <-
+  left_join(iris_1, iris_1_pt, by = c("Species", "Sepal.Width", "Sepal.Length")) %>%
+  select(-Species, -Sepal.Width, -Sepal.Length)
 
 test_that("dm_separate_tbl() works", {
-  iris_1_dec <- decompose_table(iris_1, Species_Sepal, starts_with("Sepal"), Species)
 
   expect_equivalent_dm(
     dm_for_disambiguate %>% dm_separate_tbl(iris_1, Species_Sepal, starts_with("Sepal"), Species),
     dm_for_disambiguate %>%
       dm_rm_tbl(iris_1) %>%
-      dm_add_tbl(iris_1 = iris_1_dec$child_table, iris_1_lookup = iris_1_dec$parent_table) %>%
+      dm_add_tbl(iris_1 = iris_1_ct, iris_1_lookup = iris_1_pt) %>%
       dm_select_tbl(iris_1, iris_2, iris_3, iris_1_lookup) %>%
       dm_add_pk(iris_1_lookup, Species_Sepal) %>%
       dm_add_pk(iris_1, key) %>%
