9c5a2081d802d4d4705fa6ec10010fad3320b8b3
Tobias <tobias@cynkra.com>
2020-01-16 16:43:13 +0100

    upd 'expect_equivalent_dm()'

--- tests/testthat/helper-expectations.R
+++ tests/testthat/helper-expectations.R
@@ -9,8 +9,14 @@ expect_equivalent_dm <- function(dm1, dm2) {
   expect_identical(names(tables1), names(tables2))
   walk2(tables1, tables2, expect_equal)
 
-  expect_equal(dm_get_all_pks_impl(dm1), dm_get_all_pks_impl(dm2))
-  expect_equal(dm_get_all_fks_impl(dm1), dm_get_all_fks_impl(dm2))
+  expect_equal(
+    unnest(dm_get_all_pks_impl(dm1), pk_col),
+    unnest(dm_get_all_pks_impl(dm2), pk_col)
+    )
+  expect_equal(
+    unnest(dm_get_all_fks_impl(dm1), child_fk_col),
+    unnest(dm_get_all_fks_impl(dm2), child_fk_col)
+    )
 }
 
 expect_dm_error <- function(expr, class) {
