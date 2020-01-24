80d52c274e9ffcfacba6421a1e823bc607af1f53
Kirill Müller <kirill@cynkra.com>
2020-01-24 18:02:29 +0100

    Slow examples

--- tic.R
+++ tic.R
@@ -51,7 +51,9 @@ if (ci_has_env("TIC_ONLY_TESTS")) {
       add_step(step_push_deploy())
   }
 } else {
-  do_package_checks(error_on = if (getRversion() >= "3.4") "note" else "warning")
+  # Examples are slow:
+  #do_package_checks(error_on = if (getRversion() >= "3.4") "note" else "warning")
+  do_package_checks()
 
   if (ci_has_env("TIC_BUILD_PKGDOWN")) {
     do_pkgdown()
