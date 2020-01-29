1178fa6f2c557b0ba353cdbcf59ef8d2eb76b93f
Kirill Müller <kirill@cynkra.com>
2020-01-29 19:37:07 +0000

    Deploy from Travis build 1422 [ci skip]
    Build URL: https://travis-ci.org/krlmlr/dm/builds/643533920
    Commit: da8df2627178f21641996350c9352f33f94d18ef

--- R/tidyselect.R
+++ R/tidyselect.R
@@ -3,7 +3,6 @@ quo_select_table <- function(quo, table_names) {
 
   indexes <- tryCatch(
     tidyselect::eval_select(quo, table_names),
-
     vctrs_error_subscript = function(cnd) {
       # https://github.com/r-lib/vctrs/issues/786
       cnd$subscript_elt <- "element"
