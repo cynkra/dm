3e086b15bb356bf6d2e071c0cdc30444a66602ef
Tobias <tobias@cynkra.com>
2020-01-29 15:03:23 +0100

    rm link in doc

--- R/dm.R
+++ R/dm.R
@@ -21,7 +21,7 @@
 #' @seealso
 #'
 #' - [dm_add_pk()] and [dm_add_fk()] add primary and foreign keys
-#' - [copy_dm_to()] and [dm_learn_from_db()] for DB interaction
+#' - [copy_dm_to()] for DB interaction
 #' - [dm_draw()] for visualization
 #' - [dm_join_to_tbl()] for flattening
 #' - [dm_filter()] for filtering
