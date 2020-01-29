84723ac12adb127d83e7737adce2ee1e2d4bbbc5
Tobias <tobias@cynkra.com>
2020-01-29 14:54:46 +0100

    correct 'cdm_forward'

--- R/zzx-deprecated.R
+++ R/zzx-deprecated.R
@@ -203,8 +203,8 @@ cdm_get_referencing_tables <- new_cdm_forward_2(dm_get_referencing_tables)
 #' @keywords internal
 #' @export
 cdm_learn_from_db <- function(dest) {
-  deprecate_soft("0.1.0", "dm::cdm_learn_from_db()", "dm::dm_learn_from_db()")
-  dm_learn_from_db(dest)
+  deprecate_soft("0.1.0", "dm::cdm_learn_from_db()", "dm::dm_from_src()")
+  dm_from_src(dest)
 }
 
 #' @rdname deprecated
