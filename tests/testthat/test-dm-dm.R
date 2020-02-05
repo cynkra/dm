7e5081f13382a7834f4b7a490237a29c3d76506a
Tobias <tobias@cynkra.com>
2020-02-05 09:51:36 +0100

    tests

--- tests/testthat/test-dm-dm.R
+++ tests/testthat/test-dm-dm.R
@@ -344,3 +344,15 @@ test_that("as.list()-methods work", {
     as.list(t4)
   )
 })
+
+test_that("utils-methods work", {
+  expect_identical(
+    zoomed_dm %>% head(3) %>% get_zoomed_tbl(),
+    head(t2, 3)
+  )
+
+  expect_identical(
+    zoomed_dm %>% tail(2) %>% get_zoomed_tbl(),
+    tail(t2, 2)
+  )
+})
