64bbf9ccd8af108b193f54e91185c93b7923a2f7
Tobias <tobias@cynkra.com>
2020-02-06 17:54:08 +0100

    tests

--- tests/testthat/test-dm-dplyr.R
+++ tests/testthat/test-dm-dplyr.R
@@ -367,6 +367,59 @@ test_that("basic test: 'join()'-methods for `zoomed.dm` work", {
   )
 })
 
+test_that("'nest_join()'-method for `zoomed_dm` works", {
+  expect_identical(
+    zoomed_dm_2 %>%
+      nest_join() %>%
+      pull_tbl(),
+    t3 %>%
+      nest_join(t2, by = c("f" = "e")) %>%
+      mutate(t2 = vctrs::as_list_of(t2)) %>%
+      nest_join(t4, by = c("f" = "j")) %>%
+      mutate(t4 = vctrs::as_list_of(t4))
+  )
+
+  expect_identical(
+    zoomed_dm_2 %>%
+      nest_join(t4, t2) %>%
+      pull_tbl(),
+    t3 %>%
+      nest_join(t4, by = c("f" = "j")) %>%
+      mutate(t4 = vctrs::as_list_of(t4)) %>%
+      nest_join(t2, by = c("f" = "e")) %>%
+      mutate(t2 = vctrs::as_list_of(t2))
+  )
+
+    expect_message(
+      expect_equivalent_dm(
+        dm_rm_pk(dm_for_filter, t3, TRUE) %>% dm_zoom_to(t3) %>% nest_join(),
+        dm_rm_pk(dm_for_filter, t3, TRUE) %>% dm_zoom_to(t3)
+      ),
+      "didn't have a primary key"
+    )
+
+    expect_dm_error(
+      zoomed_dm_2 %>% select(g) %>% nest_join(),
+      "pk_not_tracked"
+    )
+})
+
+test_that("'nest_join.zoomed_dm()' fails for Postgres-'dm'", {
+  skip_if_not("postgres" %in% src_names)
+  expect_dm_error(
+    dm_zoom_to(dm_for_filter_src$postgres, t3) %>% nest_join(),
+    "only_for_local_src"
+  )
+})
+
+test_that("'nest_join.zoomed_dm()' fails for SQLite-'dm'", {
+  skip_if_not("sqlite" %in% src_names)
+  expect_dm_error(
+    dm_zoom_to(dm_for_filter_src$sqlite, t3) %>% nest_join(),
+    "only_for_local_src"
+  )
+})
+
 test_that("basic test: 'join()'-methods for `dm` throws error", {
   expect_dm_error(
     left_join(dm_for_filter),
@@ -395,6 +448,11 @@ test_that("basic test: 'join()'-methods for `dm` throws error", {
 
   expect_dm_error(
     right_join(dm_for_filter),
+    "only_possible_w_zoom"
+  )
+
+  expect_dm_error(
+    nest_join(dm_for_filter),
     "only_possible_w_zoom"
   )
 })
