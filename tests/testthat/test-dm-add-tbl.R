8e82c1c2f65249a8ff6db574796a861717a73810
Tobias <tobias@cynkra.com>
2020-02-11 09:15:06 +0100

    repair tests

--- tests/testthat/test-dm-add-tbl.R
+++ tests/testthat/test-dm-add-tbl.R
@@ -118,9 +118,9 @@ test_that("dm_rm_tbl() works", {
   )
 
   # fails when table name is wrong
-  expect_dm_error(
+  expect_error(
     dm_rm_tbl(dm_for_filter, t7),
-    "w_message"
+    class = "vctrs_error_subscript"
   )
 
   # select-helpers work for 'dm_rm_tbl()'
