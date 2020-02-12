67cadffa797da85be333fc7565c6f08d31bc5c15
Tobias <tobias@cynkra.com>
2020-02-12 12:10:37 +0100

    test 'quo_get_table_indices()'

--- tests/testthat/test-dm-tidyselect.R
+++ tests/testthat/test-dm-tidyselect.R
@@ -3,6 +3,11 @@ quo <- quo(c(table_3_new = table_3, table_1_new = table_1))
 
 test_that("tidyselecting tables works", {
   expect_identical(
+    quo_get_table_indices(quo, table_names),
+    set_names(c(3L, 1L), c("table_3_new", "table_1_new"))
+  )
+
+  expect_identical(
     quo_select_table(quo, table_names),
     set_names(c("table_3", "table_1"), c("table_3_new", "table_1_new"))
   )
