c63d5c0d7a182b3eae2249e3d08005a19186c2ab
Tobias <tobias@cynkra.com>
2020-02-12 11:39:00 +0100

    tests for 'quo_select_table()' and 'quo_rename_table()'

---
+++ tests/testthat/test-dm-tidyselect.R
@@ -0,0 +1,14 @@
+table_names <- c("table_1", "table_2", "table_3")
+quo <- quo(c(table_3_new = table_3, table_1_new = table_1))
+
+test_that("tidyselecting tables works", {
+  expect_identical(
+    quo_select_table(quo, table_names),
+    set_names(c("table_3", "table_1"), c("table_3_new", "table_1_new"))
+  )
+
+  expect_identical(
+    quo_rename_table(quo, table_names),
+    set_names(table_names, c("table_1_new", "table_2", "table_3_new"))
+  )
+})
