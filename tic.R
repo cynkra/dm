8a2e862d189dde17e0685b9c7d0d073c7be3ed7d
Kirill Müller <kirill@cynkra.com>
2020-02-13 14:01:58 +0100

    Print session info

--- tic.R
+++ tic.R
@@ -1,3 +1,9 @@
+get_stage("before_script") %>%
+  add_code_step({
+    pkgload::load_all()
+    sessioninfo::session_info()
+  })
+
 if (ci_has_env("TIC_DEV_VERSIONS")) {
   get_stage("install") %>%
     add_step(step_install_github(upgrade = "always", c(
