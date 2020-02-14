b1c9f776f99137c3322f9993723e89040a93b367
Kirill Müller <kirill@cynkra.com>
2020-02-14 02:15:17 +0100

    Remove dead code, add output tests
    Closes #278.

--- tests/testthat/test-output.R
+++ tests/testthat/test-output.R
@@ -1,5 +1,17 @@
 test_that("output", {
   verify_output("out/output.txt", {
+    dm_nycflights13(cycle = TRUE)
+
+    dm_nycflights13(cycle = TRUE) %>%
+      dm_filter(flights, origin == "EWR")
+
+    dm_nycflights13(cycle = TRUE) %>%
+      str()
+
+    dm_nycflights13(cycle = TRUE) %>%
+      dm_zoom_to(airlines) %>%
+      str()
+
     dm_nycflights13(cycle = TRUE) %>%
       dm_examine_constraints() %>%
       as_tibble()
