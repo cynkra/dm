86bddc7b3eb55601158780ef09ecea09cc54ddb7
Kirill Müller <kirill@cynkra.com>
2020-02-18 22:32:50 +0100

    Tweak tests

--- tests/testthat/test-dm-paste.R
+++ tests/testthat/test-dm-paste.R
@@ -1,16 +1,28 @@
 test_that("generating code for creation of existing 'dm' works", {
-  verify_output("out/code_from_paste.txt",
-      {dm_paste(empty_dm())
-        dm_paste(dm_for_filter)
-        # changing the tab width
-        dm_paste(dm_for_filter, FALSE, 4)
-        # we don't care if the tables really exist
-        dm_paste(dm_for_filter %>% dm_rename_tbl(t1_new = t1))
-        # produce `dm_select()` statements in addition to the rest
-        dm_paste(dm_select(dm_for_filter, t5, k = k, m) %>% dm_select(t1, a), select = TRUE)
-        # produce code with colors
-        # FIXME: this leads to weirdness in 'out/code_from_paste.txt'
-        dm_set_colors(dm_for_filter, "#A0BB55" = t1:t3, "darkgreen" = t5:t6) %>%
-          dm_paste(tab_width = 4)
-    })
+  verify_output("out/code_from_paste.txt", {
+    "empty"
+    empty_dm() %>% dm_paste()
+
+    "baseline"
+    dm_for_filter %>% dm_paste()
+
+    "changing the tab width"
+    dm_for_filter %>% dm_paste(tab_width = 4)
+
+    "we don't care if the tables really exist"
+    dm_for_filter %>%
+      dm_rename_tbl(t1_new = t1) %>%
+      dm_paste()
+
+    "produce `dm_select()` statements in addition to the rest"
+    dm_for_filter %>%
+      dm_select(t5, k = k, m) %>%
+      dm_select(t1, a) %>%
+      dm_paste(select = TRUE)
+
+    "produce code with colors"
+    dm_for_filter %>%
+      dm_set_colors("orange" = t1:t3, "darkgreen" = t5:t6) %>%
+      dm_paste(tab_width = 4)
+  })
 })
