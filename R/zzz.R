893cbb3a40ff44fc52b99977d135c7b871f8f13f
Kirill Müller <kirill@cynkra.com>
2020-04-11 07:17:38 +0200

    cache_attach()

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname) {
   backports::import(pkgname, c("...length"))
 
-  dm_financial <<- memoise::memoise(dm_financial)
-  dm_financial_sqlite <<- memoise::memoise(dm_financial_sqlite)
+  dm_financial <<- memoise::memoise(dm_financial, cache = cache_attach())
+  dm_financial_sqlite <<- memoise::memoise(dm_financial_sqlite, cache = cache_attach())
 }
