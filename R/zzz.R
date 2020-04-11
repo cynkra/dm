b628e40d4a22dd5ea00092b19995dd8ca608fbc3
Kirill Müller <kirill@cynkra.com>
2020-04-11 07:16:48 +0200

    - New `dm_financial()`.

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
 .onLoad <- function(libname, pkgname) {
   backports::import(pkgname, c("...length"))
 
+  dm_financial <<- memoise::memoise(dm_financial)
   dm_financial_sqlite <<- memoise::memoise(dm_financial_sqlite)
 }
