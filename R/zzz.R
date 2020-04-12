b4b52b45fac4a24b0227b110d0fc6837f5c7b423
Kirill Müller <kirill@cynkra.com>
2020-04-12 06:58:29 +0200

    memoise only for recent R?

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,8 @@
 .onLoad <- function(libname, pkgname) {
   backports::import(pkgname, c("...length"))
 
-  dm_financial <<- memoise::memoise(dm_financial, cache = cache_attach())
-  dm_financial_sqlite <<- memoise::memoise(dm_financial_sqlite, cache = cache_attach())
+  if (getRversion() >= "3.4") {
+    dm_financial <<- memoise::memoise(dm_financial, cache = cache_attach())
+    dm_financial_sqlite <<- memoise::memoise(dm_financial_sqlite, cache = cache_attach())
+  }
 }
