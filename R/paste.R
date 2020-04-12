e0219462d32e036e4c98382499416dea44673892
Kirill Müller <kirill@cynkra.com>
2020-04-12 17:21:51 +0200

    Refactor
    - Every function should fit one page of code
    - All code in a function should have the same level of abstraction
    
    Principles from "Clean code", CC @TSchiefer

--- R/paste.R
+++ R/paste.R
@@ -38,43 +38,64 @@ dm_paste_impl <- function(dm, select, tab_width, color) {
   check_not_zoomed(dm)
   check_no_filter(dm)
 
-  # we assume the tables exist and have the necessary columns
-  # code for including the tables
-  code <- glue("dm({paste(tick_if_needed({src_tbls(dm)}), collapse = ', ')})")
   tab <- paste0(rep(" ", tab_width), collapse = "")
 
-  if (select) {
-    # adding code for selection of columns
-    tbl_select <- tibble(tbl_name = src_tbls(dm), tbls = dm_get_tables_impl(dm)) %>%
-      mutate(cols = map(tbls, colnames)) %>%
-      mutate(code = map2_chr(
-        tbl_name,
-        cols,
-        ~ glue("{tab}dm_select({..1}, {paste0(tick_if_needed(..2), collapse = ', ')})")
-      ))
-    code_select <- if (nrow(tbl_select)) summarize(tbl_select, code = glue_collapse(code, sep = " %>%\n")) %>% pull() else character()
-    code <- glue_collapse(c(code, code_select), sep = " %>%\n")
-  }
+  # we assume the tables exist and have the necessary columns
+  # code for including the tables
+  code_dm <- dm_paste_dm(dm)
+
+  # adding code for selection of columns
+  code_select <- if (select) dm_paste_select(dm)
 
   # adding code for establishing PKs
-  # FIXME: this will fail with compound keys
-  tbl_pks <- dm_get_all_pks_impl(dm) %>%
-    mutate(code = glue("{tab}dm_add_pk({table}, {pk_col})"))
-  code_pks <- if (nrow(tbl_pks)) summarize(tbl_pks, code = glue_collapse(code, sep = " %>%\n")) %>% pull() else character()
+  code_pks <- dm_paste_pks(dm)
 
   # adding code for establishing FKs
+  code_fks <- dm_paste_fks(dm)
+
+  # adding code for color
+  code_color <- if (color) dm_paste_color(dm)
+
+  glue_collapse(c(code_dm, code_select, code_pks, code_fks, code_color), sep = glue(" %>%\n{tab}", .trim = FALSE))
+}
+
+dm_paste_dm <- function(dm) {
+  glue("dm({glue_collapse1(tick_if_needed(src_tbls(dm)), ', ')})")
+}
+
+dm_paste_select <- function(dm) {
+  tbl_select <- dm %>%
+    dm_get_def() %>%
+    mutate(cols = map(data, colnames)) %>%
+    mutate(cols = map_chr(cols, ~ glue_collapse1(glue(", {tick_if_needed(.x)}")))) %>%
+    mutate(code = glue("dm_select({tick_if_needed(table)}{cols})")) %>%
+    pull()
+}
+
+dm_paste_pks <- function(dm) {
+  # FIXME: this will fail with compound keys
+  dm_get_all_pks_impl(dm) %>%
+    mutate(code = glue("dm_add_pk({table}, {pk_col})")) %>%
+    pull()
+}
+
+dm_paste_fks <- function(dm) {
   # FIXME: this will fail with compound keys
   tbl_fks <- dm_get_all_fks_impl(dm) %>%
-    mutate(code = glue("{tab}dm_add_fk({child_table}, {child_fk_cols}, {parent_table})"))
-  code_fks <- if (nrow(tbl_fks)) summarize(tbl_fks, code = glue_collapse(code, sep = " %>%\n")) %>% pull() else character()
+    mutate(code = glue("dm_add_fk({child_table}, {child_fk_cols}, {parent_table})")) %>%
+    pull()
+}
 
-  if (color) {
-    colors <- dm_get_colors(dm)
-    colors <- colors[names(colors) != "default"]
-    code_color <- imap_chr(colors, ~ glue("{tab}dm_set_colors({tick_if_needed(..2)} = {tick_if_needed(..1)})"))
+dm_paste_color <- function(dm) {
+  colors <- dm_get_colors(dm)
+  colors <- colors[names(colors) != "default"]
+  imap_chr(colors, ~ glue("dm_set_colors({tick_if_needed(..2)} = {tick_if_needed(..1)})"))
+}
+
+glue_collapse1 <- function(x, ...) {
+  if (is_empty(x)) {
+    ""
   } else {
-    code_color <- character()
+    glue_collapse(x, ...)
   }
-
-  glue_collapse(c(code, code_color, code_pks, code_fks), sep = " %>%\n")
 }
