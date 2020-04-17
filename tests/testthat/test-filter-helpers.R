ef7c9a11319006d43a93f08b1da3a78978cc87a2
Tobias <tobias@cynkra.com>
2020-04-17 12:25:56 +0200

    only my_test_src-tests

--- tests/testthat/test-filter-helpers.R
+++ tests/testthat/test-filter-helpers.R
@@ -3,10 +3,4 @@ test_that("dm_nrow() works?", {
     as.integer(sum(dm_nrow(dm_test_obj))),
     rows_dm_obj
   )
-
-  # FIXME: maybe always include this test on a DB, despite PR #313?
-  expect_identical(
-    as.integer(sum(dm_nrow(dm_test_obj_sqlite))),
-    rows_dm_obj
-  )
 })
