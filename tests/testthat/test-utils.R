ad93cb5f9de1c4a9a81e17b9af53e19e886d9733
Tobias <tobias@cynkra.com>
2020-04-17 16:06:07 +0200

    update tests for utils

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,10 +1,10 @@
 test_that("utils-methods work", {
-  expect_identical(
+  expect_equivalent_tbl(
     zoomed_dm %>% head(3) %>% get_zoomed_tbl(),
     head(t2, 3)
   )
 
-  expect_identical(
+  expect_equivalent_tbl(
     zoomed_dm %>% tail(2) %>% get_zoomed_tbl(),
     tail(t2, 2)
   )
