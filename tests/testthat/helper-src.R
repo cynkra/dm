5fc42f4dcd4edc5ee6c2a1debcdbf7e27acaa417
Tobias <tobias@cynkra.com>
2020-04-22 17:40:24 +0200

    unused, unnecessary

--- tests/testthat/helper-src.R
+++ tests/testthat/helper-src.R
@@ -364,8 +364,6 @@ dm_for_disambiguate %<-% {
     dm_add_fk(iris_2, key, iris_1)
 }
 
-dm_for_disambiguate_sqlite %<-% copy_dm_to(sqlite(), dm_for_disambiguate())
-
 dm_for_disambiguate_2 %<-% {
   as_dm(list(iris_1 = iris_1_dis(), iris_2 = iris_2_dis(), iris_3 = iris_3_dis())) %>%
     dm_add_pk(iris_1, key) %>%
@@ -496,8 +494,6 @@ dm_nycflights_small %<-% {
     dm_add_fk(flights, carrier, airlines) %>%
     dm_add_fk(flights, dest, airports)
 }
-
-dm_nycflights_small_sqlite %<-% copy_dm_to(sqlite(), dm_nycflights_small())
 
 zoomed_dm <- function() dm_zoom_to(dm_for_filter(), tf_2)
 zoomed_dm_2 <- function() dm_zoom_to(dm_for_filter(), tf_3)
