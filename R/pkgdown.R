93fb53b26cd0db46800dbf68c5984bcbfceb8bd2
Kirill Müller <kirill@cynkra.com>
2020-04-23 20:15:06 +0200

    dm_draw() works again after load_all()

--- R/pkgdown.R
+++ R/pkgdown.R
@@ -1,7 +1,17 @@
-replay_html.dm_draw <- function(x, ...) {
-  unclass(x)
-}
+register_pkgdown_methods <- function() {
+  if (Sys.getenv("IN_PKGDOWN") == "") {
+    return()
+  }
 
-print.grViz <- function(x, ...) {
-  structure(DiagrammeRsvg::export_svg(x), class = "dm_draw")
+  replay_html.dm_draw <- function(x, ...) {
+    unclass(x)
+  }
+
+  print.grViz <- function(x, ...) {
+    structure(DiagrammeRsvg::export_svg(x), class = "dm_draw")
+  }
+
+  stopifnot(rlang::is_installed(c("DiagrammeR", "DiagrammeRsvg")))
+  vctrs::s3_register("pkgdown::replay_html", "dm_draw")
+  vctrs::s3_register("base::print", "grViz")
 }
--- R/zzz.R
+++ R/zzz.R
@@ -9,9 +9,5 @@
   flights_subset <<- memoise::memoise(flights_subset, cache = cache_attach())
   weather_subset <<- memoise::memoise(weather_subset, cache = cache_attach())
 
-  if (Sys.getenv("IN_PKGDOWN") != "") {
-    stopifnot(rlang::is_installed(c("DiagrammeR", "DiagrammeRsvg")))
-    vctrs::s3_register("pkgdown::replay_html", "dm_draw")
-    vctrs::s3_register("base::print", "grViz")
-  }
+  register_pkgdown_methods()
 }
