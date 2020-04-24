878211faddf53bd9c8c7ad16a12af381ae08c6a1
Kirill Müller <kirill@cynkra.com>
2020-04-24 04:25:21 +0200

    Methods need to live in namespace

--- R/pkgdown.R
+++ R/pkgdown.R
@@ -1,17 +1,18 @@
+replay_html_dm_draw <- function(x, ...) {
+  unclass(x)
+}
+
+# Use custom name to avoid clash if pkgload is active
+print_grViz <- function(x, ...) {
+  structure(DiagrammeRsvg::export_svg(x), class = "dm_draw")
+}
+
 register_pkgdown_methods <- function() {
   if (Sys.getenv("IN_PKGDOWN") == "") {
     return()
   }
 
-  replay_html.dm_draw <- function(x, ...) {
-    unclass(x)
-  }
-
-  print.grViz <- function(x, ...) {
-    structure(DiagrammeRsvg::export_svg(x), class = "dm_draw")
-  }
-
   stopifnot(rlang::is_installed(c("DiagrammeR", "DiagrammeRsvg")))
-  vctrs::s3_register("pkgdown::replay_html", "dm_draw")
-  vctrs::s3_register("base::print", "grViz")
+  vctrs::s3_register("pkgdown::replay_html", "dm_draw", replay_html_dm_draw)
+  vctrs::s3_register("base::print", "grViz", print_grViz)
 }
