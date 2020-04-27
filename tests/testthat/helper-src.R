a7836e0c85f6861e32803476c092c3834ea7c406
Tobias <tobias@cynkra.com>
2020-04-27 14:37:47 +0000

    Deploy from Travis build 1740 [ci skip]
    Build URL: https://travis-ci.org/krlmlr/dm/builds/680126911
    Commit: c3078c2f4953ed749b15c075634e7937f457e1ad

--- tests/testthat/helper-src.R
+++ tests/testthat/helper-src.R
@@ -48,7 +48,7 @@ copy_to_my_test_src <- function(rhs, lhs) {
     # FIXME: this way we need to skip all tests with nycflights
     copy_dm_to(src, rhs, unique_table_names = TRUE)
   } else if (inherits(rhs, "list")) {
-    map(rhs, ~copy_to(src, .x, name = unique_db_table_name(name), temporary = TRUE))
+    map(rhs, ~ copy_to(src, .x, name = unique_db_table_name(name), temporary = TRUE))
   } else {
     copy_to(src, rhs, name = name, temporary = TRUE)
   }
