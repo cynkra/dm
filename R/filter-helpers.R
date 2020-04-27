826a0fe0362f95dd54874c8ca609373b99edae09
Kirill Müller <kirill@cynkra.com>
2020-04-27 14:25:32 +0200

    dm_flatten_to_tbl()

--- R/filter-helpers.R
+++ R/filter-helpers.R
@@ -22,11 +22,11 @@ dm_nrow <- function(dm) {
 
 get_by <- function(dm, lhs_name, rhs_name) {
   if (dm_has_fk_impl(dm, lhs_name, rhs_name)) {
-    lhs_col <- dm_get_fk_impl(dm, lhs_name, rhs_name)
-    rhs_col <- dm_get_pk_impl(dm, rhs_name)
+    lhs_col <- dm_get_fk2_impl(dm, lhs_name, rhs_name)
+    rhs_col <- dm_get_pk2_impl(dm, rhs_name)
   } else if (dm_has_fk_impl(dm, rhs_name, lhs_name)) {
-    lhs_col <- dm_get_pk_impl(dm, lhs_name)
-    rhs_col <- dm_get_fk_impl(dm, rhs_name, lhs_name)
+    lhs_col <- dm_get_pk2_impl(dm, lhs_name)
+    rhs_col <- dm_get_fk2_impl(dm, rhs_name, lhs_name)
   } else {
     abort_tables_not_neighbours(lhs_name, rhs_name)
   }
@@ -34,8 +34,8 @@ get_by <- function(dm, lhs_name, rhs_name) {
   if (length(lhs_col) > 1 || length(rhs_col) > 1) abort_no_cycles()
   # Construct a `by` argument of the form `c("lhs_col[1]" = "rhs_col[1]", ...)`
   # as required by `*_join()`
-  by <- rhs_col
-  names(by) <- lhs_col
+  by <- rhs_col[[1]]
+  names(by) <- lhs_col[[1]]
   by
 }
 
--- tests/testthat/test-filter-helpers.R
+++ tests/testthat/test-filter-helpers.R
@@ -4,3 +4,10 @@ test_that("dm_nrow() works?", {
     rows_dm_obj
   )
 })
+
+verify_output("out/filter-helpers-compound.txt", {
+  dm_nycflights13() %>%
+    dm_add_pk(weather, c(origin, time_hour)) %>%
+    dm_add_fk(flights, c(origin, time_hour), weather) %>%
+    dm_flatten_to_tbl()
+})
