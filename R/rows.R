9841c76c36f933868292071b1b5faf146dd63995
Kirill Müller <kirill@cynkra.com>
2020-04-30 05:01:49 +0200

    Import from dplyr

--- R/rows.R
+++ R/rows.R
@@ -1,260 +1,265 @@
 #' Manipulate individual rows
 #'
 #' @description
-#' \lifecycle{experimental}
+#' \Sexpr[results=rd, stage=render]{lifecycle::badge("experimental")}
 #'
-#' These methods provide a framework for manipulating individual rows
-#' in existing tables, modeled after the SQL operations
-#' `INSERT`, `UPDATE` and `DELETE`.
-#' All operations expect existing and new data to be compatible.
+#' These functions provide a framework for modifying rows in a table using
+#' a second table of data. The two tables are matched `by` a set of key
+#' variables whose values must uniquely identify each row. The functions are
+#' inspired by SQL's `INSERT`, `UPDATE`, and `DELETE`, and can optionally
+#' modify `in_place` for selected backends.
+#'
+#' * `rows_insert()` adds new rows (like `INSERT`); key values in `y` must
+#'    not occur in `x`.
+#' * `rows_update()` modifies existing rows (like `UPDATE`); key values in
+#'   `y` must occur in `x`.
+#' * `rows_patch()` works like `rows_update()` but only overwrites `NA` values.
+#' * `rows_upsert()` inserts or updates depending on whether or not the
+#'   key value in `y` already exists in `x`.
+#' * `rows_delete()` deletes rows (like `DELETE`); key values in `y` must
+#'   exist in `x`.
 #'
 #' @inheritParams left_join
-#' @param x Target table object.
-#' @param y Source table object. All columns in `y` must exist in `x`.
-#' @param by Key columns as unnamed character vector.
-#'   All columns in `by` must exist in `y` (and, by extension, in `x`).
-#'   The default is the first column of `y`.
-#' @param inplace
-#'   This argument is only relevant for mutable backends,
-#'   e.g. databases or \pkg{dtplyr}.
-#'   For data frames, these operations always return a modified copy
-#'   of the data.
-#'   An informative message is given if set to `TRUE`.
+#' @param x,y A pair of data frames or data frame extensions (e.g. a tibble).
+#'   `y` must have the same columns of `x` or a subset.
+#' @param by An unnamed character vector giving the key columns. The key
+#'   values must uniquely identify each row (i.e. each combination of key
+#'   values occurs at most once), and the key columns must exist in both `x`
+#'   and `y`.
 #'
-#'   For mutable backends, set to `FALSE` for running the operation
-#'   without updating the data in place.
-#'   In this mode, a modified version of `x` is returned, as for data frames.
-#'   This allows verifying the results of an operation before actually
-#'   applying it.
-#'   Set to `TRUE` to perform the update on the remote table.
-#'   The default is `FALSE` with an informative message.
+#'   By default, we use the first column in `y`, since the first column is
+#'   a reasonable place to put an identifier variable.
+#' @param in_place Should `x` be modified in place? This argument is only
+#'   relevant for mutable backends (e.g. databases, data.tables).
 #'
-#' @return A tbl object of the same structure as `x`.
-#'   On mutable backends, same as `x` if `inplace = TRUE`.
+#'   When `TRUE`, a modified version of `x` is returned invisibly;
+#'   when `FALSE`, a new object representing the resulting changes is returned.
+#' @returns
+#' An object of the same type as `x`. The order of the rows and columns of `x`
+#' is preserved as much as possible. The output has the following properties:
 #'
+#' * `rows_update()` preserves rows as is; `rows_insert()` and `rows_upsert()`
+#'   return all existing rows and potentially new rows; `rows_delete()` returns
+#'   a subset of the rows.
+#' * Columns are not added, removed, or relocated, though the data may be updated.
+#' * Groups are taken from `x`.
+#' * Data frame attributes are taken from `x`.
+#'
+#' If `in_place = TRUE`, the result will be returned invisibly.
 #' @name rows
-#' @example example/rows-d.R
+#' @examples
+#' data <- tibble(a = 1:3, b = letters[c(1:2, NA)], c = 0.5 + 0:2)
+#' data
+#'
+#' # Insert
+#' rows_insert(data, tibble(a = 4, b = "z"))
+#' try(rows_insert(data, tibble(a = 3, b = "z")))
+#'
+#' # Update
+#' rows_update(data, tibble(a = 2:3, b = "z"))
+#' rows_update(data, tibble(b = "z", a = 2:3), by = "a")
+#'
+#' # Variants: patch and upsert
+#' rows_patch(data, tibble(a = 2:3, b = "z"))
+#' rows_upsert(data, tibble(a = 2:4, b = "z"))
+#'
+#' # Delete and truncate
+#' rows_delete(data, tibble(a = 2:3))
+#' rows_delete(data, tibble(a = 2:3, b = "b"))
+#' try(rows_delete(data, tibble(a = 2:3, b = "b"), by = c("a", "b")))
 NULL
 
 
-#' rows_insert
-#'
-#' `rows_insert()` adds new rows.
-#' This operation corresponds to `INSERT` in SQL.
-#' If `by` is non-empty, no two rows with the same values in the key columns
-#' are permitted.
 #' @rdname rows
 #' @export
-rows_insert <- function(x, y, by = NULL, ..., copy = FALSE, inplace = NULL) {
-  check_rows_args(x, y, by)
+rows_insert <- function(x, y, by = NULL, ..., copy = FALSE, in_place = FALSE) {
+  ellipsis::check_dots_used()
   UseMethod("rows_insert")
 }
 
 #' @export
-rows_insert.data.frame <- function(x, y, by = NULL, ..., copy = FALSE, inplace = NULL) {
+rows_insert.data.frame <- function(x, y, by = NULL, ..., copy = FALSE, in_place = FALSE) {
+  key <- rows_check_key(by, x, y)
   y <- auto_copy(x, y, copy = copy)
-  y_key <- df_key(y, by)
-  x_key <- df_key(x, names(y_key))
-  df_inplace(inplace)
+  rows_df_in_place(in_place)
 
-  if (has_length(y_key)) {
-    idx <- vctrs::vec_match(y[y_key], x[x_key])
-    bad <- which(!is.na(idx))
-    if (has_length(bad)) {
-      abort(
-        class = "dplyr_rows_insert_duplicate",
-        "Attempting to insert duplicate rows.",
-        location = bad
-      )
-    }
+  rows_check_key_df(x, key, df_name = "x")
+  rows_check_key_df(y, key, df_name = "y")
+
+  idx <- vctrs::vec_match(y[key], x[key])
+  bad <- which(!is.na(idx))
+  if (has_length(bad)) {
+    abort(class = "dplyr_rows_insert_duplicate",
+      "Attempting to insert duplicate rows.",
+      location = bad
+    )
   }
 
-  vctrs::vec_rbind(x, y)
+  rows_bind(x, y)
 }
 
-#' rows_update
-#'
-#' `rows_update()` updates existing rows.
-#' This operation corresponds to `UPDATE` in SQL.
-#' `by` is mandatory and defaults to the first column in `y`.
-#' No two rows with the same values may exist in the new data.
-#' Each row in the new data must have exactly one corresponding row
-#' in the existing data.
 #' @rdname rows
 #' @export
-rows_update <- function(x, y, by = NULL, ..., copy = FALSE, inplace = NULL) {
-  check_rows_args(x, y, by)
+rows_update <- function(x, y, by = NULL, ..., copy = FALSE, in_place = FALSE) {
+  ellipsis::check_dots_used()
   UseMethod("rows_update", x)
 }
 
 #' @export
-rows_update.data.frame <- function(x, y, by = NULL, ..., copy = FALSE, inplace = NULL) {
+rows_update.data.frame <- function(x, y, by = NULL, ..., copy = FALSE, in_place = FALSE) {
+  key <- rows_check_key(by, x, y)
   y <- auto_copy(x, y, copy = copy)
-  y_key <- df_key(y, by)
-  x_key <- df_key(x, names(y_key))
-  df_inplace(inplace)
+  rows_df_in_place(in_place)
 
-  idx <- vctrs::vec_match(y[y_key], x[x_key])
-  # FIXME: Check key in x? https://github.com/r-lib/vctrs/issues/1032
+  rows_check_key_df(x, key, df_name = "x")
+  rows_check_key_df(y, key, df_name = "y")
+  idx <- vctrs::vec_match(y[key], x[key])
+
+  bad <- which(is.na(idx))
+  if (has_length(bad)) {
+    abort(class = "dplyr_rows_update_missing",
+      "Attempting to update missing rows.",
+      location = bad
+    )
+  }
+
   x[idx, names(y)] <- y
   x
 }
 
-#' rows_patch
-#'
-#' `rows_patch()` replaces missing values in existing rows.
-#' This operation corresponds to `UPDATE` using `COALESCE` expressions in SQL.
-#' It is similar to `rows_update()`, leaves non-missing values untouched.
-#' `by` is mandatory and defaults to the first column in `y`.
-#' No two rows with the same values may exist in the new data.
-#' Each row in the new data must have exactly one corresponding row
-#' in the existing data.
 #' @rdname rows
 #' @export
-rows_patch <- function(x, y, by = NULL, ..., copy = FALSE, inplace = NULL) {
-  check_rows_args(x, y, by)
+rows_patch <- function(x, y, by = NULL, ..., copy = FALSE, in_place = FALSE) {
+  ellipsis::check_dots_used()
   UseMethod("rows_patch", x)
 }
 
 #' @export
-rows_patch.data.frame <- function(x, y, by = NULL, ..., copy = FALSE, inplace = NULL) {
+rows_patch.data.frame <- function(x, y, by = NULL, ..., copy = FALSE, in_place = FALSE) {
+  key <- rows_check_key(by, x, y)
   y <- auto_copy(x, y, copy = copy)
-  y_key <- df_key(y, by)
-  x_key <- df_key(x, names(y_key))
-  df_inplace(inplace)
+  rows_df_in_place(in_place)
 
-  idx <- vctrs::vec_match(y[y_key], x[x_key])
-  # FIXME: Check key in x? https://github.com/r-lib/vctrs/issues/1032
+  rows_check_key_df(x, key, df_name = "x")
+  rows_check_key_df(y, key, df_name = "y")
+  idx <- vctrs::vec_match(y[key], x[key])
 
-  # FIXME: Do we need vec_coalesce()
+  bad <- which(is.na(idx))
+  if (has_length(bad)) {
+    abort(class = "dplyr_rows_patch_missing",
+      "Attempting to patch missing rows.",
+      location = bad
+    )
+  }
+
   new_data <- map2(x[idx, names(y)], y, coalesce)
 
   x[idx, names(y)] <- new_data
   x
 }
 
-#' rows_upsert
-#'
-#' `rows_upsert()` updates matching rows and adds new rows for mismatches.
-#' This operation corresponds to `INSERT ON DUPLICATE KEY UPDATE` or
-#' `INSERT ON CONFLICT` in some SQL variants.
-#' `by` is mandatory and defaults to the first column in `y`.
-#' No two rows with the same values may exist in the new data.
-#' Each row in the new data must have exactly one corresponding row
-#' in the existing data.
 #' @rdname rows
 #' @export
-rows_upsert <- function(x, y, by = NULL, ..., copy = FALSE, inplace = NULL) {
-  check_rows_args(x, y, by)
+rows_upsert <- function(x, y, by = NULL, ..., copy = FALSE, in_place = FALSE) {
+  ellipsis::check_dots_used()
   UseMethod("rows_upsert", x)
 }
 
 #' @export
-rows_upsert.data.frame <- function(x, y, by = NULL, ..., copy = FALSE, inplace = NULL) {
+rows_upsert.data.frame <- function(x, y, by = NULL, ..., copy = FALSE, in_place = FALSE) {
+  key <- rows_check_key(by, x, y)
   y <- auto_copy(x, y, copy = copy)
-  y_key <- df_key(y, by)
-  x_key <- df_key(x, names(y_key))
-  df_inplace(inplace)
+  rows_df_in_place(in_place)
 
-  idx <- vctrs::vec_match(y[y_key], x[x_key])
-  # FIXME: Check key in x?
-
+  rows_check_key_df(x, key, df_name = "x")
+  rows_check_key_df(y, key, df_name = "y")
+  idx <- vctrs::vec_match(y[key], x[key])
   new <- is.na(idx)
   idx_existing <- idx[!new]
   idx_new <- idx[new]
 
-  x[idx_existing, names(y)] <- y[!new, ]
-  vctrs::vec_rbind(x, y[new, ])
+  x[idx_existing, names(y)] <- vec_slice(y, !new)
+  rows_bind(x, vec_slice(y, new))
 }
 
-#' rows_delete
-#'
-#' `rows_delete()` deletes existing rows.
-#' This operation corresponds to `DELETE` in SQL.
-#' `by` is mandatory and defaults to the first column in `y`.
 #' @rdname rows
 #' @export
-rows_delete <- function(x, y, by = NULL, ..., copy = FALSE, inplace = NULL) {
-  check_rows_args(x, y, by)
+rows_delete <- function(x, y, by = NULL, ..., copy = FALSE, in_place = FALSE) {
+  ellipsis::check_dots_used()
   UseMethod("rows_delete", x)
 }
 
 #' @export
-rows_delete.data.frame <- function(x, y, by = NULL, ..., copy = FALSE, inplace = NULL) {
+rows_delete.data.frame <- function(x, y, by = NULL, ..., copy = FALSE, in_place = FALSE) {
+  key <- rows_check_key(by, x, y)
   y <- auto_copy(x, y, copy = copy)
-  y_key <- df_key(y, by)
-  x_key <- df_key(x, names(y_key))
-  df_inplace(inplace)
+  rows_df_in_place(in_place)
 
-  idx <- vctrs::vec_match(y[y_key], x[x_key])
-  # FIXME: Check key in x? https://github.com/r-lib/vctrs/issues/1032
-  x[-idx[!is.na(idx)], ]
+  rows_check_key_df(x, key, df_name = "x")
+  rows_check_key_df(y, key, df_name = "y")
+
+  extra_cols <- setdiff(names(y), key)
+  if (has_length(extra_cols)) {
+    inform(glue("Ignoring extra columns: ", commas(tick_if_needed(extra_cols))),
+      class = c("dplyr_message_delete_extra_cols", "dplyr_message")
+    )
+  }
+
+  idx <- vctrs::vec_match(y[key], x[key])
+
+  bad <- which(is.na(idx))
+  if (has_length(bad)) {
+    abort(class = "dplyr_rows_delete_missing",
+      "Attempting to delete missing rows.",
+      location = bad
+    )
+  }
+
+  dplyr_row_slice(x, -idx)
 }
 
-#' rows_truncate
-#'
-#' `rows_truncate()` removes all rows.
-#' This operation corresponds to `TRUNCATE` in SQL.
-#' `...` is ignored.
-#' @rdname rows
-#' @export
-rows_truncate <- function(x, ..., copy = FALSE, inplace = NULL) {
-  UseMethod("rows_truncate", x)
-}
+# helpers -----------------------------------------------------------------
 
-#' @export
-rows_truncate.data.frame <- function(x, ..., copy = FALSE, inplace = NULL) {
-  df_inplace(inplace)
+rows_check_key <- function(by, x, y) {
+  if (is.null(by)) {
+    by <- names(y)[[1]]
+    inform(glue("Matching, by = \"{by}\""),
+      class = c("dplyr_message_matching_by", "dplyr_message")
+    )
+  }
 
-  x[integer(), ]
-}
+  if (!is.character(by) || length(by) == 0) {
+    abort("`by` must be a character vector.")
+  }
+  # is_named(by) checks all(names2(by) != ""), we need any(...)
+  if (any(names2(by) != "")) {
+    abort("`by` must be unnamed.")
+  }
 
-
-
-check_rows_args <- function(x, y, by) {
-  check_col_subset(y, x)
-  check_by(by, y)
-}
-
-check_col_subset <- function(y, x) {
   bad <- setdiff(colnames(y), colnames(x))
   if (has_length(bad)) {
-    abort(
-      class = "dplyr_rows_extra_column",
-      "All columns in `y` must exist in `x`.",
-      name = bad
-    )
+    abort("All columns in `y` must exist in `x`.")
+  }
+
+  by
+}
+
+rows_check_key_df <- function(df, by, df_name) {
+  y_miss <- setdiff(by, colnames(df))
+  if (length(y_miss) > 0) {
+    abort(glue("All `by` columns must exist in `{df_name}`."))
+  }
+  if (vctrs::vec_duplicate_any(df[by])) {
+    abort(glue("`{df_name}` key values are not unique"))
   }
 }
 
-check_by <- function(by, y) {
-  if (!is.null(by) && !is.character(by)) {
-    abort("`by` must be `NULL` or a character vector.")
-  }
-  if (!is.null(names(by)) || any(names(by) != "")) {
-    abort("`by` must not have names.")
-  }
-  bad <- setdiff(by, colnames(y))
-  if (has_length(bad)) {
-    abort(
-      class = "dplyr_rows_extra_column",
-      "All `by` columns must exist in `y`.",
-      name = bad
-    )
+rows_df_in_place <- function(in_place) {
+  if (is_true(in_place)) {
+    abort("Data frames only support `in_place = FALSE`")
   }
 }
 
-df_key <- function(y, by) {
-  if (is.null(by)) {
-    set_names(1L, names(y)[[1]])
-  } else {
-    idx <- match(by, names(y))
-    set_names(idx, by)
-  }
-}
-
-df_inplace <- function(inplace) {
-  if (is_true(inplace)) {
-    inform("`rows_...()` ignore `inplace` ignored for data frames.")
-  }
+rows_bind <- function(x, y) {
+  dplyr_reconstruct(vctrs::vec_rbind(x, y), x)
 }
