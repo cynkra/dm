c1167273ecefd8ea144e27e6bf4d163fd7041d28
Kirill Müller <kirill@cynkra.com>
2020-05-25 14:22:48 +0200

    Qualify

--- example/rows-db.R
+++ example/rows-db.R
@@ -1,11 +1,11 @@
-data <- memdb_frame(a = 1:3, b = letters[c(1:2, NA)], c = 0.5 + 0:2)
+data <- dbplyr::memdb_frame(a = 1:3, b = letters[c(1:2, NA)], c = 0.5 + 0:2)
 data
 
 try(rows_insert(data, tibble(a = 4, b = "z")))
 rows_insert(data, tibble(a = 4, b = "z"), copy = TRUE)
 rows_update(data, tibble(a = 2:3, b = "w"), copy = TRUE, in_place = FALSE)
 
-rows_insert(data, memdb_frame(a = 4, b = "z"), in_place = TRUE)
+rows_insert(data, dbplyr::memdb_frame(a = 4, b = "z"), in_place = TRUE)
 data
-rows_update(data, memdb_frame(a = 2:3, b = "w"), in_place = TRUE)
+rows_update(data, dbplyr::memdb_frame(a = 2:3, b = "w"), in_place = TRUE)
 data
