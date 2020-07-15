b2ef7d10318fe739b89300ee3ccd8752f453c281
Tobias <tobias@cynkra.com>
2020-07-15 13:51:29 +0200

    use table name repair as similar to dm_add_tbl()

--- R/merge.R
+++ R/merge.R
@@ -3,8 +3,9 @@
 #' Create a single `dm` from one or more `dm`-objects.
 #'
 #' @param ... `dm`-objects to merge. If left empty, an error will be thrown.
+#' @inheritParams dm_add_tbl
 #'
-#' @details Table names need to be unique.
+#' @details By default table names need to be unique.
 #'
 #' @return `dm` containing the tables and key relations of all `dm`-objects.
 #' @export
@@ -13,19 +14,19 @@
 #' dm_1 <- dm_nycflights13()
 #' dm_2 <- dm(mtcars, iris)
 #' dm_merge(dm_1, dm_2)
-dm_merge <- function(...) {
+dm_merge <- function(..., repair = "check_unique", quiet = FALSE) {
   if (dots_n(...) == 0) abort_empty_ellipsis("dm_merge()")
   dms <- list2(...)
+
   walk(dms, check_dm)
   walk(dms, check_not_zoomed)
 
   table_names <- map(dms, src_tbls) %>% flatten_chr()
-  if (anyDuplicated(table_names)) abort_need_unique_names(
-    table_names[duplicated(table_names)]
-  )
+  new_table_names <- repair_names_vec(table_names, repair, quiet)
 
   dms_def <- map(dms, dm_get_def)
   reduce(dms_def, bind_rows) %>%
+    mutate(table = new_table_names) %>%
     new_dm3()
 }
 
--- tests/testthat/test-merge.R
+++ tests/testthat/test-merge.R
@@ -5,11 +5,11 @@ test_that("dm_merge() works?", {
   )
 
   expect_equivalent_dm(
-    dm_merge(dm_for_filter(), dm_for_flatten(), dm_for_filter()),
+    dm_merge(dm_for_filter(), dm_for_flatten(), dm_for_disambiguate()),
     bind_rows(
       dm_get_def(dm_for_filter()),
       dm_get_def(dm_for_flatten()),
-      dm_get_def(dm_for_filter())
+      dm_get_def(dm_for_disambiguate())
     ) %>%
       new_dm3()
   )
@@ -28,8 +28,33 @@ test_that("are empty_dm() handled correctly?", {
 })
 
 test_that("error if empty ellipsis", {
-  expect_error(
-    dm_merge(),
-    class = dm_error_full("empty_ellipsis")
+  expect_dm_error(dm_merge(), "empty_ellipsis")
+})
+
+test_that("by default error if duplicate table names", {
+  expect_dm_error(dm_merge(dm_for_filter(), dm_for_flatten(), dm_for_filter()), "need_unique_names")
+})
+
+test_that("auto-renaming works", {
+  expect_equivalent_dm(
+    expect_message(
+      dm_merge(dm_for_filter(), dm_for_flatten(), dm_for_filter(), repair = "unique"),
+      "New names"
+      ),
+    bind_rows(
+      dm_get_def(dm_for_filter()),
+      dm_get_def(dm_for_flatten()),
+      dm_get_def(dm_for_filter())
+    ) %>%
+      mutate(table = c(
+        "tf_1...1", "tf_2...2", "tf_3...3", "tf_4...4", "tf_5...5", "tf_6...6",
+        "fact", "dim_1", "dim_2", "dim_3", "dim_4",
+        "tf_1...12", "tf_2...13", "tf_3...14", "tf_4...15", "tf_5...16", "tf_6...17")
+      ) %>%
+      new_dm3()
+  )
+
+  expect_silent(
+    dm_merge(dm_for_filter(), dm_for_flatten(), dm_for_filter(), repair = "unique", quiet = TRUE)
   )
 })
