d900de809fbab1e0e2723b7a73bdeb8cccc7cc7f
Tobias <tobias@cynkra.com>
2020-07-15 11:47:27 +0200

    implement 'dm_merge()'

---
+++ R/merge.R
@@ -0,0 +1,19 @@
+dm_merge <- function(...) {
+  if (dots_n(...) == 0) abort_empty_ellipsis("dm_merge")
+  dms <- list2(...)
+  walk(dms, check_dm)
+  walk(dms, check_not_zoomed)
+  dms_def <- map(dms, dm_get_def)
+  reduce(dms_def, bind_rows) %>%
+    new_dm3()
+}
+
+
+# error handling ----------------------------------------------------------
+
+abort_empty_ellipsis <- function(function_name) {
+  abort(
+    glue("No argument provided for `...` in {tick(function_name)}."),
+    class = dm_error_full("empty_ellipsis")
+  )
+}
