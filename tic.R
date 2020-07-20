ccba6b21477e272f84a9b781b5d34d32d29e07b0
Kirill Müller <kirill@cynkra.com>
2020-07-20 07:26:42 +0200

    Build with pkgdown branch

--- tic.R
+++ tic.R
@@ -71,6 +71,7 @@ if (ci_has_env("TIC_DEV_VERSIONS")) {
   }
 } else if (ci_has_env("TIC_BUILD_PKGDOWN")) {
   get_stage("install") %>%
+    add_step(step_install_github("krlmlr/pkgdown@fix/examples-dontshow")) %>%
     add_step(step_install_github("cynkra/cynkratemplate"))
 
   do_pkgdown()
