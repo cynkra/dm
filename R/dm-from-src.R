cd65ca9f42dddc6d1265ade72b548c7d79818fb7
Tobias <tobias@cynkra.com>
2020-07-29 18:25:57 +0200

    add schema argument

--- R/dm-from-src.R
+++ R/dm-from-src.R
@@ -40,7 +40,7 @@
 #' dm_from_src(con)
 #'
 #' DBI::dbDisconnect(con)
-dm_from_src <- function(src = NULL, table_names = NULL, learn_keys = NULL,
+dm_from_src <- function(src = NULL, table_names = NULL, learn_keys = NULL, schema = NULL,
                         ...) {
   if (is_null(src)) {
     # FIXME: Check empty arguments and ellipsis
@@ -51,7 +51,8 @@ dm_from_src <- function(src = NULL, table_names = NULL, learn_keys = NULL,
   con <- con_from_src_or_con(src)
 
   if (is.null(learn_keys) || isTRUE(learn_keys)) {
-    dm_learned <- dm_learn_from_db(src, ...)
+    if (is.null(schema)) schema <- db_standard_schema(src)
+    dm_learned <- dm_learn_from_db(src, schema = schema, ...)
 
     if (is.null(dm_learned)) {
       if (isTRUE(learn_keys)) {
