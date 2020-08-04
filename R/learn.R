ab7f4a949fb475ba9765d3d303c7981be4aac232
Tobias Schieferdecker <tobias@cynkra.com>
2020-08-04 11:58:08 +0200

    use existing DBI::dbQuoteString()
    Co-authored-by: Kirill Müller <krlmlr@users.noreply.github.com>

--- R/learn.R
+++ R/learn.R
@@ -126,7 +126,7 @@ mssql_learn_query <- function(con, schema = "dbo") { # taken directly from {data
   order by
     tabs.create_date,
     cols.column_id",
-  gsub("(^|[^'])(['])($|[^'])", "\\1\\2\\2\\3", schema)
+  DBI::dbQuoteString(con, schema)
   )
 }
 
