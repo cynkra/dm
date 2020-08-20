6a43436b7e4dc8aa4709fd90e496f6156b29be98
Tobias <tobias@cynkra.com>
2020-08-20 09:20:04 +0200

    comp-tests for dm_filter()

--- tests/testthat/test-filter-dm.R
+++ tests/testthat/test-filter-dm.R
@@ -137,3 +137,19 @@ test_that("dm_filter() fails when no table name is provided", {
     class = "table_missing"
   )
 })
+
+
+# tests for compound keys -------------------------------------------------
+
+verify_output(
+  "out/compound-filter.txt", {
+  nyc_comp() %>%
+    dm_filter(flights, sched_dep_time <= 1200) %>%
+    dm_apply_filters() %>%
+    dm_nrow()
+  nyc_comp() %>%
+    dm_filter(weather, pressure < 1020) %>%
+    dm_apply_filters() %>%
+    dm_nrow()
+  }
+)
