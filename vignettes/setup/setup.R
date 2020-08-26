6688153386c7b37c2495eed2b79e34ece8998568
Kirill Müller <kirill@cynkra.com>
2020-08-26 08:25:14 +0200

    Avoid pkgdown error if relational.fit is down

--- vignettes/setup/setup.R
+++ vignettes/setup/setup.R
@@ -1,5 +1,5 @@
 knitr::opts_chunk$set(
-  error = (Sys.getenv("IN_PKGDOWN") == ""),
+  error = (Sys.getenv("IN_PKGDOWN") == "") && dm_has_financial(),
   message = TRUE,
   collapse = TRUE,
   comment = "#>"
