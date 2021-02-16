c0593e27056b83f09081294a9a49f683e7c6a1ea
Tobias <tobias@cynkra.com>
2021-02-16 16:25:04 +0100

    add first tests for schema handling for MSSQL & Postgres

---
+++ tests/testthat/test-schema.R
@@ -0,0 +1,78 @@
+test_that("schema handling on MSSQL and Postgres works", {
+  skip_if_src_not(c("mssql", "postgres"))
+  # dm_learn_from_mssql() --------------------------------------------------
+  src_db <- my_test_src()
+  con_db <- src_db$con
+
+  expect_dm_error(sql_schema_exists(con_db, 1), "parameter_not_correct_class")
+  expect_dm_error(sql_schema_exists(con_db, letters[1:2]), "parameter_not_correct_length")
+  expect_dm_error(sql_schema_exists(src_db, 1), "parameter_not_correct_class")
+  expect_dm_error(sql_schema_exists(src_db, letters[1:2]), "parameter_not_correct_length")
+
+  expect_false(sql_schema_exists(con_db, "dm_schema_test_schema"))
+  expect_false(sql_schema_exists(src_db, "dm_schema_test_schema"))
+
+  # create a table in the default schema
+  expect_message(sql_schema_create(con_db, "dm_schema_test_schema"), "created")
+  expect_dm_error(sql_schema_create(con_db, "dm_schema_test_schema"), "schema_exists")
+  expect_true(sql_schema_exists(con_db, "dm_schema_test_schema"))
+  expect_message(sql_schema_drop(con_db, "dm_schema_test_schema"), "dropped")
+  expect_dm_error(sql_schema_drop(con_db, "dm_schema_test_schema"), "no_schema_exists")
+  expect_false(sql_schema_exists(con_db, "dm_schema_test_schema"))
+
+  expect_message(sql_schema_create(src_db, "dm_schema_test_schema"), "created")
+  expect_true(sql_schema_exists(src_db, "dm_schema_test_schema"))
+  expect_message(sql_schema_drop(src_db, "dm_schema_test_schema"), "dropped")
+  expect_false(sql_schema_exists(src_db, "dm_schema_test_schema"))
+
+  expect_false("test_schema_1" %in% sql_schema_table_list(con_db)$table_name)
+  expect_false("test_schema_1" %in% sql_schema_table_list(src_db)$table_name)
+
+  withr::defer({
+      try(dbExecute(con_db, "DROP TABLE [dbo].[test_schema_1]"))
+      try(dbExecute(con_db, "DROP TABLE [dm_schema_test_schema].[test_schema_2]"))
+      try(dbExecute(con_db, "DROP SCHEMA dm_schema_test_schema"))
+  })
+
+  dbWriteTable(
+    con_db,
+    DBI::Id(table = "test_schema_1"),
+    value = tibble(a = 1:5)
+  )
+
+  expect_true("test_schema_1" %in% sql_schema_table_list(con_db)$table_name)
+  expect_true("test_schema_1" %in% sql_schema_table_list(src_db)$table_name)
+
+  remote_table_1 <- filter(sql_schema_table_list(src_db), table_name == "test_schema_1") %>%
+    pull(remote_table_name)
+  expect_identical(
+    tbl(src_db, remote_table_1) %>% collect(),
+    tibble(a = 1:5)
+  )
+
+  expect_message(sql_schema_create(src_db, "dm_schema_test_schema"), "created")
+
+  dbWriteTable(
+    con_db,
+    DBI::Id(schema = "dm_schema_test_schema", table = "test_schema_2"),
+    value = tibble(b = letters[1:5])
+  )
+
+  expect_identical(
+    sql_schema_table_list(con_db, schema = "dm_schema_test_schema"),
+    tibble(
+      table_name = "test_schema_2",
+      remote_table_name = dbplyr::ident_q("\"dm_schema_test_schema\".\"test_schema_2\"")
+    )
+  )
+
+  remote_table_2 <- filter(
+    sql_schema_table_list(src_db, schema = "dm_schema_test_schema"),
+    table_name == "test_schema_2"
+  ) %>%
+    pull(remote_table_name)
+  expect_identical(
+    tbl(src_db, remote_table_2) %>% collect(),
+    tibble(b = letters[1:5])
+  )
+})
