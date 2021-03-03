a359c5140e4b438961b3432f97d38f97026f554c
Tobias <tobias@cynkra.com>
2021-03-03 11:04:25 +0100

    move functions to be exported

--- R/read-write.R
+++ R/read-write.R
@@ -13,89 +13,6 @@ dm_write_csv <- function(dm, csv_directory) {
   )
 }
 
-prepare_tables <- function(dm) {
-  check_param_class(dm, "dm")
-  check_not_zoomed(dm, -3)
-  check_no_filter(dm, -3)
-  if (is_empty(dm)) {abort_empty_dm()}
-
-  if (is_db(dm_get_src(dm))) {
-    abort_only_for_local_src(dm_get_src(dm))
-  }
-
-  info_tibble <- dm_get_def(dm) %>% select(table, segment, display)
-
-  col_class_tibble <- dm_col_class(dm)
-  if ("list" %in% col_class_tibble$class) {
-    tbl_col_list <- filter(col_class_tibble, class == "list")
-    abort_no_list(tbl_col_list$table, tbl_col_list$column)
-  }
-  if ("datetime" %in% col_class_tibble$class |
-      "POSIXct" %in% col_class_tibble$class |
-      "POSIXlt" %in% col_class_tibble$class) {
-    tbl_dt_list <- filter(
-      col_class_tibble,
-      class == "datetime" |
-        class == "POSIXct" |
-        class == "POSIXlt"
-    )
-    message(
-      glue::glue(
-        "Columns containing datetimes need to be converted to TZ `UTC` to avoid errors, consider checking columns:\n",
-        "{paste0('Table: ', tick(tbl_dt_list$table), ', column: ', tick(tbl_dt_list$column), collapse = '\n')}"
-      )
-    )
-  }
-
-  # FIXME: might need to revisit for compound keys
-  pk_tibble <- dm_get_all_pks_impl(dm)
-
-  # not using dm_get_all_fks_impl(), because it will break with introduction of compound keys
-  # FIXME: might need to revisit for compound keys
-  fk_tibble <- dm_get_def(dm) %>%
-    select("child_table" = table, fks) %>%
-    unnest(fks) %>%
-    rename(parent_table = table, fk_col = column) %>%
-    # key number is needed in case of compound keys
-    # FIXME: referenced column of PK then needs to be given too
-    mutate(key_nr = row_number()) %>%
-    unnest(fk_col)
-
-  list(
-    info_tibble = info_tibble,
-    col_class_tibble = col_class_tibble,
-    pk_tibble = pk_tibble,
-    fk_tibble = fk_tibble
-  )
-}
-
-dm_write_csv_impl <- function(dm, csv_directory, zip) {
-  csv_tables <- prepare_tables(dm)
-
-  if (!dir.exists(csv_directory)) {
-    dir.create(csv_directory)
-  }
-
-  csv_info_filename <- file.path(csv_directory, "___info_file_dm.csv")
-  csv_coltypes_filename <- file.path(csv_directory, "___coltypes_file_dm.csv")
-  csv_pk_filename <- file.path(csv_directory, "___pk_file_dm.csv")
-  csv_fk_filename <- file.path(csv_directory, "___fk_file_dm.csv")
-  csv_table_filenames <- file.path(csv_directory, paste0(src_tbls(dm), ".csv"))
-
-  readr::write_csv(csv_tables$info_tibble, csv_info_filename)
-  readr::write_csv(csv_tables$col_class_tibble, csv_coltypes_filename)
-  readr::write_csv(csv_tables$pk_tibble, csv_pk_filename)
-  readr::write_csv(csv_tables$fk_tibble, csv_fk_filename)
-
-  walk2(dm_get_tables_impl(dm), csv_table_filenames, readr::write_csv)
-  if (!zip) {
-    message(
-      glue::glue("Written `dm` as csv-files to the directory {tick(csv_directory)}.")
-    )
-  }
-  invisible(csv_directory)
-}
-
 dm_read_csv <- function(csv_directory) {
   file_list <- list.files(csv_directory)
   special_files <- c(
@@ -227,6 +144,89 @@ dm_read_zip <- function(zip_file_path) {
   utils::unzip(zip_file_path, exdir = unzip_directory)
 
   dm_read_csv(unzip_directory)
+}
+
+prepare_tables <- function(dm) {
+  check_param_class(dm, "dm")
+  check_not_zoomed(dm, -3)
+  check_no_filter(dm, -3)
+  if (is_empty(dm)) {abort_empty_dm()}
+
+  if (is_db(dm_get_src(dm))) {
+    abort_only_for_local_src(dm_get_src(dm))
+  }
+
+  info_tibble <- dm_get_def(dm) %>% select(table, segment, display)
+
+  col_class_tibble <- dm_col_class(dm)
+  if ("list" %in% col_class_tibble$class) {
+    tbl_col_list <- filter(col_class_tibble, class == "list")
+    abort_no_list(tbl_col_list$table, tbl_col_list$column)
+  }
+  if ("datetime" %in% col_class_tibble$class |
+      "POSIXct" %in% col_class_tibble$class |
+      "POSIXlt" %in% col_class_tibble$class) {
+    tbl_dt_list <- filter(
+      col_class_tibble,
+      class == "datetime" |
+        class == "POSIXct" |
+        class == "POSIXlt"
+    )
+    message(
+      glue::glue(
+        "Columns containing datetimes need to be converted to TZ `UTC` to avoid errors, consider checking columns:\n",
+        "{paste0('Table: ', tick(tbl_dt_list$table), ', column: ', tick(tbl_dt_list$column), collapse = '\n')}"
+      )
+    )
+  }
+
+  # FIXME: might need to revisit for compound keys
+  pk_tibble <- dm_get_all_pks_impl(dm)
+
+  # not using dm_get_all_fks_impl(), because it will break with introduction of compound keys
+  # FIXME: might need to revisit for compound keys
+  fk_tibble <- dm_get_def(dm) %>%
+    select("child_table" = table, fks) %>%
+    unnest(fks) %>%
+    rename(parent_table = table, fk_col = column) %>%
+    # key number is needed in case of compound keys
+    # FIXME: referenced column of PK then needs to be given too
+    mutate(key_nr = row_number()) %>%
+    unnest(fk_col)
+
+  list(
+    info_tibble = info_tibble,
+    col_class_tibble = col_class_tibble,
+    pk_tibble = pk_tibble,
+    fk_tibble = fk_tibble
+  )
+}
+
+dm_write_csv_impl <- function(dm, csv_directory, zip) {
+  csv_tables <- prepare_tables(dm)
+
+  if (!dir.exists(csv_directory)) {
+    dir.create(csv_directory)
+  }
+
+  csv_info_filename <- file.path(csv_directory, "___info_file_dm.csv")
+  csv_coltypes_filename <- file.path(csv_directory, "___coltypes_file_dm.csv")
+  csv_pk_filename <- file.path(csv_directory, "___pk_file_dm.csv")
+  csv_fk_filename <- file.path(csv_directory, "___fk_file_dm.csv")
+  csv_table_filenames <- file.path(csv_directory, paste0(src_tbls(dm), ".csv"))
+
+  readr::write_csv(csv_tables$info_tibble, csv_info_filename)
+  readr::write_csv(csv_tables$col_class_tibble, csv_coltypes_filename)
+  readr::write_csv(csv_tables$pk_tibble, csv_pk_filename)
+  readr::write_csv(csv_tables$fk_tibble, csv_fk_filename)
+
+  walk2(dm_get_tables_impl(dm), csv_table_filenames, readr::write_csv)
+  if (!zip) {
+    message(
+      glue::glue("Written `dm` as csv-files to the directory {tick(csv_directory)}.")
+    )
+  }
+  invisible(csv_directory)
 }
 
 dm_col_class <- function(dm) {
