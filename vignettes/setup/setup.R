3824d332ed1fae9779cca885c1949bbc2b6cd1f9
Kirill Müller <krlmlr+r@mailbox.org>
2021-04-25 01:14:30 +0200

    Add code to time each chunk

--- vignettes/setup/setup.R
+++ vignettes/setup/setup.R
@@ -1,4 +1,20 @@
+knitr::knit_hooks$set(time_it = local({
+  now <- NULL
+  function(before, options) {
+    if (before) {
+      # record the current time before each chunk
+      now <<- Sys.time()
+    } else {
+      # calculate the time difference after a chunk
+      res <- difftime(Sys.time(), now)
+      # return a character string to show the time
+      message("Time for this code chunk to run: ", res)
+    }
+  }
+}))
+
 knitr::opts_chunk$set(
+  time_it = TRUE,
   error = (Sys.getenv("IN_PKGDOWN") == "") || !dm:::dm_has_financial(),
   message = TRUE,
   collapse = TRUE,
