b41feb63e6e5b64ffbe3a8b719622a5cafb07a66
Kirill Müller <krlmlr+r@mailbox.org>
2021-04-26 06:05:31 +0200

    Convert test

--- tests/testthat/test-zoom.R
+++ tests/testthat/test-zoom.R
@@ -127,8 +127,8 @@ test_that("all cols are tracked in zoomed table", {
 
 # tests for compound keys -------------------------------------------------
 
-verify_output(
-  "out/compound-zoom.txt", {
+test_that("zoom output for compound keys", {
+  expect_snapshot({
     nyc_comp() %>% dm_zoom_to(weather)
     nyc_comp() %>% dm_zoom_to(weather) %>% dm_update_zoomed()
     nyc_comp_2 <- nyc_comp() %>%
@@ -137,12 +137,13 @@ verify_output(
     nyc_comp_2
     attr(igraph::E(create_graph_from_dm(nyc_comp_2)), "vnames")
     dm_get_pk(nyc_comp_2, weather_2)
-    # FIXME: COMPOUND:: dm_insert_zoomed() does not recreate compound FKs
-    # nyc_comp_3 <- nyc_comp() %>%
-    #   dm_zoom_to(flights) %>%
-    #   dm_insert_zoomed("flights_2")
-    # nyc_comp_3
-    # attr(igraph::E(create_graph_from_dm(nyc_comp_3)), "vnames")
-    # dm_get_fk(nyc_comp_3, flights_2, weather)
-  }
-)
+
+    "FIXME: COMPOUND:: dm_insert_zoomed() does not recreate compound FKs"
+    nyc_comp_3 <- nyc_comp() %>%
+      dm_zoom_to(flights) %>%
+      dm_insert_zoomed("flights_2")
+    nyc_comp_3
+    attr(igraph::E(create_graph_from_dm(nyc_comp_3)), "vnames")
+    dm_get_fk(nyc_comp_3, flights_2, weather)
+  })
+})
