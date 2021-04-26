9c2e4a0b69ea48ec2f0b8c413c91af59c7cb49b0
Kirill Müller <krlmlr+r@mailbox.org>
2021-04-26 10:27:22 +0200

    Move select()

--- R/dm.R
+++ R/dm.R
@@ -330,8 +330,8 @@ dm_get_data_model_fks <- function(x, flatten) {
 
   if (nrow(fk_df) == 0) {
     return(tibble(
-      table = character(), column = character(),
-      ref = character(), ref_col = character()
+      child_table = character(), child_fk_cols = character(),
+      parent_table = character(), parent_pk_cols = character()
     ))
   }
 
@@ -346,7 +346,8 @@ dm_get_data_model_fks <- function(x, flatten) {
     select(-column) %>%
     unnest(fks) %>%
     mutate(column = my_flatten_key(column)) %>%
-    select(ref, column, table, ref_col)
+    select(ref, column, table, ref_col) %>%
+    select(child_table = table, child_fk_cols = column, parent_table = ref, parent_pk_cols = ref_col)
 }
 
 flatten_key <- function(x) {
