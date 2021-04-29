f254e6e306e753d574c96f6cc5d29068f2432a11
Kirill Müller <krlmlr+r@mailbox.org>
2021-04-29 17:50:06 +0200

    Postgres, PK and FK

--- scratch/info.R
+++ scratch/info.R
@@ -3,8 +3,8 @@ pkgload::load_all()
 
 #src <- dm::dm_get_src(dm_financial())
 #src <- test_src_maria()
-#src <- test_src_postgres()
-src <- test_src_mssql()
+src <- test_src_postgres()
+#src <- test_src_mssql()
 
 # DuckDB doesn't have references
 #src <- test_src_duckdb()
@@ -32,13 +32,36 @@ obj %>% filter(is_prefix)
 DBI::dbListObjects(con, DBI::Id(schema = "INFORMATION_SCHEMA"))
 
 info <-
-  dm_meta(con, schema = "dbo")
+  dm_meta(con)
 
 info %>%
   dm_draw()
 
 info %>%
   dm_get_tables()
+
+pk <-
+  info %>%
+  dm_zoom_to(table_constraints) %>%
+  filter(constraint_type == "PRIMARY KEY") %>%
+  dm_update_zoomed() %>%
+  dm_zoom_to(key_column_usage) %>%
+  semi_join(table_constraints)
+
+pk
+
+fk <-
+  info %>%
+  dm_zoom_to(table_constraints) %>%
+  filter(constraint_type == "FOREIGN KEY") %>%
+  dm_update_zoomed() %>%
+  dm_zoom_to(constraint_column_usage) %>%
+  semi_join(table_constraints) %>%
+  rename(fk_table_catalog = table_catalog, fk_table_schema = table_schema, fk_table_name = table_name, fk_column_name = column_name) %>%
+  left_join(key_column_usage) %>%
+  rename(pk_table_catalog = table_catalog, pk_table_schema = table_schema, pk_table_name = table_name, pk_column_name = column_name)
+
+fk
 
 info_local <-
   info %>%
