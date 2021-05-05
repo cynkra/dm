09264eda95320b4edabb1e42f0d83ac30b17826e
Kirill Müller <krlmlr+r@mailbox.org>
2021-05-05 10:06:30 +0200

    Use dm_for_filter_simple in deprecated tests

--- tests/testthat/test-zzx-deprecated.R
+++ tests/testthat/test-zzx-deprecated.R
@@ -6,8 +6,8 @@ test_that("cdm_add_tbl() works", {
   mtcars_tbl <- test_src_frame(!!!mtcars)
 
   expect_equivalent_dm(
-    cdm_add_tbl(dm_for_filter(), cars_table = mtcars_tbl),
-    dm_add_tbl(dm_for_filter(), cars_table = mtcars_tbl)
+    cdm_add_tbl(dm_for_filter_simple(), cars_table = mtcars_tbl),
+    dm_add_tbl(dm_for_filter_simple(), cars_table = mtcars_tbl)
   )
 })
 
@@ -27,8 +27,8 @@ test_that("cdm_copy_to() behaves correctly", {
   local_options(lifecycle_verbosity = "quiet")
 
   expect_equivalent_dm(
-    cdm_copy_to(sqlite(), dm_for_filter(), unique_table_names = TRUE),
-    dm_for_filter()
+    cdm_copy_to(sqlite(), dm_for_filter_simple(), unique_table_names = TRUE),
+    dm_for_filter_simple()
   )
 })
 
@@ -61,18 +61,18 @@ test_that("cdm_filter() behaves correctly", {
   local_options(lifecycle_verbosity = "quiet")
 
   expect_equivalent_tbl(
-    cdm_filter(dm_for_filter(), tf_1, a > 4) %>% dm_apply_filters_to_tbl(tf_2),
+    cdm_filter(dm_for_filter_simple(), tf_1, a > 4) %>% dm_apply_filters_to_tbl(tf_2),
     filter(tf_2(), d > 4)
   )
 
   expect_equivalent_tbl(
-    dm_filter(dm_for_filter(), tf_1, a > 4) %>% cdm_apply_filters_to_tbl(tf_2),
+    dm_filter(dm_for_filter_simple(), tf_1, a > 4) %>% cdm_apply_filters_to_tbl(tf_2),
     filter(tf_2(), d > 4)
   )
 
   skip_if_remote_src()
   expect_snapshot({
-    dm_for_filter() %>%
+    dm_for_filter_simple() %>%
       dm_filter(tf_1, a > 3, a < 8) %>%
       cdm_apply_filters() %>%
       dm_get_tables()
@@ -122,7 +122,7 @@ test_that("cdm_get_src() works", {
 
   skip_if_local_src()
   expect_identical(
-    class(cdm_get_src(dm_for_filter())),
+    class(cdm_get_src(dm_for_filter_simple())),
     class(my_test_src())
   )
 })
@@ -138,11 +138,11 @@ test_that("cdm_get_con() works", {
 
   if (is.null(my_test_src())) {
     expect_dm_error(
-      cdm_get_con(dm_for_filter()),
+      cdm_get_con(dm_for_filter_simple()),
       class = "con_only_for_dbi"
     )
   } else {
-    expect_silent(cdm_get_con(dm_for_filter()))
+    expect_silent(cdm_get_con(dm_for_filter_simple()))
   }
 })
 
@@ -152,8 +152,8 @@ test_that("cdm_get_tables() works", {
   local_options(lifecycle_verbosity = "quiet")
 
   expect_equivalent_tbl_lists(
-    cdm_get_tables(dm_for_filter()),
-    dm_get_tables(dm_for_filter())
+    cdm_get_tables(dm_for_filter_simple()),
+    dm_get_tables(dm_for_filter_simple())
   )
 })
 
@@ -162,13 +162,13 @@ test_that("cdm_get_filter() works", {
   local_options(lifecycle_verbosity = "quiet")
 
   expect_identical(
-    cdm_get_filter(dm_for_filter()),
-    dm_get_filters(dm_for_filter())
+    cdm_get_filter(dm_for_filter_simple()),
+    dm_get_filters(dm_for_filter_simple())
   )
 
   expect_identical(
-    cdm_get_filter(dm_filter(dm_for_filter(), tf_1, a > 3, a < 8)),
-    dm_get_filters(dm_filter(dm_for_filter(), tf_1, a > 3, a < 8))
+    cdm_get_filter(dm_filter(dm_for_filter_simple(), tf_1, a > 3, a < 8)),
+    dm_get_filters(dm_filter(dm_for_filter_simple(), tf_1, a > 3, a < 8))
   )
 })
 
@@ -195,33 +195,33 @@ test_that("other FK functions work", {
   skip_on_cran()
   local_options(lifecycle_verbosity = "quiet")
 
-  expect_true(cdm_has_fk(dm_for_filter(), tf_2, tf_1))
+  expect_true(cdm_has_fk(dm_for_filter_simple(), tf_2, tf_1))
 
-  expect_false(cdm_has_fk(dm_for_filter(), tf_1, tf_2))
+  expect_false(cdm_has_fk(dm_for_filter_simple(), tf_1, tf_2))
 
   expect_identical(
-    cdm_get_fk(dm_for_filter(), tf_2, tf_1),
-    dm_get_fk(dm_for_filter(), tf_2, tf_1)
+    cdm_get_fk(dm_for_filter_simple(), tf_2, tf_1),
+    dm_get_fk(dm_for_filter_simple(), tf_2, tf_1)
   )
 
   expect_identical(
-    dm_for_filter() %>%
+    dm_for_filter_simple() %>%
       cdm_get_all_fks() %>%
       mutate(child_fk_cols = new_keys(child_fk_cols), parent_pk_cols = new_keys(parent_pk_cols)),
-    dm_get_all_fks(dm_for_filter())
+    dm_get_all_fks(dm_for_filter_simple())
   )
 
   expect_equivalent_dm(
-    cdm_rm_fk(dm_for_filter(), tf_2, d, tf_1),
-    dm_rm_fk(dm_for_filter(), tf_2, d, tf_1)
+    cdm_rm_fk(dm_for_filter_simple(), tf_2, d, tf_1),
+    dm_rm_fk(dm_for_filter_simple(), tf_2, d, tf_1)
   )
 
   skip_if_remote_src()
   expect_identical(
-    dm_for_filter() %>%
+    dm_for_filter_simple() %>%
       cdm_enum_fk_candidates(tf_2, tf_1) %>%
       mutate(why = if_else(why != "", "<reason>", "")),
-    dm_for_filter() %>%
+    dm_for_filter_simple() %>%
       dm_enum_fk_candidates(tf_2, tf_1) %>%
       mutate(why = if_else(why != "", "<reason>", ""))
   )
@@ -232,13 +232,13 @@ test_that("graph-functions work", {
   local_options(lifecycle_verbosity = "quiet")
 
   expect_identical(
-    cdm_is_referenced(dm_for_filter(), tf_3),
-    dm_is_referenced(dm_for_filter(), tf_3)
+    cdm_is_referenced(dm_for_filter_simple(), tf_3),
+    dm_is_referenced(dm_for_filter_simple(), tf_3)
   )
 
   expect_identical(
-    cdm_get_referencing_tables(dm_for_filter(), tf_3),
-    dm_get_referencing_tables(dm_for_filter(), tf_3)
+    cdm_get_referencing_tables(dm_for_filter_simple(), tf_3),
+    dm_get_referencing_tables(dm_for_filter_simple(), tf_3)
   )
 })
 
@@ -251,7 +251,7 @@ test_that("cdm_learn_from_db() works from PG", {
 
   # create an object on the Postgres-DB that can be learned
   if (is_postgres_empty()) {
-    copy_dm_to(con_postgres, dm_for_filter(), unique_table_names = TRUE, temporary = FALSE)
+    copy_dm_to(con_postgres, dm_for_filter_simple(), unique_table_names = TRUE, temporary = FALSE)
   }
 
   expect_equivalent_dm(
@@ -286,7 +286,7 @@ test_that("cdm_paste() works", {
   local_options(lifecycle_verbosity = "quiet")
 
   expect_output(
-    cdm_paste(dm_for_filter(), FALSE, 4),
+    cdm_paste(dm_for_filter_simple(), FALSE, 4),
     paste0(
       "dm::dm(tf_1, tf_2, tf_3, tf_4, tf_5, tf_6) %>%\n    dm::dm_add_pk(tf_1, a) %>%\n    dm::dm_add_pk(tf_2, c) %>%",
       "\n    dm::dm_add_pk(tf_3, f) %>%\n    dm::dm_add_pk(tf_4, h) %>%\n    dm::dm_add_pk(tf_5, k) %>%\n    ",
@@ -302,25 +302,25 @@ test_that("other PK functions work", {
   local_options(lifecycle_verbosity = "quiet")
 
   expect_identical(
-    cdm_has_pk(dm_for_filter(), tf_1),
-    dm_has_pk(dm_for_filter(), tf_1)
+    cdm_has_pk(dm_for_filter_simple(), tf_1),
+    dm_has_pk(dm_for_filter_simple(), tf_1)
   )
 
   expect_identical(
-    cdm_get_pk(dm_for_filter(), tf_1),
-    dm_get_pk(dm_for_filter(), tf_1)
+    cdm_get_pk(dm_for_filter_simple(), tf_1),
+    dm_get_pk(dm_for_filter_simple(), tf_1)
   )
 
   expect_identical(
-    dm_for_filter() %>%
+    dm_for_filter_simple() %>%
       cdm_get_all_pks() %>%
       mutate(pk_col = new_keys(pk_col)),
-    dm_get_all_pks(dm_for_filter())
+    dm_get_all_pks(dm_for_filter_simple())
   )
 
   expect_equivalent_dm(
-    cdm_rm_pk(dm_for_filter(), tf_2),
-    dm_rm_pk(dm_for_filter(), tf_2)
+    cdm_rm_pk(dm_for_filter_simple(), tf_2),
+    dm_rm_pk(dm_for_filter_simple(), tf_2)
   )
 
   expect_identical(
@@ -337,13 +337,13 @@ test_that("dm_select_tbl() and dm_rename_tbl() work", {
   local_options(lifecycle_verbosity = "quiet")
 
   expect_equivalent_dm(
-    cdm_select_tbl(dm_for_filter(), tf_1_new = tf_1, tf_2, new_tf_6 = tf_6),
-    dm_select_tbl(dm_for_filter(), tf_1_new = tf_1, tf_2, new_tf_6 = tf_6)
+    cdm_select_tbl(dm_for_filter_simple(), tf_1_new = tf_1, tf_2, new_tf_6 = tf_6),
+    dm_select_tbl(dm_for_filter_simple(), tf_1_new = tf_1, tf_2, new_tf_6 = tf_6)
   )
 
   expect_equivalent_dm(
-    cdm_rename_tbl(dm_for_filter(), tf_1_new = tf_1, new_tf_6 = tf_6),
-    dm_rename_tbl(dm_for_filter(), tf_1_new = tf_1, new_tf_6 = tf_6)
+    cdm_rename_tbl(dm_for_filter_simple(), tf_1_new = tf_1, new_tf_6 = tf_6),
+    dm_rename_tbl(dm_for_filter_simple(), tf_1_new = tf_1, new_tf_6 = tf_6)
   )
 })
 
@@ -352,13 +352,13 @@ test_that("dm_select() and dm_rename() work", {
   local_options(lifecycle_verbosity = "quiet")
 
   expect_equivalent_tbl(
-    cdm_select(dm_for_filter(), tf_1, a_new = a) %>% tbl_impl("tf_1"),
-    dm_select(dm_for_filter(), tf_1, a_new = a) %>% tbl_impl("tf_1")
+    cdm_select(dm_for_filter_simple(), tf_1, a_new = a) %>% tbl_impl("tf_1"),
+    dm_select(dm_for_filter_simple(), tf_1, a_new = a) %>% tbl_impl("tf_1")
   )
 
   expect_equivalent_tbl(
-    cdm_rename(dm_for_filter(), tf_1, a_new = a) %>% tbl_impl("tf_1"),
-    dm_rename(dm_for_filter(), tf_1, a_new = a) %>% tbl_impl("tf_1")
+    cdm_rename(dm_for_filter_simple(), tf_1, a_new = a) %>% tbl_impl("tf_1"),
+    dm_rename(dm_for_filter_simple(), tf_1, a_new = a) %>% tbl_impl("tf_1")
   )
 })
 
@@ -367,23 +367,23 @@ test_that("dm_zoom_to() and related functions work", {
   local_options(lifecycle_verbosity = "quiet")
 
   expect_equivalent_dm(
-    cdm_zoom_to_tbl(dm_for_filter(), tf_1),
-    dm_zoom_to(dm_for_filter(), tf_1)
+    cdm_zoom_to_tbl(dm_for_filter_simple(), tf_1),
+    dm_zoom_to(dm_for_filter_simple(), tf_1)
   )
 
   expect_equivalent_dm(
-    dm_zoom_to(dm_for_filter(), tf_1) %>% cdm_insert_zoomed_tbl("another_name"),
-    dm_zoom_to(dm_for_filter(), tf_1) %>% dm_insert_zoomed("another_name")
+    dm_zoom_to(dm_for_filter_simple(), tf_1) %>% cdm_insert_zoomed_tbl("another_name"),
+    dm_zoom_to(dm_for_filter_simple(), tf_1) %>% dm_insert_zoomed("another_name")
   )
 
   expect_equivalent_dm(
-    dm_zoom_to(dm_for_filter(), tf_1) %>% cdm_update_zoomed_tbl(),
-    dm_zoom_to(dm_for_filter(), tf_1) %>% dm_update_zoomed()
+    dm_zoom_to(dm_for_filter_simple(), tf_1) %>% cdm_update_zoomed_tbl(),
+    dm_zoom_to(dm_for_filter_simple(), tf_1) %>% dm_update_zoomed()
   )
 
   expect_equivalent_dm(
-    dm_zoom_to(dm_for_filter(), tf_1) %>% cdm_zoom_out(),
-    dm_zoom_to(dm_for_filter(), tf_1) %>% dm_discard_zoomed()
+    dm_zoom_to(dm_for_filter_simple(), tf_1) %>% cdm_zoom_out(),
+    dm_zoom_to(dm_for_filter_simple(), tf_1) %>% dm_discard_zoomed()
   )
 })
 
