6c9810a507df2676821b60f53f4bfdf2270ba6c1
Kirill Müller <krlmlr+r@mailbox.org>
2021-06-12 03:04:27 +0200

    Reword skip message

--- tests/testthat/helper-skip.R
+++ tests/testthat/helper-skip.R
@@ -12,11 +12,15 @@ skip_if_remote_src <- function(src = my_test_src()) {
 }
 
 skip_if_src <- function(...) {
-  if (my_test_src_name %in% c(...)) skip(glue::glue("does not work on {commas(tick(c(...)))}"))
+  if (my_test_src_name %in% c(...)) {
+    skip(glue::glue("does not work on {commas(tick(c(...)))}"))
+  }
 }
 
 skip_if_src_not <- function(...) {
-  if (!(my_test_src_name %in% c(...))) skip(paste0("does not work on ", my_test_src_name))
+  if (!(my_test_src_name %in% c(...))) {
+    skip(glue::glue("only works on {commas(tick(c(...)))}"))
+  }
 }
 
 suppress_mssql_message <- function(code) {
