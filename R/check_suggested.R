576c78da70ed34af58198656df57585115062883
Antoine Fabri <antoine.fabri@gmail.com>
2021-09-14 18:24:09 +0200

    improve check_suggested

--- R/check_suggested.R
+++ R/check_suggested.R
@@ -6,25 +6,35 @@
 #' @param top_level_fun the name of the top level function called, not used if
 #'   `message` is provided
 #' @param message optional custom message, by default the message follows a template
-#'
+#' @return whether check was triggered and all packages are installed
 #' @noRd
 check_suggested <- function(packages, use, top_level_fun = NULL, message = NULL) {
+  # If NA, inform that package isn't install, but only in interactive mode
   if (is.na(use)) {
     use <- is_interactive()
-    for (pkg in packages) {
-      if (use && !is_installed(pkg)) {
+    if (!use) {
+      return(FALSE)
+    }
+
+    installed <- map_lgl(packages, ~ {
+      installed <- is_installed(.)
+      if (!installed) {
         if (is.null(message)) {
-          message <- glue("`{top_level_fun}()` is improved by the '{pkg}' package. Consider `install.packages(\"{pkg}\")`.")
+          message <- glue("`{top_level_fun}()` is improved by the '{.}' package. Consider `install.packages(\"{.}\")`.")
         }
         inform(message)
       }
-    }
+      installed
+    })
+    return(all(installed))
   }
 
+  # If FALSE, hide
   if (!use) {
     return(FALSE)
   }
 
+  # If TRUE, fail if package is not installed
   for (pkg in packages) {
     if (!is_installed(pkg)) {
       if (is.null(message)) {
@@ -35,3 +45,6 @@ check_suggested <- function(packages, use, top_level_fun = NULL, message = NULL)
   }
   TRUE
 }
+
+
+
