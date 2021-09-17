81499b5d210606726aaf2322fe56959984265ed5
Maximilian Girlich <maximilian.girlich@metoda.com>
2021-09-17 08:55:12 +0000

    Remove unnecessary functions: * `sql_rows_update.tbl_SQLiteConnection()` * `sql_rows_update.tbl_PqConnection()` * `sql_rows_patch.tbl_SQLiteConnection()`

--- R/rows-db.R
+++ R/rows-db.R
@@ -363,30 +363,6 @@ sql_rows_update.tbl_sql <- function(x, y, by, ..., returning_cols = NULL, na_mat
 }
 
 #' @export
-sql_rows_update.tbl_SQLiteConnection <- function(x, y, by, ..., returning_cols = NULL, na_matches = c("na", "never")) {
-  con <- dbplyr::remote_con(x)
-  na_matches <- arg_match(na_matches)
-
-  p <- sql_rows_prep(x, y, by, na_matches = na_matches)
-
-  sql <- paste0(
-    "WITH ", p$y_name, "(", p$y_columns_qq, ") AS (\n",
-    dbplyr::sql_render(y),
-    "\n)\n",
-    #
-    "UPDATE ", p$name, "\n",
-    "SET (", p$new_columns_qq, ") = (\n",
-    "SELECT ", p$new_columns_qual_qq, "\n",
-    "FROM ", p$y_name, "\n",
-    "WHERE (", p$compare_qual_qq, "))\n",
-    "WHERE EXISTS (SELECT * FROM ", p$y_name, " WHERE ", p$compare_qual_qq, ")",
-    sql_returning_cols(x, returning_cols)
-  )
-
-  glue::as_glue(sql)
-}
-
-#' @export
 `sql_rows_update.tbl_Microsoft SQL Server` <- function(x, y, by, ..., returning_cols = NULL, na_matches = c("na", "never")) {
   con <- dbplyr::remote_con(x)
   na_matches <- arg_match(na_matches)
@@ -430,35 +406,6 @@ sql_rows_update.tbl_MariaDBConnection <- function(x, y, by, ..., returning_cols 
     "  ON ", p$compare_qual_qq, "\n",
     "SET\n",
     paste0("  ", p$target_columns_qual_qq, " = ", p$new_columns_qual_qq, collapse = ",\n"),
-    sql_returning_cols(x, returning_cols)
-  )
-
-  glue::as_glue(sql)
-}
-
-#' @export
-sql_rows_update.tbl_PqConnection <- function(x, y, by, ..., returning_cols = NULL, na_matches = c("na", "never")) {
-  con <- dbplyr::remote_con(x)
-  na_matches <- arg_match(na_matches)
-
-  p <- sql_rows_prep(x, y, by, na_matches = na_matches)
-
-  # https://www.postgresql.org/docs/9.5/sql-update.html
-  sql <- paste0(
-    "WITH ", p$y_name, " AS (\n",
-    dbplyr::sql_render(y),
-    "\n)\n",
-    #
-    "UPDATE ", p$name, "\n",
-    "SET\n",
-    paste0(
-      "  ", unlist(p$new_columns_qq_list),
-      " = ", unlist(p$new_columns_qual_qq_list),
-      collapse = ",\n"
-    ),
-    "\n",
-    "FROM ", p$y_name, "\n",
-    "WHERE ", p$compare_qual_qq,
     sql_returning_cols(x, returning_cols)
   )
 
@@ -543,35 +490,6 @@ sql_rows_patch.tbl_MariaDBConnection <- function(x, y, by, ..., returning_cols =
     "  ON ", p$compare_qual_qq, "\n",
     "SET\n",
     paste0("  ", p$new_columns_qq, " = ", p$new_columns_patch_qq, collapse = ",\n"),
-    sql_returning_cols(x, returning_cols)
-  )
-
-  glue::as_glue(sql)
-}
-
-#' @export
-sql_rows_patch.tbl_PqConnection <- function(x, y, by, ..., returning_cols = NULL, na_matches = c("na", "never")) {
-  con <- dbplyr::remote_con(x)
-  na_matches <- arg_match(na_matches)
-
-  p <- sql_rows_prep(x, y, by, na_matches = na_matches)
-
-  # https://www.postgresql.org/docs/9.5/sql-update.html
-  sql <- paste0(
-    "WITH ", p$y_name, " AS (\n",
-    dbplyr::sql_render(y),
-    "\n)\n",
-    #
-    "UPDATE ", p$name, "\n",
-    "SET\n",
-    paste0(
-      "  ", unlist(p$new_columns_qq_list),
-      " = ", unlist(p$new_columns_patch_qq_list),
-      collapse = ",\n"
-    ),
-    "\n",
-    "FROM ", p$y_name, "\n",
-    "WHERE ", p$compare_qual_qq,
     sql_returning_cols(x, returning_cols)
   )
 
