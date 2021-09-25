4ddd6df212c24abc2a68fb9a69b40727fcba8d6c
Kirill Müller <krlmlr@users.noreply.github.com>
2021-09-25 19:59:01 +0200

    Update tests/testthat/test-pool-support.R

--- tests/testthat/test-pool-support.R
+++ tests/testthat/test-pool-support.R
@@ -1,8 +1,7 @@
 test_that("dm_from_src supports 'Pool'", {
   # expect no error
-  expect_error(regexp = NA, {
-    conn <- pool::dbPool(RSQLite::SQLite(), "", timeout = 10)
-    DBI::dbWriteTable(conn, "mtcars", mtcars)
-    thedb_raw <- dm::dm_from_src(conn, learn_keys = FALSE)
-  })
+  conn <- pool::dbPool(RSQLite::SQLite(), "", timeout = 10)
+  DBI::dbWriteTable(conn, "mtcars", mtcars)
+  dm <- dm::dm_from_src(conn, learn_keys = FALSE)
+  expect_identical(names(dm), "mtcars")
 })
