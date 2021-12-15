1dfcd5672504067e24ac9c1dd7fcccc13a6490db
Antoine Fabri <antoine.fabri@gmail.com>
2021-12-15 08:51:47 +0100

    dbSendStatement -> dbExecute
    Co-authored-by: Kirill Müller <krlmlr@users.noreply.github.com>

--- R/db-interface.R
+++ R/db-interface.R
@@ -199,8 +199,7 @@ copy_dm_to <- function(dest, dm, ...,
 
   # create tables
   walk(queries$create_table_queries$sql, ~{
-    rs <- DBI::dbSendStatement(dest_con, .x)
-    DBI::dbClearResult(rs)
+    DBI::dbExecute(dest_con, .x, immediate = TRUE)
   })
 
   # populate tables
