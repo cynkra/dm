245d0fa0966870e03c558f07b7962504bfa7aaf4
Kirill Müller <krlmlr+r@mailbox.org>
2021-12-23 09:53:39 +0100

    Tweak test

--- tests/testthat/test-dm-from-src.R
+++ tests/testthat/test-dm-from-src.R
@@ -8,12 +8,9 @@ test_that("table identifiers are quoted", {
       test_table_321 = tibble(b = 2)
     ),
     temporary = FALSE,
-    table_names = ~ DBI::SQL(unique_db_table_name(.x))
+    table_names = ~ unique_db_table_name(.x)
   )
-  remote_tbl_names_copied <- map_chr(
-    src_tbls_impl(test_dm),
-    ~ dbplyr::remote_name(test_dm[[.x]])
-  )
+  remote_tbl_names_copied <- DBI::SQL(map_chr(dm_get_tables(test_dm), dbplyr::remote_name))
 
   on.exit({
     walk(
@@ -24,16 +21,18 @@ test_that("table identifiers are quoted", {
 
   dm <-
     suppress_mssql_warning(dm_from_src(src_db, learn_keys = FALSE)) %>%
-    dm_select_tbl(!!!remote_tbl_names_copied)
+    dm_select_tbl(!!!map(
+      DBI::dbUnquoteIdentifier(src_db$con, remote_tbl_names_copied),
+      ~ .x@name[["table"]]
+    ))
 
   remote_tbl_names_learned <-
     dm %>%
     dm_get_tables() %>%
     map_chr(dbplyr::remote_name)
 
-  con <- dm_get_con(dm)
   # `gsub()`, cause schema names are part of the remote_names (also standard schemas "dbo" for MSSQL and "public" for Postgres).
-  expect_equal(gsub("^.*\\.", "", unname(remote_tbl_names_learned)), unclass(DBI::dbQuoteIdentifier(con, names(dm))))
+  expect_equal(gsub("^.*\\.", "", unname(remote_tbl_names_learned)), unclass(DBI::dbQuoteIdentifier(src_db$con, names(dm))))
 })
 
 test_that("table identifiers are quoted with learn_keys = FALSE", {
