93c6fa2371dbfde1b02d50a8168bce7b3aa9f148
krlmlr <krlmlr@users.noreply.github.com>
2021-12-28 17:14:49 +0000

    Auto-update from GitHub Actions
    Run: https://github.com/cynkra/dm/actions/runs/1631260665

--- R/pack_join.R
+++ R/pack_join.R
@@ -5,16 +5,16 @@
 #' @inheritParams dplyr::nest_join
 #'
 #' @export
-pack_join <- function (x, y, by = NULL, copy = FALSE, keep = FALSE, name = NULL, ...) {
+pack_join <- function(x, y, by = NULL, copy = FALSE, keep = FALSE, name = NULL, ...) {
   UseMethod("pack_join")
 }
 
 #' @export
-pack_join.data.frame <- function (x, y, by = NULL, copy = FALSE, keep = FALSE, name = NULL, ...) {
+pack_join.data.frame <- function(x, y, by = NULL, copy = FALSE, keep = FALSE, name = NULL, ...) {
   name_var <- name %||% as_label(enexpr(y))
   # by2 is only used for `pack`, so we keep dplyr's messages for implicit `by`
   by2 <- by %||% intersect(names(x), names(y))
   y_packed <- tidyr::pack(y, !!name_var := -all_of(by2))
-  #FIXME: handle potential conflict between name_var and existing variables
+  # FIXME: handle potential conflict between name_var and existing variables
   left_join(x, y_packed, by = by, copy = copy, keep = keep, ...)
 }
