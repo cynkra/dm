e80a460ea0616b19514a1b7ab937c7d8f822904c
Antoine Fabri <antoine.fabri@gmail.com>
2021-12-28 13:18:56 +0100

    Implement pack_join

---
+++ R/pack_join.R
@@ -0,0 +1,22 @@
+#' Pack Join
+#'
+#' `pack_join()` returns all rows and columns in x with a new packed column
+#' that contains all matches from y. When there is no match, the list column is a 0-row tibble.
+#' @inheritParams dplyr::nest_join
+#'
+#' @export
+pack_join <- function (x, y, by = NULL, copy = FALSE, keep = FALSE, name = NULL, ...) {
+  UseMethod("pack_join")
+}
+
+#' @export
+pack_join.data.frame <- function (x, y, by = NULL, copy = FALSE, keep = FALSE, name = NULL, ...) {
+  name_var <- name %||% as_label(enexpr(y))
+  # by2 is only used for `pack`, so we keep dplyr's messages for implicit `by`
+  by2 <- if(is.null(by)) intersect(names(x), names(y)) else by
+  y_packed <- tidyr::pack(y, !!name_var := -match(by2, names(y)))
+  #FIXME: handle potential conflict between name_var and existing variables
+  left_join(x, y_packed, by = by, copy = copy, keep = keep, ...)
+}
+
+# band_members %>% pack_join(band_instruments)
