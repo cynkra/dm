47dd22a6f55f79e96fc89594fe809db032f95b3c
Antoine Fabri <antoine.fabri@gmail.com>
2022-01-04 03:17:54 +0100

    Refactor functions
    We don't use attributes anymore, roundtrip works better

--- R/dm_to_tibble.R
+++ R/dm_to_tibble.R
@@ -11,219 +11,148 @@
 #'
 #' @noRd
 dm_to_tibble <- function(dm, root) {
+  pks <- dm_get_all_pks(dm)
+  fks <- dm_get_all_fks(dm)
 
-  def <- dm_get_def(dm)
+  tbl_defs <-
+    list(
+      tbl = dm_get_tables(dm),
+      pks = deframe(pks),
+      children = split(fks$child_table, fks$parent_table),
+      fks = split(fks, fks$child_table)
+    ) %>%
+    transpose()
 
-  gather_children <- function(def, root, except = NULL) {
-    fks_subset <- filter(fks, parent_table == root)
-    tbl <- def$data[[which(def$table == root)]]
-    n_children <- nrow(fks_subset)
-    for(i in seq_len(n_children)) {
-      child_tbl_nm <- fks_subset$child_table[i]
+  gather <- function(tbl_nm, except = NULL) {
+    # browser()
+    tbl_def <- tbl_defs[[tbl_nm]]
+    own_keys <- compact(tbl_def[c("pks", "fks")])
+    tbl <- tbl_def$tbl
 
-      ## recurse if relevant
-      if(child_tbl_nm %in% except) next
-      def <- gather_children(def, child_tbl_nm)
-      def <- gather_parents(def, child_tbl_nm, except = root)
-
-      ## nest child table
-      keys <- list(
-        fks = fks_subset[i, c("child_fk_cols", "parent_key_cols", "on_delete")],
-        pks = pks[pks$table == child_tbl_nm,]
-      )
-      by_cols <- with(keys$fks, set_names(unlist(child_fk_cols), unlist(parent_key_cols)))
-      child_tbl <- def$data[[which(def$table == child_tbl_nm)]]
-      tbl <- nest_join(tbl, child_tbl, by = by_cols, name = child_tbl_nm)
-
-      ## store keys for reverse op
-      # since nesting splits table and destroys attributes we store the child's
-      # keys next to the table's own keys
-      child_keys <- compact(map(child_tbl, attr, "..keys.."))
-      attr(tbl[[child_tbl_nm]], "..keys..") <- list(own = keys, child_keys = child_keys)
-
-      ## remove child from def
-      def <- def[def$table != child_tbl_nm, ]
+    # gather children
+    children_keys <- list()
+    children_nms <- setdiff(tbl_def$children, except)
+    for(child_nm in children_nms) {
+      tbl_keys <- gather(child_nm, except = tbl_nm)
+      child_tbl <- tbl_keys$tbl
+      child_keys <- tbl_keys$keys
+      children_keys[[child_nm]] <- child_keys
+      fks <-  filter(child_keys$own_keys$fks, parent_table == tbl_nm)
+      by <- with(fks, set_names(unlist(child_fk_cols), unlist(parent_key_cols)))
+      tbl <- nest_join(tbl, child_tbl, by = by, name = child_nm)
     }
-    ## update def
-    def$data[[which(def$table == root)]] <- tbl
-    def
-  }
+    children_keys <- compact(children_keys)
 
-  gather_parents <- function(def, root, except = NULL) {
-    fks_subset <- filter(fks, child_table == root)
-    tbl <- def$data[[which(def$table == root)]]
-    n_parents <- nrow(fks_subset)
-    for(i in seq_len(n_parents)) {
-      parent_tbl_nm <- fks_subset$parent_table[i]
-
-      ## recurse if relevant
-      if(parent_tbl_nm %in% except) next
-      def <- gather_parents(def, parent_tbl_nm)
-      def <- gather_children(def, parent_tbl_nm, except = root)
-
-      ## pack parent table
-      keys <- list(
-        fks = fks_subset[i, c("child_fk_cols", "parent_key_cols", "on_delete")],
-        pks = pks[pks$table == parent_tbl_nm,]
-      )
-      by_cols <- with(keys$fks, set_names(unlist(parent_key_cols), unlist(child_fk_cols)))
-      parent_tbl <- def$data[[which(def$table == parent_tbl_nm)]]
+    # gather parents
+    parents_keys <- list()
+    parent_nms <- setdiff(unique(tbl_def$fks$parent_table), except)
+    for(parent_nm in parent_nms) {
+      tbl_keys <- gather(parent_nm, except = tbl_nm)
+      parent_tbl <- tbl_keys$tbl
+      parent_keys <- tbl_keys$keys
+      parents_keys[[parent_nm]] <- parent_keys
+      fks <- filter(tbl_def$fks, parent_table == parent_nm)
+      by <- with(fks, set_names(unlist(parent_key_cols), unlist(child_fk_cols)))
       # FIXME: when pack_join is merged, replace next lines
-      parent_tbl <- pack(parent_tbl, !!parent_tbl_nm := -match(by_cols, names(parent_tbl)))
-      tbl <- left_join(tbl, parent_tbl, by = by_cols)
+      parent_tbl <- pack(parent_tbl, !!parent_nm := -match(by, names(parent_tbl)))
+      tbl <- left_join(tbl, parent_tbl, by = by)
       # tbl <- pack_join(tbl, parent_tbl, by = by_cols, name = parent_tbl_nm)
-
-      ## store keys for reverse op
-      attr(tbl[[parent_tbl_nm]], "..keys..") <- keys
-
-      ## remove parent from def
-      def <- def[def$table != parent_tbl_nm, ]
     }
-    ## update def
-    def$data[[which(def$table == root)]] <- tbl
-    def
+    parents_keys <- compact(parents_keys)
+
+    # add keys
+    keys <-
+      lst(own_keys, children_keys, parents_keys) %>%
+      compact()
+
+    lst(tbl, keys)
   }
 
-  ## setup global variables
-  # to ease the round trips we remove all unnecessary attrs since toJSON/fromJSON strips them
-  pks <- as.data.frame(dm_get_all_pks(dm))
-  fks <- as.data.frame(dm_get_all_fks(dm))
-  pks[] <- map(pks, unclass)
-  fks[] <- map(fks, unclass)
-  attributes(pks$pk_col) <- NULL
-  attributes(fks$parent_key_cols) <- NULL
-  attributes(fks$child_fk_cols) <- NULL
-
-  ## gather all tables into root table
-  def <- gather_children(def, root)
-  def <- gather_parents(def, root)
-  tbl <- def$data[[1]]
-
-  ## set the root table's keys as attributes
-  keys <- list(
-    fks = fks[fks$child_table == root, c("child_fk_cols", "parent_table", "parent_key_cols", "on_delete")],
-    pks = pks[pks$table == root,]
-  )
-  attr(tbl, "..keys..") <- keys
-
-  tbl
+  gather(root)
 }
 
 #' Convert a tibble to a dm
 #'
-#' @param data a tibble containig a nested dm, created by `dm_to_tibble()`
+#' @param data a tibble containing a nested dm, created by `dm_to_tibble()`
+#' @param keys a nested list containing keys, created by `dm_to_tibble()`
 #' @param root the name of the main table, i.e. the name of the root table used
 #'   in the `dm_to_tibble()` call.
 #'
 #' @return a dm
 #'
 #' @noRd
-tibble_to_dm <- function(data, root) {
+tibble_to_dm <- function(data, keys, root) {
 
-  replace_in_dm <- function(nm, new) {
-    def <- dm_get_def(dm)
-    def$data[[which(def$table == nm)]] <- new
-    new_dm3(def)
-  }
-
-  populate_with_parents <- function(root) {
-    tbl <- dm[[root]]
+  dm <- dm()
+  populate <- function(tbl, tbl_nm, keys) {
+    #browser()
+    # populate dm with parents
     parents_lgl <- map_lgl(tbl, is.data.frame)
-    for (parent_tbl_nm in names(tbl)[parents_lgl]) {
+    for (parent_nm in names(tbl)[parents_lgl]) {
       ## fetch data frame col
-      parent_tbl <- tbl[[parent_tbl_nm]]
-
-      ## bind its key cols back, removing "..keys.." attr
-      keys <- attr(parent_tbl, "..keys..")
-      by_cols <- with(keys$fks, set_names(unlist(parent_key_cols), unlist(child_fk_cols)))
-      attr(parent_tbl, "..keys..") <- NULL
+      parent_tbl <- tbl[[parent_nm]]
+      parent_keys <- keys$parents_keys[[parent_nm]]
+      ## bind its key cols back
+      fks <- filter(keys$own_keys$fks, parent_table == parent_nm)
+      by_cols <-
+        with(fks, set_names(unlist(parent_key_cols), unlist(child_fk_cols)))
       parent_tbl <- bind_cols(
         set_names(tbl[names(by_cols)], by_cols),
         parent_tbl
       )
-
-      ## set keys back in the dm
-      pks <- keys$pks$pk_col[[1]]
-      pk_arg <- call2("c", !!!syms(pks))
-      dm <<- dm  %>%
-        dm_add_tbl(!!parent_tbl_nm := parent_tbl) %>%
-        dm_add_pk(!!sym(parent_tbl_nm), !!pk_arg)
-      for (i in seq_len(nrow(keys$fks))) {
-        fks <- keys$fks$child_fk_cols[[i]]
-        child_fk_arg <- call2("c", !!!syms(fks))
-        dm <<- dm_add_fk(dm, !!sym(root), !!child_fk_arg, !!sym(parent_tbl_nm))
-      }
-
-      ## recurse
-      populate_with_parents(parent_tbl_nm)
-      populate_with_children(parent_tbl_nm)
+      populate(parent_tbl, parent_nm, parent_keys)
+      tbl[[parent_nm]] <- NULL
     }
-    ## remove from source table and update dm
-    tbl[parents_lgl] <- NULL
-    dm <<- replace_in_dm(root, tbl)
-  }
 
-  populate_with_children <- function(root) {
-    tbl <- dm[[root]]
+    # populate dm with children
     children_lgl <- map_lgl(tbl, is_bare_list)
-    for (child_tbl_nm in names(tbl)[children_lgl]) {
+    for (child_nm in names(tbl)[children_lgl]) {
       ## fetch list col in its own data frame
-      child_tbl <- tbl[child_tbl_nm]
-
-      ## bind its key cols back and reshape, removing "..keys.." attr
-      keys <- attr(child_tbl[[1]], "..keys..")
-      by_cols <- with(keys[[1]]$fks, set_names(unlist(child_fk_cols), unlist(parent_key_cols)))
-      attr(child_tbl, "..keys..") <- NULL
+      child_tbl <- tbl[child_nm]
+      child_keys <- keys$children_keys[[child_nm]]
+      ## bind its key cols back and reshape
+      fks <-  filter(child_keys$own_keys$fks, parent_table == tbl_nm)
+      by_cols <-
+        with(fks, set_names(unlist(child_fk_cols), unlist(parent_key_cols)))
       child_tbl <- bind_cols(
         set_names(tbl[names(by_cols)], by_cols),
         child_tbl
       ) %>%
-        unnest(!!child_tbl_nm) %>%
+        unnest(!!child_nm) %>%
         # assuming duplicate rows make no sense in data bases
         # otherwise not robust
         unique()
-
-      ## set keys back in the dm
-      for(col in names(keys[[2]])) {
-        attr(child_tbl[[col]], "..keys..") <- keys[[2]][[col]]
-      }
-      pks <- keys[[1]]$pks$pk_col[[1]]
-      pk_arg <- call2("c", !!!syms(pks))
-      dm <<-
-        dm_add_tbl(dm, !!child_tbl_nm := child_tbl) %>%
-        dm_add_pk(!!sym(child_tbl_nm), !!pk_arg)
-      for (i in seq_len(nrow(keys[[1]]$fks))) {
-        fks <- keys[[1]]$fks$child_fk_cols[[i]]
-        child_fk_arg <- call2("c", !!!syms(fks))
-        dm <<- dm_add_fk(dm, !!sym(child_tbl_nm), !!child_fk_arg, !!sym(root))
-      }
-
-      ## recurse
-      populate_with_children(child_tbl_nm)
-      populate_with_parents(child_tbl_nm)
+      populate(child_tbl, child_nm, child_keys)
+      tbl[[child_nm]] <- NULL
     }
 
-    ## remove from source table and update dm
-    tbl[children_lgl] <- NULL
-    dm <<- replace_in_dm(root, tbl)
+    pks <- keys$own_keys$pks
+    pk_arg <- call2("c", !!!syms(pks))
+    dm <<- dm %>%
+      dm_add_tbl(!!tbl_nm := tbl) %>%
+      dm_add_pk(!!sym(tbl_nm), !!pk_arg)
   }
 
-  ## buid dm from root table
-  keys <- attr(data, "..keys..")
-  pks <- keys$pks$pk_col[[1]]
-  pk_arg <- call2("c", !!!syms(pks))
-  dm <- dm(!!root := data) %>%
-    dm_add_pk(!!sym(root), !!pk_arg)
+  add_fks <- function(keys) {
+    #browser()
+    fks <- keys$own_keys$fks
+    for (i in seq_len(NROW(fks))) {
+      table <- fks$child_table[[i]]
+      columns <- fks$child_fk_cols[[i]]
+      columns_arg <- call2("c", !!!syms(columns))
+      ref_table <- fks$parent_table[[i]]
+      dm <<- dm_add_fk(dm, !!sym(table), !!columns_arg, !!sym(ref_table))
+    }
+    for(key in keys$parents_keys) {
+      add_fks(key)
+    }
+    for(key in keys$children_keys) {
+      add_fks(key)
+    }
+  }
 
-  ## populate recursively
-  populate_with_parents(root)
-  populate_with_children(root)
-
-  ## remove attributes from root table
-  tbl <- dm[[root]]
-  attr(tbl, "..keys..") <- NULL
-  dm <- replace_in_dm(root, tbl)
-
+  populate(data, root, keys)
+  add_fks(keys)
   dm
 }
 
@@ -243,7 +172,7 @@ serialize_list_cols <- function(x) {
   x[children_lgl] <- map(x[children_lgl], \(col){
     map_chr(col, \(item) {
       #browser()
-      empty = !nrow(item)
+      empty = !NROW(item)
       if(empty) {
         # toJSON destroys empty tibbles so to save the format we keep a 1
         # row table and we tag as empty
@@ -252,16 +181,11 @@ serialize_list_cols <- function(x) {
         item[map_lgl(item, is.integer)] <- 1L
         item[map_lgl(item, is.double)] <- 1
         item[map_lgl(item, is.character)] <- "a"
-        item[map_lgl(item, is_tibble)] <- list(tibble(a = 1))
+        item[map_lgl(item, is_tibble)] <- list(tibble(a=1)[0])
         item[map_lgl(item, is_bare_list)] <- list(list(1))
       }
       jsonlite::toJSON(list(
-        #type = "child",
-        # toJSON destroys empty tibbles so we save the names to rebuild
-        # to be rigorous we'd have to save the type too
         empty = if(empty) TRUE,
-        # we repeat keys in each element, we could also store in 1st only
-        keys = attr(col, "..keys.."),
         data = serialize_list_cols(item)
       ),
       na = "string")
@@ -273,9 +197,6 @@ serialize_list_cols <- function(x) {
     row_dfs <- split(df, seq_len(nrow(df)))
     map_chr(row_dfs, \(row_df) {
       jsonlite::toJSON(list(
-        # we repeat keys in each element, we could also store in 1st only
-        #type = "parent",
-        keys = attr(col, "..keys.."),
         data = serialize_list_cols(row_df)
       ),
       na = "string")
@@ -323,7 +244,6 @@ unserialize_json_cols <- function(x) {
       data
       })
     keys <- unserialized_obj[[1]]$keys
-    attr(unserialized_col, "..keys..") <- keys
     unserialized_col
     })
 
@@ -332,7 +252,6 @@ unserialize_json_cols <- function(x) {
     unserialized_obj <- map(col, jsonlite::fromJSON)
     unserialized_col <- map_dfr(unserialized_obj, ~unserialize_json_cols(.x$data))
     keys <- unserialized_obj[[1]]$keys
-    attr(unserialized_col, "..keys..") <- keys
     unserialized_col
   })
 
