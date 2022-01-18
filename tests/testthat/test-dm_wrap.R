c234cb40e8697da096e3ff06a41f6f60f3ac1e82
Kirill Müller <krlmlr+r@mailbox.org>
2022-01-18 07:57:53 +0100

    Fix test

--- tests/testthat/test-dm_wrap.R
+++ tests/testthat/test-dm_wrap.R
@@ -2,11 +2,11 @@ test_that("`dm_wrap()` and `dm_unwrap()` work", {
   skip_if_remote_src()
 
   dm1 <- dm_for_filter()
-  dm_wrapped <- dm_wrap(dm1, tf_4)
+  dm_wrapped <- dm_wrap(dm1, tf_4, silent = TRUE)
   expect_length(dm_wrapped, 1)
   expect_equal(names(dm_wrapped), "tf_4")
   dm_unwrapped <- dm_unwrap(dm_wrapped, dm1)
-  expect_identical(dm_unwrapped, dm_unwrap(wrapped, dm1))
+  expect_identical(dm_unwrapped, dm_unwrap(dm_wrapped, dm1))
 
   # to tibble
   expect_snapshot({
