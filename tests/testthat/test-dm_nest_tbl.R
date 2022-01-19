f94efa4da6febef9c553dcc9914ae364829e5d9d
Kirill Müller <krlmlr+r@mailbox.org>
2022-01-19 07:00:51 +0100

    Remove duplicate test

--- tests/testthat/test-dm_nest_tbl.R
+++ tests/testthat/test-dm_nest_tbl.R
@@ -37,21 +37,3 @@ test_that("`dm_pack_tbl()` failure modes", {
   # has both parent and child
   expect_error(dm_pack_tbl(dm_for_filter(), tf_4), "not a terminal parent table")
 })
-
-test_that("`dm_pack_tbl()`, `dm_unpack_tbl()`, `dm_nest_tbl()`, `dm_unnest_tbl()` work", {
-  skip_if_remote_src()
-
-  expect_snapshot({
-    dm_packed <- dm_pack_tbl(dm_for_filter(), tf_1)
-    dm_packed
-
-    dm_packed_nested <- dm_nest_tbl(dm_packed, tf_2)
-    dm_packed_nested
-
-    dm_packed_nested_unnested <- dm_unnest_tbl(dm_packed_nested, tf_3, tf_2, ptype = dm_for_filter())
-    dm_packed_nested_unnested
-
-    dm_packed_nested_unnested_unpacked <- dm_unpack_tbl(dm_packed_nested_unnested, tf_2, tf_1, ptype = dm_for_filter())
-    dm_packed_nested_unnested_unpacked
-  })
-})
