7926f2cd9691eb7f143c13333a4e1dee8e764ba5
Tobias Schieferdecker <tobias@cynkra.com>
2022-03-31 10:24:10 +0200

    refactor and add dm_clone_pt()

--- R/disentangle.R
+++ R/disentangle.R
@@ -60,108 +60,8 @@ dm_disentangle <- function(dm, naming_template = NULL, quiet = FALSE) {
     return(dm)
   }
 
-  # get all incoming edges, recreate the vertices (parent tables) with more than 1 incoming edge
-  # as often as there are incoming edges and use one foreign key relation per vertex,
-  # unless there is just 1 path between the two vertices
-  all_edges_in <- map(
-    cycle_info$g[!cycle_info$no_cycles], ~ igraph::incident_edges(., V(.), mode = "in")
-  ) %>%
-    flatten()
-
-  num_edges_in <- map_int(all_edges_in, length)
-  multiple_edges_in <- all_edges_in[num_edges_in > 1]
-
-  # see https://github.com/cynkra/dm/pull/862#issuecomment-1070989387
-  # need to determine which edges are acceptable as they are
-  # (those still might need to be re-implemented if the vertex is replaced)
-  edge_participants <- map(multiple_edges_in, attr, "vnames") %>%
-    map(strsplit, split = "\\|") %>%
-    enframe(name = "parent_table", value = "child_table") %>%
-    unnest(child_table) %>%
-    mutate(child_table = map_chr(child_table, ~ .x[1])) %>%
-    # igraph::all_simple_paths() counts multiple edges between two vertices as one path:
-    # in this case we use the number of foreign keys
-    mutate(num_paths = map2_int(
-      parent_table,
-      child_table,
-      ~ max(
-        length(dm_get_all_fks_impl(dm) %>% filter(parent_table == .x, child_table == .y) %>% pull()),
-        length(igraph::all_simple_paths(cycle_info$full_g, .x, .y, mode = "all"))
-      )
-    ))
-
-  # one more tool to decide which PT to deal with first:
-  # check if there is a directed path between any two PT considered here
-  # choose to first deal with the PT with the lowest total number in this category
-  min_directed_paths_from <- if (length(unique(edge_participants$parent_table)) == 1) {
-    unique(edge_participants$parent_table)
-  } else {
-    crossing(
-      pt1 = unique(edge_participants$parent_table),
-      pt2 = unique(edge_participants$parent_table)
-    ) %>%
-      filter(pt1 != pt2) %>%
-      mutate(num_simple_paths = map2_int(
-        pt1,
-        pt2,
-        ~ length(igraph::all_simple_paths(cycle_info$full_g, .x, .y, mode = "out"))
-      )) %>%
-      group_by(pt1) %>%
-      summarize(num_paths_from = sum(num_simple_paths)) %>%
-      filter(num_paths_from == min(num_paths_from)) %>%
-      pull(pt1)
-  }
-  # 1. only those parent tables have to be recreated who have at least one entry
-  # with multiple simple paths between the related tables
-  # 2. also, if there is only one child table that has multiple paths to the parent table
-  # then the problem is often solved when not multiplying this parent table
-  # 3. in addition we should start here with the most "central" parent table,
-  # that means in this case the one with the highest number of paths between it and its child tables.
-  # Under certain circumstances disentangling this table resolves already the whole problem.
-  # example: entangled_dm_2()
-  # Having said that, there is no proof that this leads to the best results.
-  # Might want to revisit that later on.
-  action_needed_prep <- edge_participants %>%
-    filter(parent_table %in% min_directed_paths_from) %>%
-    group_by(parent_table) %>%
-    # in addition to the condition
-    summarize(
-      any_mult_path = any(num_paths > 1) && sum(num_paths > 1) != 1,
-      sum_num_paths_gt_1 = sum(num_paths[num_paths > 1]),
-      .groups = "drop"
-    ) %>%
-    filter(any_mult_path)
-  action_needed <- if (nrow(action_needed_prep) > 0) {
-    action_needed_prep %>%
-      filter(sum_num_paths_gt_1 == max(sum_num_paths_gt_1)) %>%
-      pull(parent_table)
-  } else {
-    character(0)
-  }
-
-  recipe <- edge_participants %>%
-    filter(parent_table %in% !!action_needed) %>%
-    left_join(dm_get_all_fks_impl(dm), by = c("parent_table", "child_table")) %>%
-    distinct() %>%
-    group_by(parent_table) %>%
-    mutate(create_new_table = num_paths > 1) %>%
-    arrange(desc(create_new_table)) %>%
-    mutate(
-      child_fk_cols_char = map_chr(child_fk_cols, ~ paste0(unlist(.x), collapse = "_")),
-      parent_key_cols_char = map_chr(parent_key_cols, ~ paste0(unlist(.x), collapse = "_"))
-    ) %>%
-    # create table names for new parent tables
-    # for those relations where no new parent table is required we're using the first newly created table
-    mutate(new_pt_name = if_else(
-      create_new_table,
-      glue::glue(create_new_pt_name(naming_template)),
-      NA_character_
-    )) %>%
-    mutate(new_pt_name = if_else(!create_new_table, new_pt_name[1], new_pt_name)) %>%
-    select(-num_paths) %>%
-    group_split()
-
-  new_dm <- rm_cycles(dm, recipe, quiet)
+  recipe <- create_recipe_clone_pt(dm, cycle_info, naming_template)
+  new_dm <- reduce(recipe, dm_clone_pt_impl, quiet, .init = dm)
 
   no_cycles <- check_cycles_in_components(new_dm)$no_cycles
   if (!all(no_cycles)) {
@@ -171,14 +71,25 @@ dm_disentangle <- function(dm, naming_template = NULL, quiet = FALSE) {
   }
 }
 
-rm_cycles <- function(dm, recipe, quiet) {
-  for (i in seq_len(length(recipe))) {
-    dm <- rm_cycle_one_pt(dm, recipe[[i]], quiet)
+dm_clone_pt <- function(
+    dm,
+    parent_table,
+    naming_template = NULL,
+    quiet = FALSE) {
+  table_name <- dm_tbl_name(dm, {{ parent_table }})
+  cycle_info <- check_cycles_in_components(dm)
+
+  recipe <- create_recipe_clone_pt(dm, cycle_info, naming_template, table_name)
+
+  if (!has_length(recipe)) {
+    message("No cycle detected, returning original `dm`.")
+    return(dm)
   }
-  dm
+
+  dm_clone_pt_impl(dm, recipe[[1]], quiet)
 }
 
-rm_cycle_one_pt <- function(dm, recipe_tbl, quiet) {
+dm_clone_pt_impl <- function(dm, recipe_tbl, quiet) {
   # remove all FKs from original parent table (otherwise dm_insert_zoomed will make use of them)
   new_dm <- dm_get_def(dm) %>%
     mutate(fks = if_else(table == unique(recipe_tbl$parent_table), list_of(new_fk()), fks)) %>%
@@ -261,4 +172,119 @@ has_endless_cycle <- function(g) {
     mutate(endless_cycle = map2_lgl(dist_lt_inf, dist_lt_inf_inv, ~ all(.x, .y))) %>%
     pull() %>%
     any()
+}
+
+create_recipe_clone_pt <- function(
+    dm,
+    cycle_info,
+    naming_template,
+    parent_table = NULL) {
+  # get all incoming edges, recreate the vertices (parent tables) with more than 1 incoming edge
+  # as often as there are incoming edges and use one foreign key relation per vertex,
+  # unless there is just 1 path between the two vertices
+  all_edges_in <- map(
+    cycle_info$g[!cycle_info$no_cycles], ~ igraph::incident_edges(., V(.), mode = "in")
+  ) %>%
+    flatten()
+
+  num_edges_in <- map_int(all_edges_in, length)
+  multiple_edges_in <- all_edges_in[num_edges_in > 1]
+
+  if (!is.null(parent_table)) {
+    if (parent_table %in% names(multiple_edges_in)) {
+      multiple_edges_in <- multiple_edges_in[parent_table]
+    } else {
+      return(list())
+    }
+  }
+
+  # see https://github.com/cynkra/dm/pull/862#issuecomment-1070989387
+  # need to determine which edges are acceptable as they are
+  # (those still might need to be re-implemented if the vertex is replaced)
+  edge_participants <- map(multiple_edges_in, attr, "vnames") %>%
+    map(strsplit, split = "\\|") %>%
+    enframe(name = "parent_table", value = "child_table") %>%
+    unnest(child_table) %>%
+    mutate(child_table = map_chr(child_table, ~ .x[1])) %>%
+    # igraph::all_simple_paths() counts multiple edges between two vertices as one path:
+    # in this case we use the number of foreign keys
+    mutate(num_paths = map2_int(
+      parent_table,
+      child_table,
+      ~ max(
+        length(dm_get_all_fks_impl(dm) %>% filter(parent_table == .x, child_table == .y) %>% pull()),
+        length(igraph::all_simple_paths(cycle_info$full_g, .x, .y, mode = "all"))
+      )
+    ))
+
+  # one more tool to decide which PT to deal with first:
+  # check if there is a directed path between any two PT considered here
+  # choose to first deal with the PT with the lowest total number in this category
+  min_directed_paths_from <- if (length(unique(edge_participants$parent_table)) == 1) {
+    unique(edge_participants$parent_table)
+  } else {
+    crossing(
+      pt1 = unique(edge_participants$parent_table),
+      pt2 = unique(edge_participants$parent_table)
+    ) %>%
+      filter(pt1 != pt2) %>%
+      mutate(num_simple_paths = map2_int(
+        pt1,
+        pt2,
+        ~ length(igraph::all_simple_paths(cycle_info$full_g, .x, .y, mode = "out"))
+      )) %>%
+      group_by(pt1) %>%
+      summarize(num_paths_from = sum(num_simple_paths)) %>%
+      filter(num_paths_from == min(num_paths_from)) %>%
+      pull(pt1)
+  }
+  # 1. only those parent tables have to be recreated who have at least one entry
+  # with multiple simple paths between the related tables
+  # 2. also, if there is only one child table that has multiple paths to the parent table
+  # then the problem is often solved when not multiplying this parent table
+  # 3. in addition we should start here with the most "central" parent table,
+  # that means in this case the one with the highest number of paths between it and its child tables.
+  # Under certain circumstances disentangling this table resolves already the whole problem.
+  # example: entangled_dm_2()
+  # Having said that, there is no proof that this leads to the best results.
+  # Might want to revisit that later on.
+  action_needed_prep <- edge_participants %>%
+    filter(parent_table %in% min_directed_paths_from) %>%
+    group_by(parent_table) %>%
+    # in addition to the condition
+    summarize(
+      any_mult_path = any(num_paths > 1) && sum(num_paths > 1) != 1,
+      sum_num_paths_gt_1 = sum(num_paths[num_paths > 1]),
+      .groups = "drop"
+    ) %>%
+    filter(any_mult_path)
+  action_needed <- if (nrow(action_needed_prep) > 0) {
+    action_needed_prep %>%
+      filter(sum_num_paths_gt_1 == max(sum_num_paths_gt_1)) %>%
+      pull(parent_table)
+  } else {
+    character(0)
+  }
+
+  edge_participants %>%
+    filter(parent_table %in% !!action_needed) %>%
+    left_join(dm_get_all_fks_impl(dm), by = c("parent_table", "child_table")) %>%
+    distinct() %>%
+    group_by(parent_table) %>%
+    mutate(create_new_table = num_paths > 1) %>%
+    arrange(desc(create_new_table)) %>%
+    mutate(
+      child_fk_cols_char = map_chr(child_fk_cols, ~ paste0(unlist(.x), collapse = "_")),
+      parent_key_cols_char = map_chr(parent_key_cols, ~ paste0(unlist(.x), collapse = "_"))
+    ) %>%
+    # create table names for new parent tables
+    # for those relations where no new parent table is required we're using the first newly created table
+    mutate(new_pt_name = if_else(
+      create_new_table,
+      glue::glue(create_new_pt_name(naming_template)),
+      NA_character_
+    )) %>%
+    mutate(new_pt_name = if_else(!create_new_table, new_pt_name[1], new_pt_name)) %>%
+    select(-num_paths) %>%
+    group_split()
 }
