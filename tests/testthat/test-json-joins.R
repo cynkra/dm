a42dcf5fb085dcde9b302d0872998be6b0c98ab2
moodymudskipper <moodymudskipper@users.noreply.github.com>
2022-04-12 08:00:48 +0000

    Auto-update from GitHub Actions
    Run: https://github.com/cynkra/dm/actions/runs/2153517571

--- tests/testthat/test-json-joins.R
+++ tests/testthat/test-json-joins.R
@@ -2,16 +2,16 @@ test_that("json joins work", {
   skip_if_not_installed("rlang", "0.99.0.9003")
 
   df1 <- tibble(col1 = 1:2, key = letters[1:2])
-  df2 <- tibble(col2 = 3:5, col3 = c("X", "Y", "Z"), key = letters[c(1,1:2)])
+  df2 <- tibble(col2 = 3:5, col3 = c("X", "Y", "Z"), key = letters[c(1, 1:2)])
 
   expect_snapshot({
     packed <- json_pack_join(df1, df2, by = "key")
     packed
     packed$df2
-    })
+  })
   expect_snapshot({
     nested <- json_nest_join(df1, df2, by = "key")
     nested
     nested$df2
-    })
+  })
 })
