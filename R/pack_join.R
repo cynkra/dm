23acb0dd7e6f40641652af161d3a105b8e6c6baa
Antoine Fabri <antoine.fabri@gmail.com>
2022-04-13 12:54:11 +0200

    Harmonize `pack_join()`
    Move ellipsis
    Don't forward dots to left_join
    Use check_dots_empty()
    Fix documentation of x,y
    seealso
    example

--- R/pack_join.R
+++ R/pack_join.R
@@ -6,14 +6,21 @@
 #' `pack_join()` returns all rows and columns in `x` with a new packed column
 #' that contains all matches from `y`.
 #' @inheritParams dplyr::nest_join
+#' @param x,y A pair of data frames or data frame extensions (e.g. a tibble).
 #'
 #' @export
-pack_join <- function(x, y, by = NULL, copy = FALSE, keep = FALSE, name = NULL, ...) {
+#' @seealso [dplyr::nest_join], [tidyr::pack]
+#' @examples
+#' df1 <- tibble(x = 1:3)
+#' df2 <- tibble(x = c(1, 1, 2), y = c("first", "second", "third"))
+#' pack_join(df1, df2)
+pack_join <- function(x, y, by = NULL, ..., copy = FALSE, keep = FALSE, name = NULL) {
   UseMethod("pack_join")
 }
 
 #' @export
-pack_join.data.frame <- function(x, y, by = NULL, copy = FALSE, keep = FALSE, name = NULL, ...) {
+pack_join.data.frame <- function(x, y, by = NULL, ..., copy = FALSE, keep = FALSE, name = NULL) {
+  check_dots_empty()
   name_var <- name %||% as_label(enexpr(y))
   if (!copy && inherits(y, "tbl_lazy"))
     abort("`x` and `y` must share the same src, set `copy` = TRUE (may be slow)")
@@ -23,7 +30,7 @@ pack_join.data.frame <- function(x, y, by = NULL, copy = FALSE, keep = FALSE, na
   x_nms <- colnames(x)
   name_var_unique <- last(make.unique(c(names(y_local), x_nms, name_var)))
   y_packed <- tidyr::pack(y_local, !!name_var_unique := -all_of(by2))
-  joined <- left_join(x, y_packed, by = by, copy = copy, keep = keep, ...)
+  joined <- left_join(x, y_packed, by = by, copy = copy, keep = keep)
   # overwrite existing column silently in x if collision, not very safe but consistent with dplyr::nest_join
   if (name_var %in% x_nms) {
     joined[[name_var]] <- NULL
