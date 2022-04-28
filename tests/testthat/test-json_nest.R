5f73597ec52d6f572e06e03406e6a15fb8438678
Antoine Fabri <antoine.fabri@gmail.com>
2022-04-28 16:22:55 +0200

    Add tests for remote methods for json_pack() and json_nest()

--- tests/testthat/test-json_nest.R
+++ tests/testthat/test-json_nest.R
@@ -5,3 +5,21 @@ test_that("`json_nest()` works", {
     nested
   })
 })
+
+test_that("`json_nest()` works remotely", {
+  # FIXME: add "mssql" when ready
+  skip_if_src_not("postgres")
+  con <- my_test_src()$con
+
+  withr::defer(
+    try(dbExecute(con, "DROP TABLE iris"))
+  )
+
+  dbWriteTable(con, "iris", iris)
+  iris_remote <- tbl(con, "iris")
+
+  expect_snapshot({
+    json_nest(iris_remote, Sepal = 1:2, Petal = starts_with("Petal"))
+    json_nest(iris_remote, Sepal = 1:2, Petal = starts_with("Petal"), .names_sep = ".")
+  })
+})
