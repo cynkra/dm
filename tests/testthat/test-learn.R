3ca7fd80f26ac404798bdb01a3749812dce584b2
Kirill Müller <krlmlr+r@mailbox.org>
2022-05-28 07:22:25 +0200

    Skip on df

--- tests/testthat/test-learn.R
+++ tests/testthat/test-learn.R
@@ -373,6 +373,8 @@ test_that("Learning from a specific schema in another DB for MSSQL works?", {
 })
 
 test_that("dm_meta() data model", {
+  skip_if_src("df")
+
   expect_snapshot({
     dm_meta(my_test_src()) %>%
       dm_paste(options = c("select", "keys", "color"))
