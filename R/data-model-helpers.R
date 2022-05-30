00f8bf010e967c95b8230dd3bd96f88a3f1f35dd
Kirill Müller <krlmlr+r@mailbox.org>
2022-05-30 10:28:09 +0200

    Remove dead code

--- R/data-model-helpers.R
+++ R/data-model-helpers.R
@@ -16,34 +16,3 @@ new_data_model <- function(tables, columns, references) {
     class = "data_model"
   )
 }
-
-get_datamodel_from_overview <- function(overview) {
-  new_data_model(
-    tables = datamodel_tables_from_overview(overview),
-    columns = datamodel_columns_from_overview(overview),
-    references = datamodel_references_from_overview(overview)
-  )
-}
-
-datamodel_tables_from_overview <- function(overview) {
-  overview %>%
-    distinct(table) %>%
-    add_column(segment = NA_character_, display = NA_character_) %>%
-    as.data.frame(stringsAsFactors = FALSE)
-}
-
-datamodel_columns_from_overview <- function(overview) {
-  overview %>%
-    select(column, type, table, key, ref, ref_col) %>%
-    mutate(key = as.numeric(key)) %>%
-    as.data.frame(stringsAsFactors = FALSE)
-}
-
-datamodel_references_from_overview <- function(overview) {
-  overview %>%
-    filter(!is.na(ref)) %>%
-    select(table, column, ref, ref_col) %>%
-    mutate(ref_id = as.numeric(row_number())) %>%
-    add_column(ref_col_num = 1) %>%
-    as.data.frame(stringsAsFactors = FALSE)
-}
