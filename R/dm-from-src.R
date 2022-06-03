c7dd287286d670a3e917ad3e33a3e914330f4ae4
Kirill Müller <krlmlr+r@mailbox.org>
2022-06-03 06:55:03 +0200

    API: New dm_from_src() replaces dm_from_con()

--- R/dm-from-src.R
+++ R/dm-from-src.R
@@ -5,8 +5,7 @@
 #' For Postgres and SQL Server databases, primary and foreign keys
 #' are imported from the database.
 #'
-#' @param src A \pkg{dplyr} table source object or a
-#'   [`DBI::DBIConnection-class`] object is accepted.
+#' @param con A [`DBI::DBIConnection-class`] or a `Pool` object.
 #' @param table_names
 #'   A character vector of the names of the tables to include.
 #' @param learn_keys
@@ -46,21 +45,17 @@
 #' dm_from_src(con)
 #'
 #' DBI::dbDisconnect(con)
-dm_from_src <- function(src = NULL, table_names = NULL, learn_keys = NULL,
+dm_from_con <- function(con = NULL, table_names = NULL, learn_keys = NULL,
                         ...) {
-  if (is_null(src)) {
-    # FIXME: Check empty arguments and ellipsis
-    return(empty_dm())
+
+  stopifnot(is(con, "DBIConnection") || inherits(con, "Pool"))
+
+  if (inherits(con, "Pool")) {
+    con <- pool_con <- pool::poolCheckout(con)
+    on.exit(pool::poolReturn(pool_con))
   }
 
-  if (inherits(src, "Pool")) {
-    src <- pool_src <- pool::poolCheckout(src)
-    on.exit(pool::poolReturn(pool_src))
-  }
-
-  # both DBI-Connection and {dplyr}-src object are accepted
-  src <- src_from_src_or_con(src)
-  con <- con_from_src_or_con(src)
+  src <- src_from_src_or_con(con)
 
   # FIXME: Get rid of legacy method once it works for all
 
@@ -126,6 +121,19 @@ dm_from_src <- function(src = NULL, table_names = NULL, learn_keys = NULL,
   }
 
   new_dm(tbls)
+}
+
+#' @export
+#' @keywords internal
+dm_from_src <- function(src = NULL, table_names = NULL, learn_keys = NULL,
+                        ...) {
+
+  if (is_null(src)) {
+    return(empty_dm())
+  }
+
+  deprecate_soft("0.3.0", "dm::dm_from_src()", "dm::dm_from_con()")
+  dm_from_con(con = con_from_src_or_con(src), table_names, learn_keys, ...)
 }
 
 quote_ids <- function(x, con, schema = NULL) {
