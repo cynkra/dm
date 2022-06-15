43069cfd2f64c96ea6265228ca9f635c1d4491f6
Tobias Schieferdecker <tobias@cynkra.com>
2022-06-15 11:50:17 +0200

    first iteration: #1076, code gen

---
+++ R/code-generation.R
@@ -0,0 +1,68 @@
+dm_f_list <- function(f_list = list()) {
+  structure(f_list, class = c("dm_f_list", class(f_list)))
+}
+
+add_call <- function(fn_list, fn_call) {
+  fn <- function(.) NULL
+  body(fn) <- fn_call
+  dm_f_list(append(fn_list, fn))
+}
+
+print.dm_f_list <- function(x, ...) {
+  if (is_empty(x)) {
+    invisible(x)
+  } else {
+    purrr::map_chr(x,  ~ body(.) %>% call_to_char()) %>%
+      paste0("  ", ., collapse = " %>%\n") %>% cat()
+    invisible(x)
+  }
+}
+
+call_to_char <- function(body) {
+  deparse(body) %>%
+    paste0(collapse = "") %>%
+    gsub("\\s+", " ", .)
+}
+
+add_foreign_key_code <- function(
+    fn_list = dm_f_list(),
+    table,
+    columns,
+    ref_table,
+    ref_columns = NULL,
+    ...,
+    check = FALSE,
+    on_delete = c("no_action", "cascade")
+  ) {
+  fn_call <- expr(
+    dm_add_fk(
+      .,
+      table = !!enexpr(table),
+      columns = !!enexpr(columns),
+      ref_table = !!enexpr(ref_table),
+      ref_columns = !!enexpr(ref_columns),
+      check = !!check,
+      on_delete = !!on_delete
+    )
+  )
+  add_call(fn_list, fn_call)
+}
+
+rm_foreign_key_code <- function(
+    fn_list = dm_f_list(),
+    table,
+    columns,
+    ref_table,
+    ref_columns = NULL
+) {
+  fn_call <- expr(
+    dm_rm_fk(
+      .,
+      table = !!enexpr(table),
+      columns = !!enexpr(columns),
+      ref_table = !!enexpr(ref_table),
+      ref_columns = !!enexpr(ref_columns)
+    )
+  )
+  add_call(fn_list, fn_call)
+}
