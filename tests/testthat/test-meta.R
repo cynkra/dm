db4616549f4fb5ce47a59904515e0d8756e2a489
Kirill Müller <krlmlr+r@mailbox.org>
2022-06-15 04:03:03 +0200

    Snapshot columns

--- tests/testthat/test-meta.R
+++ tests/testthat/test-meta.R
@@ -13,3 +13,21 @@ test_that("dm_meta() data model", {
       dm_paste(options = c("select", "keys", "color"))
   })
 })
+
+test_that("dm_meta(simple = TRUE) columns", {
+  tryCatch(
+    columns <-
+      my_db_test_src() %>%
+      dm_meta(simple = TRUE) %>%
+      .$columns %>%
+      collect(),
+    error = function(e) {
+      skip(conditionMessage(e))
+    }
+  )
+
+  path <- tempfile(fileext = ".csv")
+  readr::write_csv(columns, path, na = "")
+
+  expect_snapshot_file(path, name = "columns.csv", variant = my_test_src_name)
+})
