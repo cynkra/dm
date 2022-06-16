a884d47aadb9b6e1499de9fc4ee293d8dea5c1be
Tobias Schieferdecker <tobias@cynkra.com>
2022-06-16 08:16:43 +0200

    remove wrappers for code gen; only add_call() necessary

--- R/code-generation.R
+++ R/code-generation.R
@@ -4,7 +4,7 @@ dm_f_list <- function(f_list = list()) {
 
 add_call <- function(f_list, fn_call) {
   fn <- function(.) NULL
-  body(fn) <- fn_call
+  body(fn) <- enexpr(fn_call)
   dm_f_list(append(f_list, fn))
 }
 
@@ -28,43 +28,4 @@ call_to_char <- function(body) {
   deparse(body) %>%
     paste0(collapse = "") %>%
     gsub("\\s+", " ", .)
-}
-
-add_foreign_key_code <- function(f_list = dm_f_list(),
-                                 table,
-                                 columns,
-                                 ref_table,
-                                 ref_columns = NULL,
-                                 ...,
-                                 check = FALSE,
-                                 on_delete = c("no_action", "cascade")) {
-  fn_call <- expr(
-    dm_add_fk(
-      .,
-      table = !!enexpr(table),
-      columns = !!enexpr(columns),
-      ref_table = !!enexpr(ref_table),
-      ref_columns = !!enexpr(ref_columns),
-      check = !!check,
-      on_delete = !!on_delete
-    )
-  )
-  add_call(f_list, fn_call)
-}
-
-rm_foreign_key_code <- function(f_list = dm_f_list(),
-                                table,
-                                columns,
-                                ref_table,
-                                ref_columns = NULL) {
-  fn_call <- expr(
-    dm_rm_fk(
-      .,
-      table = !!enexpr(table),
-      columns = !!enexpr(columns),
-      ref_table = !!enexpr(ref_table),
-      ref_columns = !!enexpr(ref_columns)
-    )
-  )
-  add_call(f_list, fn_call)
 }
