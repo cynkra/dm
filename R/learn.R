44566cfeb90247979b641c35b549566926d5412e
Kirill Müller <krlmlr+r@mailbox.org>
2022-06-18 01:42:45 +0200

    Works better with new RMariaDB

--- R/learn.R
+++ R/learn.R
@@ -58,7 +58,7 @@ dm_learn_from_db <- function(dest, dbname = NA, schema = NULL, name_format = "{t
 
   from <-
     df_info$tables %>%
-    select(schema = table_schema, table = table_name) %>%
+    select(catalog = table_catalog, schema = table_schema, table = table_name) %>%
     pmap_chr(~ DBI::dbQuoteIdentifier(con, DBI::Id(...)))
 
   df_key_info <-
