f5203177253f6b11f3c56955b2fe197b58853349
Kirill Müller <krlmlr+r@mailbox.org>
2022-06-19 06:53:21 +0200

    Add tests for dm_meta() contents

--- tests/testthat/test-meta.R
+++ tests/testthat/test-meta.R
@@ -33,3 +33,51 @@ test_that("dm_meta(simple = TRUE) columns", {
 
   expect_snapshot_file(path, name = "columns.csv", variant = my_test_src_name)
 })
+
+test_that("dm_meta() contents", {
+  skip_if_src_not(c("mssql", "postgres", "maria"))
+
+  # produces a randomized schema name with a length of 4-10 characters
+  # consisting of the symbols in `reservoir`
+  random_schema <- function() {
+    reservoir <- c(letters, LETTERS, "'", "-", "_", as.character(0:9))
+    how_long <- sample(4:10, 1)
+    paste0(reservoir[sample(seq_len(length(reservoir)), how_long, replace = TRUE)], collapse = "")
+  }
+
+  schema_name <- random_schema()
+
+  # dm_learn_from_mssql() --------------------------------------------------
+  con_db <- my_test_con()
+
+  schema_name_q <- DBI::dbQuoteIdentifier(con_db, schema_name)
+
+  DBI::dbExecute(con_db, paste0("CREATE SCHEMA ", schema_name_q))
+
+  # create an object on the MSSQL-DB that can be learned
+  withr::defer({
+    try(walk(
+      order_of_deletion,
+      ~ try(dbExecute(con_db, paste0("DROP TABLE ", schema_name_q, ".", .x, " CASCADE")))
+    ))
+    try(dbExecute(con_db, paste0("DROP SCHEMA ", schema_name_q)))
+  })
+
+  dm_for_filter_copied <- copy_dm_to(con_db, dm_for_filter(), temporary = FALSE, schema = schema_name)
+  order_of_deletion <- c("tf_2", "tf_1", "tf_5", "tf_6", "tf_4", "tf_3")
+
+  meta <- dm_meta(con_db, schema = schema_name)
+
+  constraints <- dm_examine_constraints(meta, progress = FALSE)
+  expect_true(all(constraints$is_key))
+
+  expect_snapshot(variant = my_test_src_name, {
+    meta %>%
+      dm_get_tables() %>%
+      map(arrange_all) %>%
+      map(collect) %>%
+      jsonlite::toJSON(pretty = TRUE) %>%
+      gsub(schema_name, "schema_name", .) %>%
+      writeLines()
+  })
+})
