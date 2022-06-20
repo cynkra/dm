e04e8911e4166ce936f40fc4f06c5789c701e582
Tobias Schieferdecker <tobias@cynkra.com>
2022-06-20 17:28:23 +0200

    also use cg_input_object

--- R/code-generation.R
+++ R/code-generation.R
@@ -1,19 +1,30 @@
-dm_f_list <- function(f_list = list()) {
-  structure(f_list, class = c("dm_f_list", class(f_list)))
+new_cg_block <- function(cg_input_object = list(), cg_f_list = list()) {
+  structure(list(cg_input_object = cg_input_object, cg_f_list = cg_f_list), class = "cg_code_block")
 }
 
-add_call <- function(f_list, fn_call) {
+cg_add_call <- function(cg_block, fn_call) {
   fn <- function(.) NULL
   body(fn) <- enexpr(fn_call)
-  dm_f_list(append(f_list, fn))
+  new_cg_block(cg_block$cg_input_object, append(cg_block$cg_f_list, fn))
 }
 
 #' @export
-print.dm_f_list <- function(x, ...) {
-  show_dm_f_list(x)
+print.cg_code_block <- function(x, ...) {
+  show_cg_input_object(x, is_empty(x$cg_f_list))
+  show_cg_f_list(x$cg_f_list)
 }
 
-show_dm_f_list <- function(x) {
+show_cg_input_object <- function(x, has_f_call) {
+  if (has_f_call) {
+    if (!is_empty(x$cg_input_object)) {
+      cat(trimws(expr_deparse(x$cg_input_object)))
+    }
+  } else {
+    cat(paste0(trimws(expr_deparse(x$cg_input_object)), " %>%\n"))
+  }
+}
+
+show_cg_f_list <- function(x) {
   if (is_empty(x)) {
     invisible(x)
   } else {
@@ -25,7 +36,9 @@ show_dm_f_list <- function(x) {
 }
 
 call_to_char <- function(body) {
-  deparse(body) %>%
-    paste0(collapse = "") %>%
-    gsub("\\s+", " ", .)
+  trimws(expr_deparse(body)) %>% paste0(collapse = "\n  ")
+}
+
+cg_eval_block <- function(cg_block) {
+  freduce(eval(cg_block$cg_input_object), cg_block$cg_f_list)
 }
