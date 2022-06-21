4e1a1949bf08e035cdd8c1b30a4e1150efa22db4
Kirill Müller <krlmlr+r@mailbox.org>
2022-06-21 13:42:53 +0200

    table_name -> table_names

--- R/enum_ops.R
+++ R/enum_ops.R
@@ -15,31 +15,35 @@ enum_ops <- function(dm = NULL, ..., op_name = NULL) {
   }
 }
 
-enum_ops_dm_add_pk <- function(dm = NULL, ..., table_name = NULL) {
-  if (is.null(table_name)) {
+enum_ops_dm_add_pk <- function(dm = NULL, ..., table_names = NULL) {
+  if (is.null(table_names)) {
     # enumerate all tables that don't have a pk
-    list(table_name = names(dm))
+    list(table_names = names(dm))
   } else {
+    stopifnot(length(table_names) == 1)
+
     list2(
-      table_name = table_name,
-      !!!enum_ops_dm_add_pk_table(dm, ..., table_name = table_name)
+      table_names = table_names,
+      !!!enum_ops_dm_add_pk_table(dm, ..., table_names = table_names)
     )
   }
 }
 
-enum_ops_dm_add_pk_table <- function(dm = NULL, ..., table_name, column_names = NULL) {
+enum_ops_dm_add_pk_table <- function(dm = NULL, ..., table_names, column_names = NULL) {
+  stopifnot(length(table_names) == 1)
+
   if (is.null(column_names)) {
     # enumerate all columns that are not list
-    list(column_names = colnames(dm[[table_name]]))
+    list(column_names = colnames(dm[[table_names]]))
   } else if (length(column_names) > 1) {
     list2(
       column_names = column_names,
-      call = expr(dm_add_pk(., !!sym(table_name), c(!!!syms(column_names))))
+      call = expr(dm_add_pk(., !!sym(table_names), c(!!!syms(column_names))))
     )
   } else {
     list2(
       column_names = column_names,
-      call = expr(dm_add_pk(., !!sym(table_name), !!sym(column_names)))
+      call = expr(dm_add_pk(., !!sym(table_names), !!sym(column_names)))
     )
   }
 }
