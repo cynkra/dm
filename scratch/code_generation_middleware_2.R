715dec3f7ab32b10afb9979696fcc3bd3e5517c6
Kirill Müller <krlmlr+r@mailbox.org>
2022-06-21 14:00:58 +0200

    Usage

---
+++ scratch/code_generation_middleware_2.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(rlang)
+devtools::load_all()
+
+parent <- tibble(a = c(1L, 1:3), b = -1)
+child <- tibble(a = 1:4, c = 3)
+
+dm <- dm(parent, child)
+
+# This is done by the Shiny app
+ops <- enum_ops(dm)
+ops
+
+enum_ops(dm, table_names = "parent")
+enum_ops(dm, table_names = c("parent", "child"))
+
+enum_ops(dm, op_name = "dm_add_pk", table_names = "parent")
+
+final_ops <- enum_ops(dm, op_name = "dm_add_pk", table_names = "parent", column_names = "a")
+final_ops
+
+cg_block <- new_cg_block(quo(dm))
+
+cg_block %>%
+  cg_add_call(!!final_ops$call)
+
+cg_block %>%
+  cg_add_call(!!final_ops$call) %>%
+  cg_eval_block()
