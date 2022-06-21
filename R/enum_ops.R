70750ccc44f5af5819d6dfd5484f8aeb8d25565b
Kirill Müller <krlmlr+r@mailbox.org>
2022-06-21 13:59:03 +0200

    Allow selection of table_names in the beginning

--- R/enum_ops.R
+++ R/enum_ops.R
@@ -1,22 +1,47 @@
-enum_ops <- function(dm = NULL, ..., op_name = NULL) {
-  if (is.null(op_name)) {
-    list(
-      op_name = c(
-        "dm_add_pk", # only if not all tables have PK
-        "dm_add_fk",
-        NULL
-      )
-    )
-  } else {
-    list2(
+enum_ops <- function(dm = NULL, ..., table_names = NULL, op_name = NULL) {
+  if (!is.null(table_names) && !is.null(op_name)) {
+    return(list2(
+      table_names = table_names,
       op_name = op_name,
-      !!!exec(paste0("enum_ops_", op_name), dm = dm, ...)
-    )
+      !!!exec(paste0("enum_ops_", op_name), dm = dm, ..., table_names = table_names, op_name = op_name)
+    ))
   }
+
+  check_dots_empty()
+
+  out <- list(
+    table_names = names(dm),
+    op_name = c(
+      "dm_add_pk", # only if not all tables have PK
+      "dm_add_fk",
+      NULL
+    )
+  )
+
+  if (!is.null(table_names)) {
+    if (length(table_names) == 1) {
+      out$op_name <- setdiff(out$op_name, "dm_add_fk")
+    } else if (length(table_names) == 2) {
+      out$op_name <- setdiff(out$op_name, "dm_add_pk")
+    } else {
+      out$op_name <- setdiff(out$op_name, c("dm_add_pk", "dm_add_fk"))
+    }
+
+    out$table_names <- table_names
+  }
+
+  if (!is.null(op_name)) {
+    # FIXME: Restrict table_names based on selected operation
+
+    out$op_name <- op_name
+  }
+
+  out
 }
 
 enum_ops_dm_add_pk <- function(dm = NULL, ..., table_names = NULL) {
   if (is.null(table_names)) {
+    check_dots_empty()
     # enumerate all tables that don't have a pk
     list(table_names = names(dm))
   } else {
@@ -29,10 +54,11 @@ enum_ops_dm_add_pk <- function(dm = NULL, ..., table_names = NULL) {
   }
 }
 
-enum_ops_dm_add_pk_table <- function(dm = NULL, ..., table_names, column_names = NULL) {
+enum_ops_dm_add_pk_table <- function(dm = NULL, ..., op_name, table_names, column_names = NULL) {
   stopifnot(length(table_names) == 1)
 
   if (is.null(column_names)) {
+    check_dots_empty()
     # enumerate all columns that are not list
     list(column_names = colnames(dm[[table_names]]))
   } else if (length(column_names) > 1) {
