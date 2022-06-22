325fb27bcc0e1d0646b0ac57462e7a90617d4f73
Eugene Ha <12226767+egnha@users.noreply.github.com>
2022-06-22 11:35:32 +0200

    Snapshot tests

--- tests/testthat/test-enum-ops.R
+++ tests/testthat/test-enum-ops.R
@@ -1,33 +1,174 @@
-test_that("enum_ops() works", {
-  parent <- tibble(a = c(1L, 1:3), b = -1)
-  child <- tibble(a = 1:4, c = 3)
-  dm <- dm(parent, child)
-  expect_equal(
-    enum_ops(dm),
-    list(input = list(dm = dm),
-         single = list(op_name = "dm_rm_fk"),
-         multiple = list(table_names = names(dm)))
-  )
-  expect_equal(
-    enum_ops(dm, op_name = "dm_add_pk"),
-    list(input = list(dm = dm, op_name = "dm_add_pk"),
-         single = `names<-`(list(), character(0)),
-         multiple = list(table_names = names(dm)))
-  )
-  expect_equal(
-    enum_ops(dm, op_name = "dm_add_pk", table_names = "parent"),
-    list(input = list(dm = dm,
-                      table_names = "parent",
-                      column_names = NULL,
-                      op_name = "dm_add_pk"),
-         multiple = list(column_names = colnames(dm[["parent"]])))
-  )
-  expect_equal(
-    enum_ops(dm, op_name = "dm_add_pk", table_names = "parent", column_names = "a"),
-    list(input = list(dm = dm,
-                      table_names = "parent",
-                      column_names = "a",
-                      op_name = "dm_add_pk"),
-         call = quote(dm_add_pk(., parent, a)))
-  )
+# NB. The following changes were made to the snapshotted files:
+#
+# 1. Calls to `rlang::quo()` have been suppressed, since these calls capture
+#    an ephemeral environment that cannot be reproduced across separate test
+#    runs.
+#
+# 2. `cg_block` invocations are excluded, since they belong to a separate and
+#    independent testing unit.
+
+test_that("snapshot of code_generation_middleware.R is unchanged", {
+  expect_snapshot({
+    parent <- tibble(a = c(1L, 1:3), b = -1)
+    child <- tibble(a = 1:4, c = 3)
+
+    dm <- dm(parent, child)
+
+    # This is done by the Shiny app
+    ops <- enum_ops(dm)
+    ops
+
+    enum_ops(dm, op_name = "dm_add_pk")
+    enum_ops(dm, op_name = "dm_add_pk", table_names = "parent")
+
+    enum_ops(dm, op_name = "dm_add_pk", table_names = "parent", column_names = "a")$call
+  })
+})
+
+test_that("snapshot of code_generation_middleware_3.R is unchanged", {
+  expect_snapshot({
+    parent <- tibble(a = c(1L, 1:3), b = -1)
+    child <- tibble(a = 1:4, c = 3)
+
+    dm <- dm(parent, child)
+
+    # This is done by the Shiny app
+    ops <- enum_ops(dm)
+    ops
+
+    ops$multiple
+
+    # Choice options (the user can choose arbitrarily many):
+    user_choice <- list(
+      table_names = "parent"
+    )
+
+    # Called by the Shiny app, always with the same pattern:
+    ops_2 <- exec(
+      # Always enum_ops
+      enum_ops,
+      # Returned by the previous call to enum_ops()
+      !!!ops$input,
+      # Input selected by the user
+      !!!user_choice
+    )
+    # Same as:
+    # ops_2 <- enum_ops(dm = ops$input$dm, table_names = table_names)
+
+    ops_2
+
+    # Choice options (the user can choose arbitrarily many):
+    ops_2$multiple
+
+    # User's choice:
+    user_choice_2 <- list(column_names = c("a"))
+
+    # Called by the Shiny app, always with the same pattern:
+    ops_3 <- exec(
+      # Always enum_ops
+      enum_ops,
+      # Returned by the previous call to enum_ops()
+      !!!ops_2$input,
+      # Input selected by the user
+      !!!user_choice_2
+    )
+
+    ops_3
+
+    # Choice options (the user can choose only one of those):
+    ops_3$single
+
+    # User's choice:
+    user_choice_3 <- list(op_name = "dm_add_pk")
+
+    # Called by the Shiny app, always with the same pattern:
+    ops_4 <- exec(
+      # Always enum_ops
+      enum_ops,
+      # Returned by the previous call to enum_ops()
+      !!!ops_3$input,
+      # Input selected by the user
+      !!!user_choice_3
+    )
+
+    ops_4
+
+    # Final result:
+    ops_4$call
+  })
+})
+
+test_that("snapshot of code_generation_middleware_4.R is unchanged", {
+  expect_snapshot({
+    parent <- tibble(a = c(1L, 1:3), b = -1)
+    child <- tibble(a = 1:4, c = 3)
+
+    dm <-
+      dm(parent, child) %>%
+      dm_add_pk(parent, b)
+
+    # This is done by the Shiny app
+    ops <- enum_ops(dm)
+    ops
+
+    ops$multiple
+
+    # Choice options (the user can choose arbitrarily many):
+    user_choice <- list(
+      table_names = "parent"
+    )
+
+    # Called by the Shiny app, always with the same pattern:
+    ops_2 <- exec(
+      # Always enum_ops
+      enum_ops,
+      # Returned by the previous call to enum_ops()
+      !!!ops$input,
+      # Input selected by the user
+      !!!user_choice
+    )
+    # Same as:
+    # ops_2 <- enum_ops(dm = ops$input$dm, table_names = table_names)
+
+    ops_2
+
+    # Choice options (the user can choose arbitrarily many):
+    ops_2$multiple
+
+    # User's choice:
+    user_choice_2 <- list(column_names = c("a"))
+
+    # Called by the Shiny app, always with the same pattern:
+    ops_3 <- exec(
+      # Always enum_ops
+      enum_ops,
+      # Returned by the previous call to enum_ops()
+      !!!ops_2$input,
+      # Input selected by the user
+      !!!user_choice_2
+    )
+
+    ops_3
+
+    # Choice options (the user can choose only one of those):
+    ops_3$single
+
+    # User's choice:
+    user_choice_3 <- list(op_name = "dm_add_pk")
+
+    # Called by the Shiny app, always with the same pattern:
+    ops_4 <- exec(
+      # Always enum_ops
+      enum_ops,
+      # Returned by the previous call to enum_ops()
+      !!!ops_3$input,
+      # Input selected by the user
+      !!!user_choice_3
+    )
+
+    ops_4
+
+    # Final result:
+    ops_4$call
+  })
 })
