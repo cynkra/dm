c82271fb6766130af0d5abbaab9db749630dbd2c
Eugene Ha <12226767+egnha@users.noreply.github.com>
2022-06-23 14:06:34 +0200

    Add scratch/code_generation_middleware_2.R to snapshot tests

--- tests/testthat/test-enum-ops.R
+++ tests/testthat/test-enum-ops.R
@@ -25,6 +25,79 @@ test_that("snapshot of code_generation_middleware.R is unchanged", {
   })
 })
 
+test_that("snapshot of code_generation_middleware_2.R is unchanged", {
+  expect_snapshot({
+    parent <- tibble(a = c(1L, 1:3), b = -1)
+    child <- tibble(a = 1:4, c = 3)
+
+    dm <- dm(parent, child)
+
+    # This is done by the Shiny app
+    ops <- enum_ops(dm)
+    ops
+
+    ops$multiple
+
+    # Choice options (the user can choose arbitrarily many):
+    user_choice <- list(
+      table_names = "parent"
+    )
+
+    # Called by the Shiny app, always with the same pattern:
+    ops_2 <- exec(
+      # Always enum_ops
+      enum_ops,
+      # Returned by the previous call to enum_ops()
+      !!!ops$input,
+      # Input selected by the user
+      !!!user_choice
+    )
+    # Same as:
+    # ops_2 <- enum_ops(dm = ops$input$dm, table_names = table_names)
+
+    ops_2
+
+    # Choice options (the user can choose only one of those):
+    ops_2$single
+
+    # User's choice:
+    user_choice_2 <- list(op_name = "dm_add_pk")
+
+    # Called by the Shiny app, always with the same pattern:
+    ops_3 <- exec(
+      # Always enum_ops
+      enum_ops,
+      # Returned by the previous call to enum_ops()
+      !!!ops_2$input,
+      # Input selected by the user
+      !!!user_choice_2
+    )
+
+    ops_3
+
+    # Choice options (the user can choose arbitrarily many):
+    ops_3$multiple
+
+    # User's choice:
+    user_choice_3 <- list(column_names = c("a"))
+
+    # Called by the Shiny app, always with the same pattern:
+    ops_4 <- exec(
+      # Always enum_ops
+      enum_ops,
+      # Returned by the previous call to enum_ops()
+      !!!ops_3$input,
+      # Input selected by the user
+      !!!user_choice_3
+    )
+
+    ops_4
+
+    # Final result:
+    ops_4$call
+  })
+})
+
 test_that("snapshot of code_generation_middleware_3.R is unchanged", {
   expect_snapshot({
     parent <- tibble(a = c(1L, 1:3), b = -1)
