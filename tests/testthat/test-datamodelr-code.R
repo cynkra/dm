6e36302ad58d1d2facbc06011d80f6de09e1e13c
Kirill Müller <krlmlr+r@mailbox.org>
2022-07-07 17:08:01 +0200

    Prefer double quotes over single quotes

--- tests/testthat/test-datamodelr-code.R
+++ tests/testthat/test-datamodelr-code.R
@@ -8,10 +8,23 @@ test_that("datamodel-code for drawing", {
 })
 
 test_that("snapshot test for datamodelr code", {
-  data_model <- dm_get_data_model(dm_nycflights13(cycle = TRUE))
+  dm <- dm_nycflights13(cycle = TRUE)
+  data_model <- dm_get_data_model(dm)
 
   path <- tempfile(fileext = ".dot")
   writeLines(bdm_create_graph(data_model)$dot_code, path)
 
   expect_snapshot_file(path, "nycflights13.dot")
+})
+
+test_that("snapshot test for weird data models", {
+  dm <- dm("a b" = tibble(`c d` = 1)) %>%
+    dm_add_pk(`a b`, `c d`)
+
+  data_model <- dm_get_data_model(dm)
+
+  path <- tempfile(fileext = ".dot")
+  writeLines(bdm_create_graph(data_model)$dot_code, path)
+
+  expect_snapshot_file(path, "weird.dot")
 })
