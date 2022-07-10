c5bebd6c01979443f0a4c0359f802760d01f5f24
Kirill Müller <krlmlr+r@mailbox.org>
2022-07-10 05:20:06 +0200

    Copy test for dm()

--- tests/testthat/_snaps/dm.md
+++ tests/testthat/_snaps/dm.md
@@ -39,6 +39,45 @@
       Error in `dm()`:
       ! names2(quos)[is_dm] == "" is not TRUE
 
+# dm() works for adding tables
+
+    Code
+      dm(dm_for_filter(), tf_1 = data_card_1(), .name_repair = "check_unique")
+    Condition
+      Error in `dm()`:
+      ! Names must be unique.
+      x These names are duplicated:
+        * "tf_1" at locations 1 and 7.
+
+# dm() for adding tables with compound keys
+
+    Code
+      dm(dm_for_flatten(), res_flat = result_from_flatten()) %>% dm_paste(options = c(
+        "select", "keys"))
+    Message
+      dm::dm(
+        fact,
+        dim_1,
+        dim_2,
+        dim_3,
+        dim_4,
+        res_flat,
+      ) %>%
+        dm::dm_select(fact, fact, dim_1_key_1, dim_1_key_2, dim_2_key, dim_3_key, dim_4_key, something) %>%
+        dm::dm_select(dim_1, dim_1_pk_1, dim_1_pk_2, something) %>%
+        dm::dm_select(dim_2, dim_2_pk, something) %>%
+        dm::dm_select(dim_3, dim_3_pk, something) %>%
+        dm::dm_select(dim_4, dim_4_pk, something) %>%
+        dm::dm_select(res_flat, fact, dim_1_key_1, dim_1_key_2, dim_2_key, dim_3_key, dim_4_key, fact.something, dim_1.something, dim_2.something, dim_3.something, dim_4.something) %>%
+        dm::dm_add_pk(dim_1, c(dim_1_pk_1, dim_1_pk_2)) %>%
+        dm::dm_add_pk(dim_2, dim_2_pk) %>%
+        dm::dm_add_pk(dim_3, dim_3_pk) %>%
+        dm::dm_add_pk(dim_4, dim_4_pk) %>%
+        dm::dm_add_fk(fact, c(dim_1_key_1, dim_1_key_2), dim_1) %>%
+        dm::dm_add_fk(fact, dim_2_key, dim_2) %>%
+        dm::dm_add_fk(fact, dim_3_key, dim_3) %>%
+        dm::dm_add_fk(fact, dim_4_key, dim_4)
+
 # errors: duplicate table names, src mismatches
 
     Code
--- tests/testthat/test-dm.R
+++ tests/testthat/test-dm.R
@@ -11,6 +11,104 @@ test_that("dm() API", {
   })
 })
 
+test_that("dm() works for adding tables", {
+  # is a table added?
+  expect_identical(
+    length(dm_get_tables(dm(dm_for_filter(), data_card_1()))),
+    7L
+  )
+
+  # can I retrieve the tibble under its old name?
+  expect_equivalent_tbl(
+    dm(dm_for_filter(), data_card_1())[["data_card_1()"]],
+    data_card_1()
+  )
+
+  # can I retrieve the tibble under a new name?
+  expect_equivalent_tbl(
+    dm(dm_for_filter(), test = data_card_1())[["test"]],
+    data_card_1()
+  )
+
+  # use special names with :=
+  expect_identical(
+    names(dm(dm_for_filter(), dm := data_card_1(), repair := data_card_2())),
+    c(names(dm_for_filter()), "dm", "repair")
+  )
+
+  # we accept even weird table names, as long as they are unique
+  expect_equivalent_tbl(
+    dm(dm_for_filter(), . = data_card_1())[["."]],
+    data_card_1()
+  )
+
+  # do I avoid the warning when piping the table but setting the name?
+  expect_silent(
+    expect_equivalent_tbl(
+      dm_for_filter() %>% dm(new_name = data_card_1()) %>% pull_tbl(new_name),
+      data_card_1()
+    )
+  )
+
+  # adding more than 1 table:
+  # 1. Is the resulting number of tables correct?
+  expect_identical(
+    length(dm_get_tables(dm(dm_for_filter(), data_card_1(), data_card_2()))),
+    8L
+  )
+
+  # 2. Is the resulting order of the tables correct?
+  expect_identical(
+    src_tbls_impl(dm(dm_for_filter(), data_card_1(), data_card_2())),
+    c(src_tbls_impl(dm_for_filter()), "data_card_1()", "data_card_2()")
+  )
+
+  # Is an error thrown in case I try to give the new table an old table's name if `repair = "check_unique"`?
+  expect_snapshot(error = TRUE, {
+    dm(dm_for_filter(), tf_1 = data_card_1(), .name_repair = "check_unique")
+  })
+
+  # are in the default case (`repair = 'unique'`) the tables renamed (old table AND new table) according to "unique" default setting
+  expect_identical(
+    dm(dm_for_filter(), tf_1 = data_card_1(), .name_repair = "unique", .quiet = TRUE) %>% src_tbls_impl(),
+    c("tf_1...1", "tf_2", "tf_3", "tf_4", "tf_5", "tf_6", "tf_1...7")
+  )
+
+  expect_name_repair_message(
+    expect_equivalent_dm(
+      dm(dm_for_filter(), tf_1 = data_card_1(), .name_repair = "unique"),
+      dm_for_filter() %>%
+        dm_rename_tbl(tf_1...1 = tf_1) %>%
+        dm(tf_1...7 = data_card_1())
+    )
+  )
+
+  # can I use dm_select_tbl(), selecting among others the new table?
+  expect_silent(
+    dm(dm_for_filter(), tf_7_new = tf_7()) %>% dm_select_tbl(tf_1, tf_7_new, everything())
+  )
+
+  skip_if_not_installed("dbplyr")
+
+  # error in case table srcs don't match
+  expect_dm_error(
+    dm(dm_for_filter(), data_card_1_duckdb()),
+    "not_same_src"
+  )
+
+  # adding tables to an empty `dm` works for all sources
+  expect_equivalent_tbl(
+    dm(dm(), test = data_card_1_duckdb())$test,
+    data_card_1()
+  )
+})
+
+test_that("dm() for adding tables with compound keys", {
+  expect_snapshot({
+    dm(dm_for_flatten(), res_flat = result_from_flatten()) %>% dm_paste(options = c("select", "keys"))
+  })
+})
+
 test_that("dm() works for dm objects", {
   expect_equivalent_dm(
     dm(dm_for_filter()),
