3889ef296544e6b9573b864229b9ed8a210f487f
Tobias Schieferdecker <tobias@cynkra.com>
2022-07-14 18:19:06 +0200

    move dm_bind() and dm_add_tbl() to zzx-deprecated.R

--- R/zzx-deprecated.R
+++ R/zzx-deprecated.R
@@ -649,3 +649,100 @@ abort_rm_fk_col_missing <- function() {
 error_txt_rm_fk_col_missing <- function() {
   "Parameter `columns` has to be set. Pass `NULL` for removing all references."
 }
+
+#' @description
+#' `dm_add_tbl` is deprecated as of dm 1.0.0, because the same functionality
+#' is offered by [dm()] with `.name_repair = "unique"`.
+#' @rdname deprecated
+#' @keywords internal
+#' @export
+dm_add_tbl <- function(dm, ..., repair = "unique", quiet = FALSE) {
+  deprecate_soft("1.0.0", "dm_add_tbl()", "dm()",
+                 details = 'Use `.name_repair = "unique"` if necessary.'
+  )
+
+  check_not_zoomed(dm)
+
+  new_names <- names(exprs(..., .named = TRUE))
+  new_tables <- list2(...)
+
+  check_new_tbls(dm, new_tables)
+
+  old_names <- src_tbls_impl(dm)
+  names_list <- repair_table_names(old_names, new_names, repair, quiet)
+  # rename old tables in case name repair changed their names
+
+  dm <- dm_select_tbl_impl(dm, names_list$new_old_names)
+  dm_add_tbl_impl(dm, new_tables, names_list$new_names)
+}
+
+#' @description
+#' `dm_bind()`  is deprecated as of dm 1.0.0, because the same functionality
+#' is offered by [dm()].
+#'
+#' @rdname deprecated
+#' @keywords internal
+#' @export
+dm_bind <- function(..., repair = "check_unique", quiet = FALSE) {
+  deprecate_soft("1.0.0", "dm_bind()", "dm()")
+
+  dms <- list2(...)
+
+  new_def <- dm_bind_impl(dms, repair, quiet)
+  new_dm3(new_def)
+}
+
+dm_bind_impl <- function(dms, repair, quiet, repair_arg = "", caller = caller_env()) {
+  if (length(dms) == 0) {
+    return(new_dm_def())
+  }
+
+  walk(dms, check_dm)
+  walk(dms, check_not_zoomed)
+  if (!all_same_source(map(dms, dm_get_tables_impl) %>% flatten())) {
+    abort_not_same_src(dm_bind = TRUE)
+  }
+
+  # repair table names
+  table_names <- map(dms, src_tbls_impl) %>% flatten_chr()
+  new_table_names <- vec_as_names(
+    table_names,
+    repair = repair,
+    quiet = quiet,
+    repair_arg = repair_arg,
+    call = caller
+  )
+  # need to individually rename tables for each `dm`
+  ntables_dms <- map(dms, length)
+  dms_indices <- map(ntables_dms, seq_len)
+  renaming_recipe <- map2(
+    dms_indices,
+    lag(cumsum(ntables_dms), default = 0),
+    ~ set_names(.x, new_table_names[.x + .y])
+  )
+
+  dms_renamed <- map2(dms, renaming_recipe, bind_rename_tbl)
+
+  new_defs <- map(dms_renamed, dm_get_def)
+  vec_rbind(!!!new_defs)
+}
+
+bind_rename_tbl <- function(dm, renamed) {
+  if (length(dm) == 0) {
+    return(dm)
+  }
+
+  def <- dm_get_def(dm)
+
+  renamed_names <- set_names(def$table[renamed], names(renamed))
+
+  def %>%
+    bind_filter_recode_table_def(renamed) %>%
+    filter_recode_table_fks(renamed_names) %>%
+    new_dm3()
+}
+
+bind_filter_recode_table_def <- function(def, renamed) {
+  def$table[renamed] <- names(renamed)
+  def
+}
