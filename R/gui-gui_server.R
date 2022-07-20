b713a6d8c581933ae48f223212aa5a56c1ce7131
Kirill Müller <krlmlr+r@mailbox.org>
2022-07-20 07:03:14 -0400

    Style

--- R/gui-gui_server.R
+++ R/gui-gui_server.R
@@ -2,6 +2,7 @@ g_table_names <- character()
 
 
 gui_server <- function(input, output, session) {
+
   # debug <- isTRUE(getOption("shiny.fullstacktrace"))
   # if (!debug) {
   #   print <- function(x, ...) invisible(x)
