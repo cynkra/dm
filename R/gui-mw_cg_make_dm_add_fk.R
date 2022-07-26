98405526876fe2bfb495272e1157749b11dc75e6
Kirill Müller <krlmlr+r@mailbox.org>
2022-07-26 06:43:53 -0400

    GUI improvements

--- R/gui-mw_cg_make_dm_add_fk.R
+++ R/gui-mw_cg_make_dm_add_fk.R
@@ -9,26 +9,41 @@ mw_cg_make_dm_add_fk <- function(dm, ..., table_names = NULL,
   other_table <- dm[[other_table_name]]
 
   stopifnot(length(column_names) >= 1)
-  stopifnot(length(column_names) == length(other_column_names))
+  stopifnot(length(other_column_names) %in% c(0, length(column_names)))
   stopifnot(column_names %in% colnames(table))
   stopifnot(other_column_names %in% colnames(other_table))
 
   # Call generation
   if (length(column_names) == 1) {
     column_names_sym <- sym(column_names)
-    other_column_names_sym <- sym(other_column_names)
   } else {
     column_names_sym <- expr(c(!!!syms(column_names)))
-    other_column_names_sym <- expr(c(!!!syms(other_column_names)))
   }
 
-  list(
-    call = expr(dm_add_fk(
-      .,
-      !!sym(table_name),
-      !!column_names_sym,
-      !!sym(other_table_name),
-      !!other_column_names_sym
-    ))
-  )
+  if (length(other_column_names) == 0) {
+    list(
+      call = expr(dm_add_fk(
+        .,
+        !!sym(table_name),
+        !!column_names_sym,
+        !!sym(other_table_name)
+      ))
+    )
+  } else {
+    if (length(other_column_names) == 1) {
+      other_column_names_sym <- sym(other_column_names)
+    } else {
+      other_column_names_sym <- expr(c(!!!syms(other_column_names)))
+    }
+
+    list(
+      call = expr(dm_add_fk(
+        .,
+        !!sym(table_name),
+        !!column_names_sym,
+        !!sym(other_table_name),
+        !!other_column_names_sym
+      ))
+    )
+  }
 }
--- R/gui-mw_cg_make_dm_set_colors.R
+++ R/gui-mw_cg_make_dm_set_colors.R
@@ -5,12 +5,18 @@ mw_cg_make_dm_set_colors <- function(dm, ..., table_names = NULL, color_name = N
   stopifnot(is.character(table_names))
   stopifnot(table_names %in% names(dm))
 
-  table_color <- set_names(table_names, color_name)
+  if (length(table_names) == 1) {
+    table_sym <- sym(table_names)
+  } else {
+    table_sym <- expr(c(!!!syms(table_names)))
+  }
+
+  table_syms <- exprs(!!sym(color_name) := !!table_sym)
 
   list(
     call = expr(dm_set_colors(
       .,
-      !!!syms(table_color)
+      !!!table_syms
     ))
   )
 }
