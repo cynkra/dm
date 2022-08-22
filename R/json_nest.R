d1fb8682a07fe4528e79dd17fc04872700ed6db5
moodymudskipper <moodymudskipper@users.noreply.github.com>
2022-08-22 12:41:04 +0000

    Auto-update from GitHub Actions
    Run: https://github.com/cynkra/dm/actions/runs/2903923060

--- R/json_nest.R
+++ R/json_nest.R
@@ -131,7 +131,7 @@ sql_json_nest.PqConnection <- function(con, cols, names_sep, packed_col, id_cols
     glue_collapse(" AND ")
 
   # compute subqueries for each nested column
-  nest_col_queries <- imap_chr(nest_cols, ~{
+  nest_col_queries <- imap_chr(nest_cols, ~ {
     cols <- toString(DBI::dbQuoteLiteral(con, .x))
     alias <- DBI::dbQuoteLiteral(con, .y)
     glue::glue("(SELECT {cols} FROM {tbl_name} WHERE ({joins}) FOR JSON PATH) AS {alias}")
