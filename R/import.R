4448f904ee2d5989491c03731213c2aa2b76d12a
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-09-06 11:10:32 +0200

    Fix raw `NAMESPACE` for `{glue}`
    There is no function called `glue::collapse()`.
    
    This causes problems for lintr:
    https://github.com/r-lib/lintr/issues/1529

--- R/import.R
+++ R/import.R
@@ -12,7 +12,7 @@
 #' @rawNamespace import(purrr, except = c(list_along, rep_along, invoke, modify, as_function, flatten_dbl, flatten_lgl, flatten_int, flatten_raw, flatten_chr, splice, flatten, prepend, `%@%`, `%||%`))
 #' @rawNamespace import(magrittr, except = c(set_names))
 #' @importFrom methods is
-#' @rawNamespace import(glue, except = c(collapse))
+#' @rawNamespace import(glue)
 #' @importFrom igraph V E
 #' @importFrom utils head tail str packageVersion
 #' @import lifecycle
