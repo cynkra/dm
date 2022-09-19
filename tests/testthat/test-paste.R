910ac1447053361224aa84c85208108b6337ea63
krlmlr <krlmlr@users.noreply.github.com>
2022-09-19 09:55:53 +0000

    Auto-update from GitHub Actions
    Run: https://github.com/cynkra/dm/actions/runs/3081381055

--- tests/testthat/test-paste.R
+++ tests/testthat/test-paste.R
@@ -84,10 +84,10 @@ test_that("output", {
 
     "no error for factor column that leads to code with width > 500"
     dm(tibble(a = factor(levels = expand.grid(
-      letters, as.character(1:5)) %>%
-        transmute(x = paste0(Var1, Var2)) %>%
-        pull()))
-      ) %>%
+      letters, as.character(1:5)
+    ) %>%
+      transmute(x = paste0(Var1, Var2)) %>%
+      pull()))) %>%
       dm_paste(options = "tables")
   })
 })
