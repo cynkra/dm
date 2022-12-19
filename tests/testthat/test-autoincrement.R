80b29ec971430282e2e53c651f723a5c2744141b
Tobias Schieferdecker <tobias@cynkra.com>
2022-12-19 16:39:35 +0100

    basic test for autoincrement feature

---
+++ tests/testthat/test-autoincrement.R
@@ -0,0 +1,18 @@
+x <- tibble::tibble(
+  x_id = integer(0L),
+  x_data = character(0L),
+)
+y <- tibble::tibble(
+  y_id = integer(0L),
+  x_id = integer(0L),
+  y_data = character(0L),
+)
+
+dm <- dm(x, y) %>%
+  dm_add_pk(x, x_id, autoincrement = TRUE) %>%
+  dm_add_pk(y, y_id, autoincrement = FALSE) %>%
+  dm_add_fk(y, x_id, x)
+
+test_that("autoincrement produces valid R code", {
+  expect_snapshot(dm)
+})
