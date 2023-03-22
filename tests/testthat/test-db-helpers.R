f00c3518a7dc86d5c3abc92d4080428ac2125c0d
owenjonesuob <21007837+owenjonesuob@users.noreply.github.com>
2023-03-22 10:12:01 +0000

    Add tests for find_name_clashes()

--- tests/testthat/test-db-helpers.R
+++ tests/testthat/test-db-helpers.R
@@ -183,3 +183,25 @@ test_that("DB helpers work for other DBMS than MSSQL or Postgres", {
     class = "arg_not"
   )
 })
+
+test_that("find name clashes", {
+
+  # If all old names change to different new names...
+  res <- find_name_clashes(
+    c("one", "two", "three"),
+    c("uno", "dos", "tres")
+  )
+  # ... we shouldn't get anything
+  expect_length(res, 0)
+
+
+  # If multiple old names change to the same new name...
+  res <- find_name_clashes(
+    c("one", "two", "three"),
+    c("uno", "uno", "tres")
+  )
+  # We should get a list, with one element per "clashing" new name
+  expect_named(res, "uno")
+  expect_equal(res[["uno"]], c("one", "two"))
+
+})
