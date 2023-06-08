7e939ef2cc99942feb50af543d465e17d3296e61
jangorecki <j.gorecki@wit.edu.pl>
2023-06-08 15:47:37 +0200

    new fun db_duckdb_csv to operate on remote csv files

--- R/csv.R
+++ R/csv.R
@@ -78,3 +78,150 @@ dm_export_csv <- function(x, path) {
   lapply(tbls, tbl_export_csv, x)
   invisible(NULL)
 }
+
+#' @name dm_duckdb_csv
+#'
+#' @title Create dm object based on remote csv files
+#'
+#' @description
+#' Function takes path to directory where multiple tables are stored in csv
+#' files and creates `dm` from those tables. It uses `duckdb` to create remote
+#' `tbl` objects which are then used as tables for `dm` object.
+#'
+#' @details
+#' Function operates on remote tables, therefore data are not being loaded to R.
+#' The are following consequences of that:
+#' \itemize{
+#'   \item{ Size of data can exceed machine's memory limit. }
+#'   \item{ Queries run against csv are likely to be slower than if data are
+#'   loaded into R's memory. }
+#'   \item{ Performance of queries can be improved by partitioning csv files. }
+#'   \item{ Data can be inserted or updated in csv files directly and changes will be
+#'   reflected on the next query. There is no need to reload data. }
+#' }
+#'
+#' @param path Path to directory from where csv files has to be read from.
+#'   Directory can have sub-directories in case of partitioned data.
+#' @param conn Connection object to duckdb. See [duckdb::duckdb()] for details.
+#'
+#' @section Partitioning:
+#' Tables provided in csv files can be partitioned. This will likely bring
+#' significant performance improvement in case when queries do not need to
+#' access data from all partitions. Structure for partitioned data in csv files
+#' must follow \href{https://duckdb.org/docs/data/partitioning/hive_partitioning}{duckdb's Hive Partitioning} documentation.
+#' Partitioning can support nested partitioning, but not recursively nested.
+#' So the following are supported:
+#' \preformatted{
+#'   path/flights/year=2013/*.csv
+#'   path/flights/year=2014/*.csv
+#'
+#'   path/transactions/year=2013/month=1/*.csv
+#'   path/transactions/year=2013/month=2/*.csv
+#' }
+#'
+#' @return A `dm` object.
+#'
+#' @export
+#' @examples
+#' ## create exanple source data directory
+#' path <- file.path(tempdir(), "data")
+#' dir.create(path, showWarnings=FALSE)
+#' x <- dm_nycflights13()
+#' tbl_export_csv <- function(tbl, dm) {
+#'   file <- file.path(path, paste(tbl, "csv", sep="."))
+#'   utils::write.csv(dm[[tbl]], file=file, row.names=FALSE)
+#' }
+#' invisible(lapply(names(x), tbl_export_csv, x))
+#' list.files(path)
+#'
+#' ## create dm from remote csv files via duckdb
+#' conn <- DBI::dbConnect(duckdb::duckdb(), dbdir=":memory:")
+#' d <- dm_duckdb_csv(path, conn)
+#' d
+#'
+#' ## cleanup db connection
+#' DBI::dbDisconnect(conn)
+dm_duckdb_csv <- function(path = ".", conn) {
+  if (!is.character(path) || length(path)!=1L || is.na(path))
+    stop("Argument 'path' must be non-NA scalar character")
+  if (!dir.exists(path))
+    stop("Argument 'path' must point to an existing directory")
+  if (!inherits(conn, "duckdb_connection"))
+    stop("Argument 'conn' must be an object of 'duckdb_connection' class")
+
+  ## single csv per table: path/table.csv
+  files <- list.files(path=path, pattern="\\.csv$")
+  ## hive partitioned csv table:
+  ## path/table/year=2012/*.csv
+  ## path/table/year=2012/month=1/*.csv
+  dirs <- list.dirs(path=path, recursive=FALSE, full.names=FALSE)
+  if (!(length(files) + length(dirs)))
+    stop("In provided 'path' there are no csv files neither sub-directories of partitioned csv files")
+
+  ## partitions handling
+  ## validate structure, dir names, csv files
+  ## determine if there is nested partitioning: year=2001/month=1
+  if (length(dirs)) {
+    validate.partition.names <- function(p) {
+      p1 <- strsplit(p, "=", fixed=TRUE)
+      if (any(lengths(p1)!=2L)) ## when not: LHS=RHS
+        stop("Partition sub-directories must be named as column=value")
+      if (length(unique(sapply(p1, `[[`, 1L)))!=1L) ## when: year=2012, jahr=2013
+        stop("Partition sub-directories must specify same column for all partitions")
+    }
+    get.partition.level <- function(p, d, path) {
+      p1.path <- file.path(path, d, p)
+      part2 <- list.dirs(path=p1.path, recursive=FALSE, full.names=FALSE)
+      if (!length(part2)) {
+        ## no nested partitions, expect csv file(s)
+        p1.files <- list.files(path=p1.path, pattern="\\.csv$")
+        ## lets assume empty partitions are fine, but there must be at least one non-empty to obtain table schema
+        if (!length(p1.files))
+          stop("There must be at least one csv file in partitions sub-directory in ", p1.path)
+        1L
+      } else {
+        ## expect nested partitions sub-dirs, no csv
+        p1.files <- list.files(path=p1.path, pattern="\\.csv$")
+        if (length(p1.files))
+          stop("There must be no csv files when there exists nested partitions sub-directories in ", p1.path)
+        validate.partition.names(part2)
+        p2.path = file.path(p1.path, part2)
+        p2.files <- list.files(path=p2.path, pattern="\\.csv$")
+        if (!length(p2.files))
+          stop("There must be at least one csv file in partitions sub-directory in ", p2.path)
+        2L
+      }
+    }
+
+    p.levels <- vapply(stats::setNames(nm=dirs), function(d, path) {
+      tbl.path <- file.path(path, d)
+      part1 <- list.dirs(path=tbl.path, recursive=FALSE, full.names=FALSE)
+      if (!length(part1))
+        stop("There must be partitions sub-directories in", tbl.path)
+      validate.partition.names(part1)
+      tbl.partitions <- vapply(stats::setNames(nm=part1), get.partition.level, 0L, d, path)
+      ## mixed levels nested partitioning for one table not allowed
+      ## when: flights/year=2013/data.csv, flights/year=2014/month=1/data.csv
+      lvl = unique(tbl.partitions)
+      if (length(lvl)!=1L)
+        stop("Mixed levels of nested partitioning for one table are not allowed, fix structure for ", d)
+      lvl
+    }, 0L, path)
+  } else {
+    p.levels = integer()
+  }
+
+  ## source structure metadata table
+  df <- as.data.frame(list(
+    tbl = c(tools::file_path_sans_ext(basename(files)), dirs),
+    partitioning = c(rep.int(0L, length(files)), p.levels),
+    path = file.path(path, c(
+      files,
+      file.path(dirs, "*", ifelse(p.levels==1L, "*.csv", file.path("*","*.csv")))
+    ))
+  ))
+  df$duckdb_src <- paste0("read_csv('", df$path, "', hive_partitioning=", ifelse(df$partitioning > 0L, 1L, 0L), ", delim=',', header=True, auto_detect=True)")
+  df <- df[order(df$tbl), , drop=FALSE]
+
+  new_dm(lapply(stats::setNames(df$duckdb_src, df$tbl), dplyr::tbl, src=conn))
+}
--- tests/testthat/test-csv.R
+++ tests/testthat/test-csv.R
@@ -32,3 +32,183 @@ test_that("dm_import_csv() read csv files into dm", {
     c("airlines", "airports", "flights", "planes", "weather")
   )
 })
+
+
+test_that("dm_duckdb_csv() examples", {
+
+  ## create exanple source data directory
+  path <- file.path(tempdir(), "data")
+  dir.create(path, showWarnings=FALSE)
+  x <- dm_nycflights13()
+  tbl_export_csv <- function(tbl, dm) {
+    file <- file.path(path, paste(tbl, "csv", sep="."))
+    utils::write.csv(dm[[tbl]], file=file, row.names=FALSE)
+  }
+  invisible(lapply(names(x), tbl_export_csv, x))
+  expect_equal(
+    list.files(path),
+    c("airlines.csv", "airports.csv", "flights.csv", "planes.csv", "weather.csv")
+  )
+
+  ## create dm from remote csv files via duckdb
+  conn <- DBI::dbConnect(duckdb::duckdb(), dbdir=":memory:")
+  d <- dm_duckdb_csv(path, conn)
+  expect_true(inherits(d, "dm"))
+  expect_equal(
+    names(d),
+    c("airlines", "airports", "flights", "planes", "weather")
+  )
+
+  ## cleanup db connection
+  invisible(DBI::dbDisconnect(conn))
+})
+
+test_that("dm_duckdb_csv() handles partitioning", {
+  ## 3 tables, 2 partitioned, one not
+  path <- file.path(tempdir(), "data-partitioning")
+  ## iris, partitioned by Species
+  x <- split(datasets::iris, datasets::iris$Species)
+  lapply(names(x), function(tbl) {
+    file <- file.path(path, "iris", paste("Species", tbl, sep="="), "data.csv")
+    dir.create(dirname(file), recursive=TRUE, showWarnings=FALSE)
+    utils::write.csv(x[[tbl]], file=file, row.names=FALSE)
+  })
+  ## Titanic, partitioned by Sex
+  ti <- as.data.frame(datasets::Titanic)
+  x <- split(ti, ti$Sex)
+  lapply(names(x), function(tbl) {
+    file <- file.path(path, "titanic", paste("Sex", tbl, sep="="), "data.csv")
+    dir.create(dirname(file), recursive=TRUE, showWarnings=FALSE)
+    utils::write.csv(x[[tbl]], file=file, row.names=FALSE)
+  })
+  utils::write.csv(datasets::state.x77, file=file.path(path, "state.csv"), row.names=row.names(state.x77))
+  conn <- DBI::dbConnect(duckdb::duckdb(), dbdir=":memory:")
+  d <- dm_duckdb_csv(path, conn)
+  expect_true(inherits(d, "dm"))
+  expect_equal(
+    names(d),
+    c("iris","state","titanic")
+  )
+  expect_equal(
+    sapply(lapply(lapply(d, dplyr::count), collect), `[[`, "n"),
+    c(iris=nrow(iris), state=nrow(state.x77), titanic=nrow(ti))
+  )
+  DBI::dbDisconnect(conn)
+})
+
+test_that("dm_duckdb_csv() handles nested partitioning", {
+  ## 2 tables, 1 nested partitioned, one not
+  path <- file.path(tempdir(), "data-nested-partitioning")
+  ## Titanic, partitioned by Survived, Sex
+  ti <- as.data.frame(datasets::Titanic)
+  x <- lapply(split(ti, ti$Survived), function(y) split(y, y$Sex))
+  lapply(names(x), function(sur) {
+    lapply(names(x[[sur]]), function(sex) {
+      file <- file.path(path, "titanic", paste("Survived", sur, sep="="), paste("Sex", sex, sep="="), "data.csv")
+      dir.create(dirname(file), recursive=TRUE, showWarnings=FALSE)
+      utils::write.csv(x[[sur]][[sex]], file=file, row.names=FALSE)
+    })
+  })
+  utils::write.csv(datasets::state.x77, file=file.path(path, "state.csv"), row.names=row.names(state.x77))
+  conn <- DBI::dbConnect(duckdb::duckdb(), dbdir=":memory:")
+  d <- dm_duckdb_csv(path, conn)
+  expect_true(inherits(d, "dm"))
+  expect_equal(
+    names(d),
+    c("state","titanic")
+  )
+  expect_equal(
+    sapply(lapply(lapply(d, dplyr::count), collect), `[[`, "n"),
+    c(state=nrow(state.x77), titanic=nrow(ti))
+  )
+  DBI::dbDisconnect(conn)
+})
+
+test_that("dm_duckdb_csv() handles mixed partitioning", {
+  ## 3 tables, 1 partitioned, 1 nested partitioned, one not
+  path <- file.path(tempdir(), "data-mixed-partitioning")
+  ## iris, partitioned by Species
+  x <- split(datasets::iris, datasets::iris$Species)
+  lapply(names(x), function(tbl) {
+    file <- file.path(path, "iris", paste("Species", tbl, sep="="), "data.csv")
+    dir.create(dirname(file), recursive=TRUE, showWarnings=FALSE)
+    utils::write.csv(x[[tbl]], file=file, row.names=FALSE)
+  })
+  ## Titanic, partitioned by Survived, Sex
+  ti <- as.data.frame(datasets::Titanic)
+  x <- lapply(split(ti, ti$Survived), function(y) split(y, y$Sex))
+  lapply(names(x), function(sur) {
+    lapply(names(x[[sur]]), function(sex) {
+      file <- file.path(path, "titanic", paste("Survived", sur, sep="="), paste("Sex", sex, sep="="), "data.csv")
+      dir.create(dirname(file), recursive=TRUE, showWarnings=FALSE)
+      utils::write.csv(x[[sur]][[sex]], file=file, row.names=FALSE)
+    })
+  })
+  utils::write.csv(datasets::state.x77, file=file.path(path, "state.csv"), row.names=row.names(state.x77))
+  conn <- DBI::dbConnect(duckdb::duckdb(), dbdir=":memory:")
+  d <- dm_duckdb_csv(path, conn)
+  expect_true(inherits(d, "dm"))
+  expect_equal(
+    names(d),
+    c("iris","state","titanic")
+  )
+  expect_equal(
+    sapply(lapply(lapply(d, dplyr::count), collect), `[[`, "n"),
+    c(iris=nrow(iris), state=nrow(state.x77), titanic=nrow(ti))
+  )
+  DBI::dbDisconnect(conn)
+})
+
+test_that("dm_duckdb_csv() handles updates to data", {
+  ## we technically dont update here but rewrite
+  ## duckdb seems to not care about that
+  ## so most likely does not hold open connection to file
+  ## this could possibly change in future and then this test has to be updated to really update the file rather than rewrite it
+
+  path <- file.path(tempdir(), "data-updates")
+  dir.create(path, showWarnings=FALSE)
+  df <- data.frame(a=1:2, b=3:4)
+  file <- file.path(path, "data.csv")
+  utils::write.csv(df, file=file, row.names=FALSE)
+  conn <- DBI::dbConnect(duckdb::duckdb(), dbdir=":memory:")
+  d <- dm_duckdb_csv(path, conn)
+  expect_equal(collect(d$data), tibble(a=1:2, b=3:4))
+  df <- data.frame(a=c(1,20), b=3:4)
+  utils::write.csv(df, file=file, row.names=FALSE)
+  expect_equal(collect(d$data), tibble(a=c(1,20), b=3:4))
+
+  invisible(DBI::dbDisconnect(conn))
+})
+
+test_that("dm_duckdb_csv() handles inserts to data", {
+
+  path <- file.path(tempdir(), "data-inserts")
+  dir.create(path, showWarnings=FALSE)
+  df <- data.frame(a=1:2, b=3:4)
+  file <- file.path(path, "data.csv")
+  utils::write.csv(df, file=file, row.names=FALSE)
+  conn <- DBI::dbConnect(duckdb::duckdb(), dbdir=":memory:")
+  d <- dm_duckdb_csv(path, conn)
+  expect_identical(collect(dplyr::count(d$data))$n, 2)
+  cat("3,6", file=file, append=TRUE)
+  expect_identical(collect(dplyr::count(d$data))$n, 3)
+
+  ## test inserting to partition
+  ## TODO
+  ## test inserting to new (non existing before) partition
+  ## TODO
+
+  invisible(DBI::dbDisconnect(conn))
+})
+
+test_that("dm_duckdb_csv() handles partitioned tables of 2+ csv files per partition", {
+
+})
+
+test_that("dm_duckdb_csv() errors for invalid input", {
+
+})
+
+test_that("dm_duckdb_csv() errors for invalid csv files structure", {
+
+})
