61a16e37a31d13f8a33c87dd26295d11da87741e
Kirill Müller <krlmlr+r@mailbox.org>
2023-06-18 10:10:47 +0200

    Switch to using Id() for mssql_sys_db()

--- R/mssql.R
+++ R/mssql.R
@@ -1,13 +1,12 @@
-mssql_sys_db <- function(con, dbname, name, vars = NULL) {
+mssql_sys_db <- function(con, dbname, sys_table, vars = NULL) {
   if (is.na(dbname)) {
-    fq_name <- name
+    id <- Id(schema = "sys", table = sys_table)
     sql_name <- sql("DB_NAME()")
   } else {
-    fq_name <- paste0(dbname, ".", name)
+    id <- Id(catalog = dbname, schema = "sys", table = sys_table)
     sql_name <- dbname
   }
-  browser()
-  tbl(con, dbplyr::ident_q(fq_name), vars = vars) %>%
+  tbl(con, id, vars = vars) %>%
     mutate(catalog = !!sql_name) %>%
     select(catalog, everything())
 }
@@ -19,31 +18,31 @@ mssql_constraint_column_usage <- function(con, table_constraints, dbname) {
     select(constraint_catalog, constraint_schema, constraint_name, constraint_type) %>%
     filter(constraint_type == "FOREIGN KEY")
 
-  fkc <- mssql_sys_db(con, dbname, "sys.foreign_key_columns", vars = c(
+  fkc <- mssql_sys_db(con, dbname, "foreign_key_columns", vars = c(
     "constraint_object_id", "constraint_column_id",
     "referenced_object_id", "referenced_column_id"
   ))
 
   columns <-
-    mssql_sys_db(con, dbname, "sys.columns", vars = c(
+    mssql_sys_db(con, dbname, "columns", vars = c(
       "name", "object_id", "column_id"
     )) %>%
     rename(column_name = name)
 
   tables <-
-    mssql_sys_db(con, dbname, "sys.tables", vars = c(
+    mssql_sys_db(con, dbname, "tables", vars = c(
       "schema_id", "name", "object_id"
     )) %>%
     rename(table_name = name)
 
   schemas <-
-    mssql_sys_db(con, dbname, "sys.schemas", vars = c(
+    mssql_sys_db(con, dbname, "schemas", vars = c(
       "schema_id", "name"
     )) %>%
     rename(table_schema = name)
 
   objects <-
-    mssql_sys_db(con, dbname, "sys.objects", vars = c(
+    mssql_sys_db(con, dbname, "objects", vars = c(
       "name", "object_id"
     )) %>%
     select(constraint_name = name, object_id)
