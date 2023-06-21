d39d71b0c8a829723207770563599c9c37335898
Kirill Müller <krlmlr+r@mailbox.org>
2023-06-21 06:04:38 +0200

    Document globals

--- R/globals.R
+++ R/globals.R
@@ -10,6 +10,7 @@ utils::globalVariables(c(
   "unique_def", # <build_copy_queries>
   "child_table", # <build_copy_queries>
   "child_fk_cols", # <build_copy_queries>
+  "on_delete", # <build_copy_queries>
   "fk_def", # <build_copy_queries>
   "remote_name", # <build_copy_queries>
   "remote_name_unquoted", # <build_copy_queries>
@@ -130,7 +131,6 @@ utils::globalVariables(c(
   "table_name", # <dm_learn_from_db>
   "columns", # <dm_learn_from_db>
   "ordinal_position", # <dm_learn_from_db>
-  "delete_rule", # <dm_learn_from_db>
   "constraint_column_usage", # <dm_learn_from_db>
   "key_column_usage", # <dm_learn_from_db>
   "table_constraints", # <dm_learn_from_db>
@@ -139,7 +139,9 @@ utils::globalVariables(c(
   "constraint_catalog", # <dm_learn_from_db>
   "constraint_schema", # <dm_learn_from_db>
   "constraint_name", # <dm_learn_from_db>
+  "is_autoincrement", # <dm_learn_from_db>
   "pks", # <dm_learn_from_db>
+  "delete_rule", # <dm_learn_from_db>
   "constraint_column_usage.dm_name", # <dm_learn_from_db>
   "constraint_column_usage.column_name", # <dm_learn_from_db>
   "key_column_usage.dm_name", # <dm_learn_from_db>
@@ -148,12 +150,12 @@ utils::globalVariables(c(
   "ref_column", # <dm_learn_from_db>
   "column", # <dm_learn_from_db>
   "fks", # <dm_learn_from_db>
+  "extra", # <dm_meta_raw>
   "constraint_catalog", # <dm_meta_raw>
   "constraint_schema", # <dm_meta_raw>
   "table_name", # <dm_meta_raw>
   "constraint_type", # <dm_meta_raw>
   "constraint_name", # <dm_meta_raw>
-  "is_autoincrement", # <dm_meta_raw>
   "referenced_table_name", # <dm_meta_raw>
   "referenced_table_schema", # <dm_meta_raw>
   "referenced_column_name", # <dm_meta_raw>
@@ -192,11 +194,13 @@ utils::globalVariables(c(
   "ordinal_position", # <select_dm_meta>
   "column_default", # <select_dm_meta>
   "is_nullable", # <select_dm_meta>
+  "is_autoincrement", # <select_dm_meta>
   "table_constraints", # <select_dm_meta>
   "constraint_catalog", # <select_dm_meta>
   "constraint_schema", # <select_dm_meta>
   "constraint_name", # <select_dm_meta>
   "constraint_type", # <select_dm_meta>
+  "delete_rule", # <select_dm_meta>
   "key_column_usage", # <select_dm_meta>
   "constraint_column_usage", # <select_dm_meta>
   "DATABASE", # <filter_dm_meta>
