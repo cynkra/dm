e8236b7ea3932f5236abb5714e62e5004502f011
Kirill Müller <krlmlr+r@mailbox.org>
2023-07-11 23:44:03 +0200

    Close pool

--- tests/testthat/test-pool-support.R
+++ tests/testthat/test-pool-support.R
@@ -3,6 +3,7 @@ test_that("dm_from_con() supports 'Pool'", {
 
   # expect no error
   conn <- pool::dbPool(RSQLite::SQLite(), dbname = "", timeout = 10)
+  on.exit(pool::poolClose(conn))
   DBI::dbWriteTable(conn, "mtcars", mtcars)
   dm <- dm::dm_from_con(conn, learn_keys = FALSE)
   expect_identical(names(dm), "mtcars")
