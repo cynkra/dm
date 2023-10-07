44da3a6dc4b68a994ef24fa3ec2253c44f51bbcf
Kirill Müller <kirill@cynkra.com>
2023-10-07 15:21:19 +0200

    chore: Switch to usethis imports, blanket vctrs import

--- R/dm-package.R
+++ R/dm-package.R
@@ -1,20 +1,22 @@
-#' @import cli
+#' @keywords internal
+"_PACKAGE"
+
 #' @import rlang
-#' @importFrom vctrs vec_ptype2 vec_ptype_abbr vec_slice vec_cast vec_proxy_compare vec_data vec_c
-#' @importFrom vctrs vec_names vec_as_names vec_cbind vec_rbind vec_recycle
-#' @importFrom vctrs list_of as_list_of new_list_of is_list_of
-#' @importFrom vctrs vec_size vec_match s3_register vec_duplicate_detect vec_split
-#' @import dplyr
-#' @import DBI
-#' @import tibble
-#' @rawNamespace import(tidyr, except = c(extract))
-#' @rawNamespace import(purrr, except = c(list_along, rep_along, invoke, modify, as_function, flatten_dbl, flatten_lgl, flatten_int, flatten_raw, flatten_chr, splice, flatten, prepend, `%@%`, `%||%`))
-#' @rawNamespace import(magrittr, except = c(set_names))
-#' @importFrom methods is
+#' @import cli
 #' @import glue
+#' @import lifecycle
+#' @import vctrs
+#' @import tibble
+#' @import dplyr
+#' @rawNamespace import(purrr, except = c(list_along, rep_along, invoke, modify, as_function, flatten_dbl, flatten_lgl, flatten_int, flatten_raw, flatten_chr, splice, flatten, prepend, `%@%`, `%||%`))
+#' @rawNamespace import(tidyr, except = c(extract))
+#' @rawNamespace import(magrittr, except = c(set_names))
+#' @import DBI
+## usethis namespace: start
+#' @importFrom methods is
 #' @importFrom igraph V E
 #' @importFrom utils head tail str packageVersion
-#' @import lifecycle
 #' @importFrom grDevices col2rgb colors rgb
 #' @importFrom pillar pillar_shaft
+## usethis namespace: end
 NULL
