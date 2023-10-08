2d89cdf36418fbc0252530eeb8a02357d7b93a7f
Kirill Müller <kirill@cynkra.com>
2023-10-08 13:50:29 +0200

    Test copies

---
+++ tests/testthat/test-duckdb.R
@@ -0,0 +1,15 @@
+test_that("dummy", {
+  expect_snapshot({
+    "dummy"
+  })
+})
+
+skip_if_src_not("duckdb")
+
+test_that("dm_sql()", {
+  expect_snapshot({
+    dm_for_filter() %>%
+      collect() %>%
+      dm_sql(my_test_con())
+  })
+})
