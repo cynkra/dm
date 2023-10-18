d2de155f1d79b9957127bfb911de11664ba76698
Kirill Müller <kirill@cynkra.com>
2023-10-18 16:09:41 +0200

    Only use ptype if from database

--- R/dm_sql.R
+++ R/dm_sql.R
@@ -107,8 +107,12 @@ dm_ddl_pre <- function(
 
   dm <- ddl_reorder_dm(dm, dest)
 
-  ## use 0-rows object
-  ptype_dm <- collect(dm_ptype(dm))
+  ## use 0-rows object if from database
+  if (is_src_db(dm)) {
+    ptype_dm <- collect(dm_ptype(dm))
+  } else {
+    ptype_dm <- dm
+  }
 
   con <- con_from_src_or_con(dest)
 
